3e283aa79ee53bf539b47e7c30798661b35ee0cf
dreamflyer <gleb@onpositive.com>
2018-03-27 03:41:37 +0600

    built in types references completion;

---
+++ shared/src/main/scala/org/mulesoft/als/suggestions/plugins/raml/TypeReferencesCompletionPlugin.scala
@@ -0,0 +1,51 @@
+package org.mulesoft.als.suggestions.plugins.raml
+
+import amf.core.remote.{Raml10, Vendor}
+import org.mulesoft.als.suggestions.implementation.Suggestion
+import org.mulesoft.als.suggestions.interfaces.{ICompletionPlugin, ICompletionRequest, ISuggestion}
+
+class TypeReferenceCompletionPlugin extends ICompletionPlugin {
+	override def id: String = TemplateReferencesCompletionPlugin.ID;
+	
+	override def languages: Seq[Vendor] = TemplateReferencesCompletionPlugin.supportedLanguages;
+	
+	override def isApplicable(request:ICompletionRequest): Boolean = request.config.astProvider match {
+		case Some(astProvider) => languages.indexOf(astProvider.language) >= 0 && isInTypeTypeProperty(request);
+		
+		case _ => false;
+	}
+	
+	override def suggest(request: ICompletionRequest): Seq[ISuggestion] = {
+		var builtIns = request.astNode.get.asAttr.get.definition.get.universe.builtInNames() :+ "object";
+		
+		builtIns.map(name => Suggestion(name, id, name, request.prefix));
+	}
+	
+	def isInTypeTypeProperty(request: ICompletionRequest): Boolean = {
+		if(request.astNode.get.isElement) {
+			return false;
+		}
+		
+		if(request.astNode.get.property.get == null) {
+			return false;
+		}
+		
+		if(request.astNode.get.property.get.nameId.get != "type") {
+			return false;
+		}
+		
+		if(request.astNode.get.property.get.domain.get.nameId.get != "TypeDeclaration") {
+			return false;
+		}
+		
+		true;
+	}
+}
+
+object TypeReferenceCompletionPlugin {
+	val ID = "typeRef.completion";
+	
+	val supportedLanguages: List[Vendor] = List(Raml10);
+	
+	def apply(): TypeReferenceCompletionPlugin = new TypeReferenceCompletionPlugin();
+}
