4e49e0d359201edd311467e599e4ab80032a5b7d
dreamflyer <gleb@onpositive.com>
2018-04-03 04:03:53 +0600

    adding tests

--- shared/src/test/scala/org/mulesoft/als/suggestions/test/raml/StructureTests.scala
+++ shared/src/test/scala/org/mulesoft/als/suggestions/test/raml/StructureTests.scala
@@ -3,8 +3,11 @@ package org.mulesoft.als.suggestions.test.raml
 import org.mulesoft.als.suggestions.test.RAMLTest
 
 class StructureTests extends RAMLTest {
-
-  test("test"){
-    this.runTest("test01.raml", Set("responses"))
-  }
+	test("test") {
+		this.runTest("test01.raml", Set("responses"));
+	}
+	
+	test("facets test") {
+		this.runTest("facets/test01.raml", Set("testFacet1", "testFacet2", "testFacet3", "testFacet5"));
+	}
 }
