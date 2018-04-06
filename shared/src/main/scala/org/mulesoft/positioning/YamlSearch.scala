64f3a557d635af6b77b32b38cc2c32f1a1f37497
Denis Denisenko <denis@onpositive.com>
2018-04-07 00:01:09 +0700

    Bugfixing, adding parent stack to yaml search results

--- shared/src/main/scala/org/mulesoft/positioning/YamlSearch.scala
+++ shared/src/main/scala/org/mulesoft/positioning/YamlSearch.scala
@@ -5,7 +5,8 @@ import org.yaml.model._
 
 object YamlSearch {
 
-    def getLocation(position:Int, yPart:YPart, mapper:IPositionsMapper):YamlLocation = {
+    def getLocation(position:Int, yPart:YPart, mapper:IPositionsMapper,
+                    parentStack: List[YamlLocation] = List()):YamlLocation = {
 
         val range = YRange(yPart)
         val offset = mapper.offset(position)
@@ -20,8 +21,8 @@ object YamlSearch {
 
             yPart match {
                 case mapEntry: YMapEntry =>
-                    val thisLocation = YamlLocation(mapEntry, mapper)
-                    var keyLocation = getLocation(position,mapEntry.key,mapper)
+                    val thisLocation = YamlLocation(mapEntry, mapper, parentStack)
+                    var keyLocation = getLocation(position,mapEntry.key,mapper, thisLocation :: parentStack)
                     if(keyLocation.nonEmpty){
                         thisLocation
                     }
@@ -29,7 +30,7 @@ object YamlSearch {
                         YamlLocation.empty
                     }
                     else {
-                        var valLocation = getLocation(position,mapEntry.value,mapper)
+                        var valLocation = getLocation(position,mapEntry.value,mapper, thisLocation :: parentStack)
                         if(valLocation.isEmpty){
                             thisLocation
                         }
@@ -42,8 +43,8 @@ object YamlSearch {
                     }
 
                 case node:YNode =>
-                    var thisLocation = YamlLocation(node,mapper)
-                    var valLocation = getLocation(position,node.value,mapper)
+                    var thisLocation = YamlLocation(node,mapper, parentStack)
+                    var valLocation = getLocation(position,node.value,mapper, thisLocation :: parentStack)
                     if(valLocation.isEmpty){
                         thisLocation
                     }
@@ -60,10 +61,10 @@ object YamlSearch {
                         mapper.initRange(r)
                         r.containsPosition(position)
                     })
-                    val thisLocation = YamlLocation (map, mapper)
+                    val thisLocation = YamlLocation (map, mapper, parentStack)
                     entryOpt match {
                         case Some(me) =>
-                            var entryLocation = getLocation(position, me, mapper)
+                            var entryLocation = getLocation(position, me, mapper, thisLocation :: parentStack)
                             if (entryLocation.nonEmpty) {
                                 entryLocation
                             }
@@ -75,7 +76,7 @@ object YamlSearch {
                     }
 
                 case sequence:YSequence =>
-                    val thisLocation = YamlLocation (sequence, mapper)
+                    val thisLocation = YamlLocation (sequence, mapper, parentStack)
                     var componentOpt = sequence.nodes.find(n=>{
                         var r = YRange(n)
                         mapper.initRange(r)
@@ -83,19 +84,19 @@ object YamlSearch {
                     })
                     componentOpt match {
                         case Some(me) =>
-                            var componentLocation = getLocation(position, me, mapper)
+                            var componentLocation = getLocation(position, me, mapper, thisLocation :: parentStack)
                             if (componentLocation.nonEmpty) {
                                 componentLocation
                             }
                             else {
                                 thisLocation
                             }
-                        case None => YamlLocation(sequence,mapper)
+                        case None => YamlLocation(sequence,mapper, parentStack)
                     }
 
-                case scalar:YScalar => YamlLocation(scalar,mapper)
+                case scalar:YScalar => YamlLocation(scalar,mapper, parentStack)
 
-                case _ => YamlLocation(yPart,mapper)
+                case _ => YamlLocation(yPart,mapper, parentStack)
             }
         }
     }
