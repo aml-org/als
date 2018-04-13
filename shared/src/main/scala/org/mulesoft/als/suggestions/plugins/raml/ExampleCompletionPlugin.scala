a829b7e9b80d484cce747019769a5d592aa5e842
dreamflyer <gleb@onpositive.com>
2018-04-14 01:39:53 +0600

    JSON example completion

--- shared/src/main/scala/org/mulesoft/als/suggestions/plugins/raml/ExampleCompletionPlugin.scala
+++ shared/src/main/scala/org/mulesoft/als/suggestions/plugins/raml/ExampleCompletionPlugin.scala
@@ -1,12 +1,12 @@
 package org.mulesoft.als.suggestions.plugins.raml
 
 import amf.core.remote.{Raml10, Vendor}
-
 import org.mulesoft.als.suggestions.implementation.Suggestion
 import org.mulesoft.als.suggestions.interfaces.{ICompletionPlugin, ICompletionRequest, ISuggestion, LocationKind}
+import org.mulesoft.positioning.{PositionsMapper, YamlLocation, YamlSearch}
 import org.mulesoft.typesystem.nominal_interfaces.ITypeDefinition
-
-import org.yaml.model.YScalar
+import org.yaml.model._
+import org.yaml.parser.YamlParser
 
 import scala.concurrent.{Future, Promise}
 
@@ -15,8 +15,10 @@ class ExampleCompletionPlugin extends ICompletionPlugin {
 	
 	override def languages: Seq[Vendor] = FacetsCompletionPlugin.supportedLanguages;
 	
+	private def positionMarker = "$$$positionMarker$$$";
+	
 	override def isApplicable(request:ICompletionRequest): Boolean = request.config.astProvider match {
-		case Some(astProvider) => languages.indexOf(astProvider.language) >= 0 && isFullRequest(request) && isInKey(request) && isExample(request);
+		case Some(astProvider) => languages.indexOf(astProvider.language) >= 0 && isFullRequest(request) && isExample(request) && isInKey(request);
 		
 		case _ => false;
 	}
@@ -25,8 +27,118 @@ class ExampleCompletionPlugin extends ICompletionPlugin {
 		request.astNode.isDefined && request.astNode.get != null && request.astNode.get.property.isDefined && request.astNode.get.property.get != null;
 	}
 	
-	def isInKey(request: ICompletionRequest): Boolean = {
-		request.kind == LocationKind.KEY_COMPLETION;
+	def isInKey(request: ICompletionRequest): Boolean = if(request.kind == LocationKind.KEY_COMPLETION && extractScalarNode(request).isEmpty) {
+		true;
+	} else extractParsedScalar(request) match {
+		case Some(node) => {
+			findLocationInParsedScalar(request, node) match {
+				case Some(location) => location.keyValue.nonEmpty;
+				
+				case _ => false
+			}
+		}
+		
+		case _ => false;
+	}
+	
+	def findIndent(scalarNode: YScalar): Int = scalarNode.children match {
+		case children if children.nonEmpty => children(0) match {
+			case child: YNonContent => child.tokens.filter(_.tokenType.name == "Indent").map(_.text.length).min;
+			
+			case _ => -1;
+		}
+		
+		case _ => -1;
+	}
+	
+	def findFirstIndent(scalarNode: YScalar): Int = scalarNode.children match {
+		case children if children.nonEmpty => children(0) match {
+			case child: YNonContent => child.tokens.filter(_.tokenType.name == "Indent").reverse.last.text.length;
+			
+			case _ => -1;
+		}
+		
+		case _ => -1;
+	}
+	
+	def findLocationInParsedScalar(request: ICompletionRequest, parsedScalar: YNode): Option[YamlLocation] = extractScalarNode(request) match {
+		case Some(node) => Some(YamlSearch.getLocation(relativePosition(request), parsedScalar, PositionsMapper("").withText(node.text), List(), true));
+		
+		case _ => Option.empty;
+	}
+	
+	def relativePosition(request: ICompletionRequest): Int = {
+		var scalarPosition = request.yamlLocation.get.value.get.range.start.position;
+		
+		var text = getContent(request);
+		
+		var leftPart = text.substring(scalarPosition).trim;
+		
+		var actualLeftPart = leftPart;
+		
+		if(actualLeftPart.startsWith("|") && actualLeftPart.length > 1) {
+			actualLeftPart = actualLeftPart.substring(1).trim
+		}
+		
+		var valueShift = leftPart.length - actualLeftPart.length;
+		
+		var indentedPosition = request.position - scalarPosition - valueShift;
+		
+		var indent = findIndent(extractScalarNode(request).get);
+		
+		var firstIndent = findFirstIndent(extractScalarNode(request).get);
+		
+		actualLeftPart = 0.to(firstIndent - 1).map(_ => " ").mkString + actualLeftPart;
+		
+		indentedPosition = indentedPosition + firstIndent;
+		
+		actualLeftPart = actualLeftPart.substring(0, indentedPosition) + positionMarker + actualLeftPart.substring(indentedPosition);
+		
+		var notIndentedLeftPart = actualLeftPart.lines.map(_.drop(firstIndent) + "\n").mkString
+		
+		notIndentedLeftPart.indexOf(positionMarker);
+	}
+	
+	def getContent(request: ICompletionRequest): String = {
+		request.config.editorStateProvider match {
+			case Some(provider) => provider.getText;
+			
+			case _ => ""
+		}
+	}
+	
+	def extractScalarNode(request: ICompletionRequest): Option[YScalar] = {
+		request.yamlLocation match {
+			case Some(location) => location.value match {
+				case Some(value) => value.yPart match {
+					case scalar: YScalar => Some(scalar);
+					
+					case _ => Option.empty;
+				}
+				
+				case _ => Option.empty;
+			}
+			
+			case _ => Option.empty;
+		}
+	}
+	
+	def extractParsedScalar(request: ICompletionRequest): Option[YNode] = extractScalarNode(request) match {
+		case Some(scalar: YScalar) => YamlParser(scalar.text) match {
+			case parser => parser.parse(true) match {
+				case documents if documents.nonEmpty => documents.last match {
+					case document: YDocument => Some(document.node);
+					
+					case _ => Option.empty;
+				}
+				
+				case _ => Option.empty;
+			}
+			
+			case _ => Option.empty;
+		}
+		
+		case _ => Option.empty;
 	}
 	
 	override def suggest(request: ICompletionRequest): Future[Seq[ISuggestion]] = Promise.successful(findProperties(extractAstPath(request), extractLocalType(request)).map(propertyName => Suggestion(propertyName, id, propertyName, request.prefix))).future
@@ -55,15 +167,23 @@ class ExampleCompletionPlugin extends ICompletionPlugin {
 		case _ => null;
 	}
 	
-	def extractAstPath(request: ICompletionRequest): Seq[String] = request.actualYamlLocation.get.parentStack.filter(_.keyValue.isDefined).map(_.keyValue.get.yPart).filter(_ match {
-		case n: YScalar => true;
+	def extractAstPath(request: ICompletionRequest): Seq[String] = (extractScalarNode(request) match {
+		case Some(node) => findLocationInParsedScalar(request, extractParsedScalar(request).get);
 		
-		case _ => false
-	}).map(_.asInstanceOf[YScalar].text);
-	
+		case _ => request.actualYamlLocation
+	}) match {
+		case Some(location) => location.parentStack.filter(_.keyValue.isDefined).map(_.keyValue.get.yPart).filter(_ match {
+			case n: YScalar => true;
+			
+			case _ => false
+		}).map(_.asInstanceOf[YScalar].text)
+		
+		case _ => Seq();
+	}
+
 	def isExample(request: ICompletionRequest): Boolean = request.astNode.get.property.get.domain match {
 		case Some(domain) => domain.nameId.get == "ExampleSpec";
-		
+
 		case _ => false;
 	}
 }
