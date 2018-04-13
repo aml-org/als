03381e34b773fcb8e9bf234a05bd0f5cfeab0999
dreamflyer <gleb@onpositive.com>
2018-04-14 01:40:06 +0600

    adding tests

--- shared/src/test/scala/org/mulesoft/als/suggestions/test/raml/ExamplesTests.scala
+++ shared/src/test/scala/org/mulesoft/als/suggestions/test/raml/ExamplesTests.scala
@@ -3,7 +3,11 @@ package org.mulesoft.als.suggestions.test.raml
 import org.mulesoft.als.suggestions.test.RAMLTest
 
 class ExamplesTests extends RAMLTest {
-	test("test"){
+	test("common test"){
 		this.runTest("examples/test01.raml", Set("valueSeven", "valueEight"))
+	}
+	
+	test("json test"){
+		this.runTest("examples/test02.raml", Set("valueSeven", "valueEight"))
 	}
 }
