83a947f19453da42f75b726448df6f3224dd77ab
Denis Denisenko <denis@onpositive.com>
2018-05-31 10:29:19 +0700

    Adapting to newer version of amf

--- js/src/main/scala/org/mulesoft/als/suggestions/js/FSProviderBasedPlatform.scala
+++ js/src/main/scala/org/mulesoft/als/suggestions/js/FSProviderBasedPlatform.scala
@@ -1,14 +1,19 @@
 package org.mulesoft.als.suggestions.js
 
-import amf.core.lexer.CharSequenceStream
+import java.net.URI
 
+import amf.client.remote.Content
+import amf.core.lexer.CharSequenceStream
 import amf.core.remote._
-import amf.client.remote._
+import amf.core.remote.server.Path
+import amf.internal.resource.ResourceLoader
 import org.mulesoft.common.io.FileSystem
 import org.scalajs.dom.ext.Ajax
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.scalajs.js.URIUtils
+import amf.internal.environment.Environment
 
 object Http {
   def unapply(uri: String): Option[(String, String, String)] = uri match {
@@ -44,6 +49,15 @@ class FSProviderBasedPlatform (fsProvider: IFSProvider) extends Platform {
 
   override val fs: FileSystem = new FSProviderBasedFS(fsProvider)
 
+  val fileLoader = this.createFileLoader(this.fsProvider)
+  val loaders: Seq[ResourceLoader] = Seq(
+
+    fileLoader,
+    this.createHttpLoader()
+  )
+
+  val defaultEnvironment = new Environment(this.loaders)
+
   override def resolvePath(uri: String): String = {
 
     uri match {
@@ -58,35 +72,66 @@ class FSProviderBasedPlatform (fsProvider: IFSProvider) extends Platform {
     }
   }
 
-  override protected def fetchFile(path: String): Future[Content] = {
-
-    val uri = if(path.startsWith(File.FILE_PROTOCOL)) path else File.FILE_PROTOCOL + path
 
 
-    this.fsProvider.contentAsync(uri).toFuture
-      .map(
-        content => {
+  override def resolve(url: String, environment: Environment = defaultEnvironment): Future[Content] =
+    super.resolve(url, environment )
 
-          Content(new CharSequenceStream(path, content),
-            ensureFileAuthority(path),
-            extension(path).flatMap(mimeFromExtension))
-        })
-  }
+//  private def loaderConcat(url: String, loaders: Seq[ResourceLoader]): Future[Content] = loaders.toList match {
+//    case Nil         => Future.failed(new UnsupportedUrlScheme(url))
+//    case head :: Nil => head.fetch(url)
+//    case head :: tail =>
+//      head.fetch(url).recoverWith {
+//        case _ => loaderConcat(url, tail)
+//      }
+//  }
+//  def resolve(url: String): Future[Content] = {
+//    println("Here1")
+//    println("Original loaders: " + defaultEnvironment.loaders)
+//
+//    val filteredLoaders = defaultEnvironment.loaders.filter(loader=>{
+//      println("Loader: " + loader)
+//      loader.accepts(url)
+//    })
+//
+//    println("Filtered loaders: " + filteredLoaders)
+//
+//    val result = loaderConcat(url, filteredLoaders)
+//
+//    println("Here2")
+//    result
+//  }
 
-  override protected def fetchHttp(url: String): Future[Content] = {
 
-    Ajax
-      .get(url)
-      .flatMap(xhr =>
-        xhr.status match {
-          case 200 => Future { Content(new CharSequenceStream(xhr.responseText), url) }
-          case s   => Future.failed(FileNotFound(new Exception(s"Unhandled status code $s with ${xhr.statusText}")))
-        })
-  }
+  /** encodes a complete uri. Not encodes chars like / */
+  override def encodeURI(url: String): String = URIUtils.encodeURI(url)
+
+  /** encodes a uri component, including chars like / and : */
+  override def encodeURIComponent(url: String): String = URIUtils.encodeURIComponent(url)
+
+  /** decode a complete uri. */
+  override def decodeURI(url: String): String = URIUtils.decodeURI(url)
+
+  /** decodes a uri component */
+  override def decodeURIComponent(url: String): String = URIUtils.decodeURIComponent(url)
+
+  override def normalizeURL(url: String): String = Path.resolve(url)
+
+  override def normalizePath(url: String): String = new URI(encodeURI(url)).normalize.toString
+
+  override def findCharInCharSequence(stream: CharSequence)(p: Char => Boolean): Option[Char] = stream.toString.find(p)
 
   override def tmpdir(): String = ???
 
   private def withTrailingSlash(path: String) = {
     (if (!path.startsWith("/")) "/" else "") + path
+  }
+
+  protected def createFileLoader(fsProvider: IFSProvider): ResourceLoader = {
+    new FileLoader(fsProvider, this)
+  }
+
+  protected def createHttpLoader(): ResourceLoader = {
+    new HTTPLoader()
   }
 }
