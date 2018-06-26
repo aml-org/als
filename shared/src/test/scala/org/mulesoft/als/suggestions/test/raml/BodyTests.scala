e9e4db5ad98a3b4b83b875c5d3197539aa94dc13
KonstantinSviridov <ksviridov@gmail.com>
2018-06-26 20:09:50 +0700

    fix https://www.mulesoft.org/jira/browse/ALS-494

--- shared/src/test/scala/org/mulesoft/als/suggestions/test/raml/BodyTests.scala
+++ shared/src/test/scala/org/mulesoft/als/suggestions/test/raml/BodyTests.scala
@@ -4,15 +4,50 @@ import org.mulesoft.als.suggestions.test.RAMLTest
 
 class BodyTests extends RAMLTest {
 
-    test("test001") {
+    test("Method with no bodies") {
         this.runTest("body/test001.raml",
             Set("application/json:",
                 "application/xml:",
                 "multipart/formdata:",
+                "application/x-www-form-urlencoded:",
+                "displayName:",
+                "type:",
+                "xml:",
+                "default:",
+                "description:",
+                "schema:",
+                "examples:",
+                "example:",
+                "facets:",
+                "properties:"))
+    }
+
+    test("Method with some bodies") {
+        this.runTest("body/test002.raml",
+            Set("application/json:",
+                "multipart/formdata:",
                 "application/x-www-form-urlencoded:"))
     }
 
-    test("test002") {
+    test("Response with no bodies") {
+        this.runTest("body/test001.raml",
+            Set("application/json:",
+                "application/xml:",
+                "multipart/formdata:",
+                "application/x-www-form-urlencoded:",
+                "displayName:",
+                "type:",
+                "xml:",
+                "default:",
+                "description:",
+                "schema:",
+                "examples:",
+                "example:",
+                "facets:",
+                "properties:"))
+    }
+
+    test("Response with some bodies") {
         this.runTest("body/test002.raml",
             Set("application/json:",
                 "multipart/formdata:",
