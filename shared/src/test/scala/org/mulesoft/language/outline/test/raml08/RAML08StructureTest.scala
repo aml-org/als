036cb6a89222eb895a2486e6c4c14f34be2f5bc9
KonstantinSviridov <ksviridov@gmail.com>
2018-09-04 06:09:06 +0700

    switching to AMF 1.9.0-SNAPSHOT

---
+++ shared/src/test/scala/org/mulesoft/language/outline/test/raml08/RAML08StructureTest.scala
@@ -0,0 +1,7 @@
+package org.mulesoft.language.outline.test.raml08
+
+import org.mulesoft.language.outline.test.StructureTest
+
+class RAML08StructureTest extends StructureTest with RAML08Test {
+
+}
