9e731708a96923dc08baf5145bbda81fd32bb26f
dreamflyer <gleb@onpositive.com>
2018-10-15 16:32:17 +0600

    https://www.mulesoft.org/jira/browse/ALS-593

--- shared/src/main/scala/org/mulesoft/als/suggestions/plugins/raml/ExampleCompletionPlugin.scala
+++ shared/src/main/scala/org/mulesoft/als/suggestions/plugins/raml/ExampleCompletionPlugin.scala
@@ -3,6 +3,7 @@ package org.mulesoft.als.suggestions.plugins.raml
 import amf.core.remote.{Raml10, Vendor}
 import org.mulesoft.als.suggestions.implementation.{CompletionResponse, Suggestion}
 import org.mulesoft.als.suggestions.interfaces._
+import org.mulesoft.high.level.interfaces.IHighLevelNode
 import org.mulesoft.positioning.{PositionsMapper, YamlLocation, YamlSearch}
 import org.mulesoft.typesystem.nominal_interfaces.ITypeDefinition
 import org.yaml.model._
@@ -147,10 +148,12 @@ class ExampleCompletionPlugin extends ICompletionPlugin {
         Promise.successful(response).future
     }
 	
-	def findProperties(path: Seq[String], localType: Option[ITypeDefinition],request:ICompletionRequest): Seq[ISuggestion] = if(localType.isEmpty) {
+	def findProperties(path: Seq[String], localType: Option[ITypeDefinition], request:ICompletionRequest): Seq[ISuggestion] = if(localType.isEmpty) {
 		Seq();
 	} else if(path.isEmpty) {
-        localType.get.allProperties.map(prop => {
+		var siblingNames = extractSiblingNames(request, extractAstPath(request));
+		
+        localType.get.allProperties.filter(item => !siblingNames.contains(item.nameId.get)).map(prop => {
             var propertyName = prop.nameId.get
             var ws:String = ""
             if(prop.range.exists(!_.isValueType)) {
@@ -160,6 +163,7 @@ class ExampleCompletionPlugin extends ICompletionPlugin {
                     ws = "\n" + " " * pm.lineOffset(line) + "  "
                 })
             }
+			
             Suggestion(propertyName, s"'$propertyName' property", propertyName, request.prefix).withTrailingWhitespace(ws)
         });
 	} else localType.get.property(path.last) match {
@@ -168,11 +172,71 @@ class ExampleCompletionPlugin extends ICompletionPlugin {
 		case _ => Seq();
 	}
 	
+	def extractSiblingNames(request: ICompletionRequest, path: Seq[String]): Seq[String] = {
+		request.astNode match {
+			case Some(requestNode) => {
+				requestNode.parent match {
+					case Some(parent) => if(parent.sourceInfo.yamlSources.length > 0) {
+						parent.sourceInfo.yamlSources.head match {
+							case rootNode: YNode => yNodeByPath(rootNode, path) match {
+								case Some(ynodeValue: YNode) => ynodeValue.value match {
+									case value: YMap => value.entries.map(entry => entry.key.value match {
+										case scalar: YScalar => scalar.text
+										
+										case _ => null;
+									}).filter(_ != null);
+									
+									case _ => Seq();
+								}
+								
+								case _ => Seq();
+							};
+						}
+					} else {
+						Seq();
+					}
+					
+					case _ => Seq();
+				}
+			}
+			
+			case _ => Seq();
+		}
+	}
+	
+	def yNodeByPath(root: YNode, path: Seq[String]): Option[YNode] = {
+		if(path.isEmpty) {
+			return Some(root);
+		}
+		
+		var current = path.last;
+		
+		root.value match {
+			case value: YMap => value.entries.find(entry => entry.key.value match {
+				case scalar: YScalar => current.equals(scalar.text);
+				
+				case _ => false;
+			}) match {
+				case Some(entry) => entry.value match {
+					case node: YNode => yNodeByPath(node, path.dropRight(1));
+					
+					case _ => None;
+				};
+				
+				case _ => None;
+			};
+			
+			case _ => None;
+		}
+	}
+	
 	def extractLocalType(request: ICompletionRequest): Option[ITypeDefinition] = {
         var node = if(request.astNode.get.isElement) request.astNode else request.astNode.get.parent
         node.flatMap(_.parent).flatMap(_.localType)
     }
-
+	
+	def extractExampleNode(request: ICompletionRequest): Option[IHighLevelNode] = if(request.astNode.get.isElement) request.astNode.get.asElement else request.astNode.get.parent;
+	
 	def extractAstPath(request: ICompletionRequest): Seq[String] = (extractScalarNode(request) match {
 		case Some(node) => findLocationInParsedScalar(request, extractParsedScalar(request).get);
 		
