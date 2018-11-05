390011bd21b54b5b74a0ffffd0919ec6ee4ffe3c
KonstantinSviridov <ksviridov@gmail.com>
2018-11-06 02:09:31 +0700

    commit the missing interface

---
+++ shared/src/main/scala/org/mulesoft/typesystem/nominal_interfaces/IDialectUniverse.scala
@@ -0,0 +1,14 @@
+package org.mulesoft.typesystem.nominal_interfaces
+
+import org.mulesoft.typesystem.project.ITypeCollectionBundle
+
+import scala.collection.Map
+
+trait IDialectUniverse extends IUniverse {
+
+    def root: Option[ITypeDefinition]
+
+    def library: Option[ITypeDefinition]
+
+    def fragments:Map[String,ITypeDefinition]
+}
