5471083c706df6a95bec2ebc2250f9cc7b2f00f0
KonstantinSviridov <ksviridov@gmail.com>
2018-11-27 23:10:20 +0700

    working on https://www.mulesoft.org/jira/browse/ALS-630

---
+++ shared/src/test/scala/org/mulesoft/als/suggestions/test/aml/AMLSuggestionsTest.scala
@@ -0,0 +1,18 @@
+package org.mulesoft.als.suggestions.test.aml
+
+import amf.core.remote.Aml
+import org.mulesoft.als.suggestions.test.SuggestionsTest
+import org.mulesoft.high.level.interfaces.IProject
+import org.scalatest.Assertion
+
+import scala.concurrent.Future
+
+abstract class AMLSuggestionsTest extends SuggestionsTest {
+
+    def format:String = Aml.toString
+
+    def runTest(path:String,dialectPath:String,originalSuggestions: Set[String]):Future[Assertion] = {
+        parseAMF(filePath(dialectPath)).flatMap(_ => runTest(path,originalSuggestions))
+    }
+
+}
