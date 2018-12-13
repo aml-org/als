2d5ba6121733459e2cfe5bcbb8e84a8210c93b74
Hernan Najles <hernan.naj@gmail.com>
2018-12-13 15:15:01 -0300

    Fix js compilation

--- als-server/shared/src/test/scala/org/mulesoft/language/test/serverConnection/FS.scala
+++ als-server/shared/src/test/scala/org/mulesoft/language/test/serverConnection/FS.scala
@@ -9,29 +9,19 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 
 object FS extends PlatformSecrets {
-	def exists(path: String): Future[Boolean] = {
-		Future.successful(new File(path).exists())
-	}
+	def exists(path: String): Future[Boolean] = platform.fs.asyncFile(path).exists
 
-	def readDir(path: String): Future[Seq[String]] = {
-        val arr = new File(path).list()
-        Future.successful(arr.toSeq)
-	}
+	def readDir(path: String): Future[Array[String]] = platform.fs.asyncFile(path).list
 
-	def isDirectory(path: String): Future[Boolean] = {
-        Future.successful(new File(path).isDirectory())
-	}
+	def isDirectory(path: String): Future[Boolean] = platform.fs.asyncFile(path).isDirectory
 
 	def content(path: String): Future[String] = {
 		throw new Error("not implemented")
 	}
 
 	private def removeProtocol(path: String): String = {
-		if(path.indexOf("file:///") == 0) {
-			return path.replace("file:///", "/");
-		}
-
-		return path;
+		if(path.indexOf("file:///") == 0) path.replace("file:///", "/")
+		else path
 	}
 }
 
