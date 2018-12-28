d41b1b69955033358484d9f22bc9264eb162b082
Hernan Najles <hernan.naj@gmail.com>
2018-12-28 09:22:18 -0300

    ALS-667 check if key node at media types for bodies suggestions and add whitespaces

--- als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/raml10/BodyTests.scala
+++ als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/raml10/BodyTests.scala
@@ -2,59 +2,97 @@ package org.mulesoft.als.suggestions.test.raml10
 
 class BodyTests extends RAML10Test {
 
-    test("Method with no bodies") {
-        this.runTest("body/test001.raml",
-            Set("application/json:",
-                "application/xml:",
-                "multipart/form-data:",
-                "application/x-www-form-urlencoded:",
-                "displayName:",
-                "type:",
-                "xml:\n        ",
-                "default:",
-                "description:",
-                "schema:",
-                "examples:\n        ",
-                "example:\n        ",
-                "facets:\n        ",
-                "properties:\n        "))
-    }
+  test("Method with no bodies") {
+    this.runTest(
+      "body/test001.raml",
+      Set(
+        "application/json:\n        ",
+        "application/xml:\n        ",
+        "multipart/form-data:\n        ",
+        "application/x-www-form-urlencoded:\n        ",
+        "displayName:",
+        "type:",
+        "xml:\n        ",
+        "default:",
+        "description:",
+        "schema:",
+        "examples:\n        ",
+        "example:\n        ",
+        "facets:\n        ",
+        "properties:\n        "
+      )
+    )
+  }
 
-    test("Method with some bodies") {
-        this.runTest("body/test002.raml",
-            Set("application/json:",
-                "multipart/form-data:",
-                "application/x-www-form-urlencoded:"))
-    }
+  test("Method with some bodies") {
+    this.runTest(
+      "body/test002.raml",
+      Set("application/json:\n      ", "multipart/form-data:\n      ", "application/x-www-form-urlencoded:\n      "))
+  }
 
-    test("Response with no bodies") {
-        this.runTest("body/test003.raml",
-            Set("application/json:",
-                "application/xml:",
-                "displayName:",
-                "type:",
-                "xml:\n            ",
-                "default:",
-                "description:",
-                "schema:",
-                "examples:\n            ",
-                "example:\n            ",
-                "facets:\n            ",
-                "properties:\n            "))
-    }
+  test("Response with no bodies") {
+    this.runTest(
+      "body/test003.raml",
+      Set(
+        "application/json:\n            ",
+        "application/xml:\n            ",
+        "displayName:",
+        "type:",
+        "xml:\n            ",
+        "default:",
+        "description:",
+        "schema:",
+        "examples:\n            ",
+        "example:\n            ",
+        "facets:\n            ",
+        "properties:\n            "
+      )
+    )
+  }
 
-    test("Response with some bodies") {
-        this.runTest("body/test004.raml",
-            Set("application/json:"))
-    }
+  test("Response with some bodies") {
+    this.runTest("body/test004.raml", Set("application/json:\n          "))
+  }
 
-    test("Method body type shortcut") {
-        this.runTest("body/test005.raml",
-            Set("boolean", "integer", "datetime", "date-only", "datetime-only", "file", "any", "number", "string", "time-only", "nil", "array", "object", "A", "B"))
-    }
+  test("Method body type shortcut") {
+    this.runTest(
+      "body/test005.raml",
+      Set("boolean",
+          "integer",
+          "datetime",
+          "date-only",
+          "datetime-only",
+          "file",
+          "any",
+          "number",
+          "string",
+          "time-only",
+          "nil",
+          "array",
+          "object",
+          "A",
+          "B")
+    )
+  }
 
-    test("Response body type shortcut") {
-        this.runTest("body/test006.raml",
-            Set("boolean", "integer", "datetime", "date-only", "datetime-only", "file", "any", "number", "string", "time-only", "nil", "array", "object", "A", "B"))
-    }
+  test("Response body type shortcut") {
+    this.runTest(
+      "body/test006.raml",
+      Set("boolean",
+          "integer",
+          "datetime",
+          "date-only",
+          "datetime-only",
+          "file",
+          "any",
+          "number",
+          "string",
+          "time-only",
+          "nil",
+          "array",
+          "object",
+          "A",
+          "B")
+    )
+  }
 }
