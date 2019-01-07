b9ebe6377b87d589c5c6c336d1908c3894365dd0
Leandro Libarona <llibarona@llibarona-ltm.internal.salesforce.com>
2019-01-07 14:21:49 -0300

    ALS-668: Added cyclic test for Node Structures. Corrected bug with dialect (added link type). Ignored failed test with missin link type

--- als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/aml/DialectLevelSuggestionsTest.scala
+++ als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/aml/DialectLevelSuggestionsTest.scala
@@ -1,5 +1,6 @@
 package org.mulesoft.als.suggestions.test.aml
 
+import amf.core.model.domain.DomainElement
 import amf.plugins.document.vocabularies.AMLPlugin
 import amf.plugins.document.vocabularies.model.document.{Dialect, DialectInstance}
 import amf.plugins.document.vocabularies.model.domain.{DocumentMapping, NodeMapping, PropertyMapping}
@@ -17,15 +18,20 @@ trait DialectLevelSuggestionsTest extends SuggestionsTest {
     * @param nodeName : node father name for suggest properties. Send None (default) if is root node
     * @return
     */
-  def runDialectTest(path: String, goldenDialectPath: String, nodeName: Option[String] = None): Future[Assertion] = {
-    buildModel(path).flatMap { r =>
+  def runDialectTest(path: String,
+                     goldenDialectPath: String,
+                     nodeName: Option[String] = None,
+                     level: Int = 1,
+                     label: String = "*",
+                     labels: Array[String] = Array("*")): Future[Assertion] = {
+    buildModel(path, label, true, labels).flatMap { r =>
       r.u match {
         case d: DialectInstance =>
           AMLPlugin.registry.dialectFor(d) match {
             case Some(d: Dialect) =>
               assert(d.location().contains(goldenDialectPath))
               buildCompletionProviderFromUnit(r).flatMap(cp =>
-                cp.suggest.map(_.map(_.text).toSet).map(assertByDialect(_, d, nodeName, path, 1)))
+                cp.suggest.map(_.map(_.text).toSet).map(assertByDialect(_, d, nodeName, path, level)))
             case _ => fail(s"Cannot find dialect for ${d.definedBy().value()}")
           }
         case other => fail(s"Dialect Instance expected but ${other.meta.getClass.getName} found")
@@ -33,11 +39,22 @@ trait DialectLevelSuggestionsTest extends SuggestionsTest {
     }
   }
 
+  private def getPropertiesByPath(d: Dialect, nodeName: String): Seq[PropertyMapping] = {
+    val de: Option[DomainElement] = d.declares.find(de => de.id endsWith s"/${nodeName}")
+    de match {
+      case Some(n: NodeMapping) => n.propertiesMapping()
+    }
+  }
+
   def assertByDialect(actual: Set[String], d: Dialect, nodeName: Option[String], path: String, level: Int): Assertion = {
     nodeName match {
-      case Some(n) => ??? // todo implement assertion by one property (level2?)
-      case _ => // all root nodes
-        val mapping = d.documents().root()
+      case Some(n) => {
+        val dialectProps = getPropertiesByPath(d, n).map(addPropTrailingsSpaces(_, level)).toSet
+        assert(path, actual, dialectProps)
+      }
+      case _ => {
+        // all root nodes
+        val mapping: DocumentMapping = d.documents().root()
         val dialectProps: Set[String] =
           d.declares
             .find(_.id == mapping.encoded().value())
@@ -46,10 +63,9 @@ trait DialectLevelSuggestionsTest extends SuggestionsTest {
             .getOrElse(Set.empty) ++ mapping
             .declaredNodes()
             .map(_.name().value() + ":\n" + (" " * (level * 2))) // declared cannot be scalars??
-
         assert(path, actual, dialectProps)
+      }
     }
-
   }
 
   private def addPropTrailingsSpaces(propertyMapping: PropertyMapping, level: Int): String = {
