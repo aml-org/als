8e8d35af51bf46c3552088dedb6a44baa91fc230
Hernan Najles <hernan.naj@gmail.com>
2019-01-09 20:23:49 -0300

    ALS-689 add often keys for oas media types. Take yscalar text when searching key at ymap entries

---
+++ als-hl/shared/src/main/scala/org/mulesoft/typesystem/definition/system/OasCommonMediaTypes.scala
@@ -0,0 +1,18 @@
+package org.mulesoft.typesystem.definition.system
+
+object OasCommonMediaTypes extends OftenKeysConfig {
+
+  override val all: Seq[String] = Seq(
+    "text/plain; charset=utf-8",
+    "application/json",
+    "application/vnd.github+json",
+    "application/vnd.github.v3+json",
+    "application/vnd.github.v3.raw+json",
+    "application/vnd.github.v3.text+json",
+    "application/vnd.github.v3.html+json",
+    "application/vnd.github.v3.full+json",
+    "appxlication/vnd.github.v3.diff",
+    "application/vnd.github.v3.patch"
+  )
+  override val quotedMark: String = "\""
+}
