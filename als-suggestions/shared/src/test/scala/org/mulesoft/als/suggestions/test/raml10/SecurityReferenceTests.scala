5688744182600ec9f517aad915568ae6ed980dfe
Hernan Najles <hernan.naj@gmail.com>
2019-01-11 17:31:01 -0300

    ALS-690 move to amf 3.2.0-snapshot in order to have fix over empty responses at resource types

--- als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/raml10/SecurityReferenceTests.scala
+++ als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/raml10/SecurityReferenceTests.scala
@@ -1,115 +1,100 @@
 package org.mulesoft.als.suggestions.test.raml10
 
+import org.mulesoft.typesystem.definition.system.RamlResponseCodes
+
 class SecurityReferenceTests extends RAML10Test {
 
   test("authorizationGrants test 001") {
     this.runTest("security/test001.raml",
-      Set("[ authorization_code ]", "[ password ]", "[ client_credentials ]", "[ implicit ]"))
+                 Set("[ authorization_code ]", "[ password ]", "[ client_credentials ]", "[ implicit ]"))
   }
 
-    test("authorizationGrants test 002") {
-    this.runTest("security/test002.raml",
-        Set("authorization_code", "password", "client_credentials", "implicit"))
+  test("authorizationGrants test 002") {
+    this.runTest("security/test002.raml", Set("authorization_code", "password", "client_credentials", "implicit"))
   }
 
-    test("authorizationGrants test 003") {
-    this.runTest("security/test003.raml",
-        Set("password", "client_credentials", "implicit"))
+  test("authorizationGrants test 003") {
+    this.runTest("security/test003.raml", Set("password", "client_credentials", "implicit"))
   }
 
-    test("authorizationGrants test 004") {
-    this.runTest("security/test004.raml",
-        Set("[ authorization_code ]"))
+  test("authorizationGrants test 004") {
+    this.runTest("security/test004.raml", Set("[ authorization_code ]"))
   }
 
   test("Api securedBy test001") {
-    this.runTest("security/test005.raml",
-        Set("[ obasic ]","[ oauth2 ]","[ null ]"))
+    this.runTest("security/test005.raml", Set("[ obasic ]", "[ oauth2 ]", "[ null ]"))
   }
 
-    test("Api securedBy test002") {
-    this.runTest("security/test006.raml",
-        Set("[ obasic ]","[ oauth2 ]"))
+  test("Api securedBy test002") {
+    this.runTest("security/test006.raml", Set("[ obasic ]", "[ oauth2 ]"))
   }
 
-    test("Api securedBy test003") {
-    this.runTest("security/test007.raml",
-        Set("obasic","oauth2","null"))
+  test("Api securedBy test003") {
+    this.runTest("security/test007.raml", Set("obasic", "oauth2", "null"))
   }
 
-    test("Api securedBy test004") {
-    this.runTest("security/test008.raml",
-        Set("obasic","oauth2"))
+  test("Api securedBy test004") {
+    this.runTest("security/test008.raml", Set("obasic", "oauth2"))
   }
 
-    test("Api securedBy test005") {
-    this.runTest("security/test009.raml",
-        Set("null","oauth2"))
+  test("Api securedBy test005") {
+    this.runTest("security/test009.raml", Set("null", "oauth2"))
   }
 
-    test("Api securedBy test006") {
-    this.runTest("security/test010.raml",
-        Set("obasic","oauth2"))
+  test("Api securedBy test006") {
+    this.runTest("security/test010.raml", Set("obasic", "oauth2"))
   }
 
-    test("Resource securedBy test001") {
-        this.runTest("security/test005.raml",
-            Set("[ obasic ]","[ oauth2 ]","[ null ]"))
-    }
+  test("Resource securedBy test001") {
+    this.runTest("security/test005.raml", Set("[ obasic ]", "[ oauth2 ]", "[ null ]"))
+  }
 
-    test("Resource securedBy test002") {
-        this.runTest("security/test006.raml",
-            Set("[ obasic ]","[ oauth2 ]"))
-    }
+  test("Resource securedBy test002") {
+    this.runTest("security/test006.raml", Set("[ obasic ]", "[ oauth2 ]"))
+  }
 
-    test("Resource securedBy test003") {
-        this.runTest("security/test007.raml",
-            Set("obasic","oauth2","null"))
-    }
+  test("Resource securedBy test003") {
+    this.runTest("security/test007.raml", Set("obasic", "oauth2", "null"))
+  }
 
-    test("Resource securedBy test004") {
-        this.runTest("security/test008.raml",
-            Set("obasic","oauth2"))
-    }
+  test("Resource securedBy test004") {
+    this.runTest("security/test008.raml", Set("obasic", "oauth2"))
+  }
 
-    test("Resource securedBy test005") {
-        this.runTest("security/test009.raml",
-            Set("null","oauth2"))
-    }
+  test("Resource securedBy test005") {
+    this.runTest("security/test009.raml", Set("null", "oauth2"))
+  }
 
-    test("Resource securedBy test006") {
-        this.runTest("security/test010.raml",
-            Set("obasic","oauth2"))
-    }
+  test("Resource securedBy test006") {
+    this.runTest("security/test010.raml", Set("obasic", "oauth2"))
+  }
 
-    test("Method securedBy test001") {
-        this.runTest("security/test005.raml",
-            Set("[ obasic ]","[ oauth2 ]","[ null ]"))
-    }
+  test("Method securedBy test001") {
+    this.runTest("security/test005.raml", Set("[ obasic ]", "[ oauth2 ]", "[ null ]"))
+  }
 
-    test("Method securedBy test002") {
-        this.runTest("security/test006.raml",
-            Set("[ obasic ]","[ oauth2 ]"))
-    }
+  test("Method securedBy test002") {
+    this.runTest("security/test006.raml", Set("[ obasic ]", "[ oauth2 ]"))
+  }
 
-    test("Method securedBy test003") {
-        this.runTest("security/test007.raml",
-            Set("obasic","oauth2","null"))
-    }
+  test("Method securedBy test003") {
+    this.runTest("security/test007.raml", Set("obasic", "oauth2", "null"))
+  }
 
-    test("Method securedBy test004") {
-        this.runTest("security/test008.raml",
-            Set("obasic","oauth2"))
-    }
+  test("Method securedBy test004") {
+    this.runTest("security/test008.raml", Set("obasic", "oauth2"))
+  }
 
-    test("Method securedBy test005") {
-        this.runTest("security/test009.raml",
-            Set("null","oauth2"))
-    }
+  test("Method securedBy test005") {
+    this.runTest("security/test009.raml", Set("null", "oauth2"))
+  }
 
-    test("Method securedBy test006") {
-        this.runTest("security/test010.raml",
-            Set("obasic","oauth2"))
-    }
+  test("Method securedBy test006") {
+    this.runTest("security/test010.raml", Set("obasic", "oauth2"))
+  }
+
+  test("Method response codes") {
+    this.runTest("security/test023.raml", RamlResponseCodes.all.map(_ + ":\n          ").toSet)
+  }
 
 }
