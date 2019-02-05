c81e5b192e17291f3456a174fb2b29b4b4796f24
llibarona <leandro.libarona@mulesoft.com>
2019-02-05 12:26:17 -0300

    Fix of various None.get exceptions (unsynchronized Future in UniverseProvider)

--- als-hl/shared/src/main/scala/org/mulesoft/high/level/amfmanager/AmfInitializationHandler.scala
+++ als-hl/shared/src/main/scala/org/mulesoft/high/level/amfmanager/AmfInitializationHandler.scala
@@ -2,30 +2,29 @@ package org.mulesoft.high.level.amfmanager
 
 import amf.core.AMF
 import amf.plugins.document.vocabularies.AMLPlugin
-import amf.plugins.document.webapi.{Oas20Plugin, Oas30Plugin, Raml08Plugin, Raml10Plugin}
 import amf.plugins.document.webapi.validation.PayloadValidatorPlugin
+import amf.plugins.document.webapi.{Oas20Plugin, Oas30Plugin, Raml08Plugin, Raml10Plugin}
 import amf.plugins.features.validation.AMFValidatorPlugin
 
 import scala.concurrent.Future
-
 object AmfInitializationHandler {
 
-  private var initialized = false
+  private var initialization: Option[Future[Unit]] = None
 
   def init(): Future[Unit] = synchronized {
-    if (initialized) Future.successful(Unit)
-    else {
-      initialized = true
-      amf.core.AMF.registerPlugin(AMLPlugin)
-      amf.core.AMF.registerPlugin(Raml10Plugin)
-      amf.core.AMF.registerPlugin(Raml08Plugin)
-      amf.core.AMF.registerPlugin(Oas20Plugin)
-      amf.core.AMF.registerPlugin(Oas30Plugin)
-      amf.core.AMF.registerPlugin(AMFValidatorPlugin)
-      amf.core.AMF.registerPlugin(PayloadValidatorPlugin)
-      AMF
-        .init()
+    initialization match {
+      case Some(f) => f
+      case _ =>
+        amf.core.AMF.registerPlugin(AMLPlugin)
+        amf.core.AMF.registerPlugin(Raml10Plugin)
+        amf.core.AMF.registerPlugin(Raml08Plugin)
+        amf.core.AMF.registerPlugin(Oas20Plugin)
+        amf.core.AMF.registerPlugin(Oas30Plugin)
+        amf.core.AMF.registerPlugin(AMFValidatorPlugin)
+        amf.core.AMF.registerPlugin(PayloadValidatorPlugin)
+        val f = AMF.init()
+        initialization = Some(f)
+        f
     }
-
   }
 }
