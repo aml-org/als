721c285e76cc93fb7c957e582b207bfc5f3448ac
llibarona <leandro.libarona@mulesoft.com>
2019-03-13 18:01:15 -0300

    ALS-739: Fix on new Completion messages (including range)

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/client/Suggestion.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/client/Suggestion.scala
@@ -1,7 +1,14 @@
 package org.mulesoft.als.suggestions.client
 
+import common.dtoTypes.PositionRange
+
 import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
 
 @JSExportAll
 @JSExportTopLevel(name = "Suggestion")
-class Suggestion(val text: String, val description: String, val displayText: String, val prefix: String, val category: String) {}
+class Suggestion(val text: String,
+                 val description: String,
+                 val displayText: String,
+                 val prefix: String,
+                 val category: String,
+                 val range: Option[PositionRange]) {}
