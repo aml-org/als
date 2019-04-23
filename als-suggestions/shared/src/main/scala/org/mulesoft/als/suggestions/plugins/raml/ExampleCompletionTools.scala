d29005a948f4d745f7a6458a62ed9bd66ee3e043
llibarona <leandro.libarona@mulesoft.com>
2019-04-23 18:07:18 -0300

    WIP

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/raml/ExampleCompletionTools.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/raml/ExampleCompletionTools.scala
@@ -6,7 +6,9 @@ trait ExampleCompletionTools {
   protected def isExample(request: ICompletionRequest): Boolean = {
     request.astNode match {
       case Some(node) =>
-        if (node.isElement) node.asElement.get.definition.nameId.contains("ExampleSpec")
+        if (node.isElement)
+          node.asElement.get.definition.nameId.contains("ExampleSpec") ||
+          node.asElement.get.definition.nameId.contains("ExampleSpecFragment")
         else
           node.parent.exists(p =>
             p.definition.nameId.contains("ExampleSpec") || p.definition.nameId.contains("ExampleSpecFragment"))
