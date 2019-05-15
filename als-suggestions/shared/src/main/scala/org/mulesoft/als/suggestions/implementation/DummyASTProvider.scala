9b91c0cbb7d28298b0616d07e0f748206a4ba11d
llibarona <leandro.libarona@mulesoft.com>
2019-05-15 17:22:37 -0300

    Changes for NamedExamples in AMF 3.2.1

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/implementation/DummyASTProvider.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/implementation/DummyASTProvider.scala
@@ -5,21 +5,19 @@ import org.mulesoft.als.suggestions.interfaces.{IASTProvider, Syntax}
 import org.mulesoft.high.level.interfaces.{IHighLevelNode, IParseResult}
 import org.mulesoft.high.level.interfaces.IProject
 
-class DummyASTProvider(project: IProject, position:Int) extends IASTProvider{
+class DummyASTProvider(project: IProject, position: Int) extends IASTProvider {
 
-    override def getASTRoot: IHighLevelNode = project.rootASTUnit.rootNode
+  override def getASTRoot: IHighLevelNode = project.rootASTUnit.rootNode
 
-    override def getSelectedNode: Option[IParseResult] = getASTRoot.getNodeByPosition(position)
+  override def getSelectedNode: Option[IParseResult] = getASTRoot.getNodeByPosition(position)
 
-    override def language: Vendor = project.language
+  override def language: Vendor = project.language
 
-    override def syntax: Syntax = {
-        val node = getSelectedNode.getOrElse(getASTRoot)
-        if (node.astUnit.text.trim.startsWith("{")) {
-            Syntax.JSON
-        }
-        else {
-            Syntax.YAML
-        }
-    }
+  override def syntax: Syntax = {
+    val node = getSelectedNode.getOrElse(getASTRoot)
+    if (node.astUnit.text.trim.startsWith("{"))
+      Syntax.JSON
+    else
+      Syntax.YAML
+  }
 }
