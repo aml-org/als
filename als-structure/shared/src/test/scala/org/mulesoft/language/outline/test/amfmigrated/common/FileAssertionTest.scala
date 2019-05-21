763d33778360b07950be9fd1a8276ffcdf3c2e1f
llibarona <leandro.libarona@mulesoft.com>
2019-05-21 09:40:52 -0300

    Update FileAssertionTest.scala
    fix in structure tests for Windows

--- als-structure/shared/src/test/scala/org/mulesoft/language/outline/test/amfmigrated/common/FileAssertionTest.scala
+++ als-structure/shared/src/test/scala/org/mulesoft/language/outline/test/amfmigrated/common/FileAssertionTest.scala
@@ -14,7 +14,7 @@ trait FileAssertionTest extends PlatformSecrets {
 
   protected def writeTemporaryFile(golden: String)(content: String): Future[AsyncFile] = {
     val file   = tmp(s"${golden.replaceAll("/", "-")}.tmp")
-    val actual = fs.asyncFile(platform.resolvePath(file))
+    val actual = fs.asyncFile(file)
     actual.write(content).map(_ => actual)
   }
 
