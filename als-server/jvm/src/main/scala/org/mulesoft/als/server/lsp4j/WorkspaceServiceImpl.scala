f6aafd5cd44141ef1c6b7e06a97529d2e17231e3
llibarona <leandro.libarona@mulesoft.com>
2019-06-21 16:49:53 -0300

    ALS-708: Added ExecuteCommand for focus change (just JVM)

--- als-server/jvm/src/main/scala/org/mulesoft/als/server/lsp4j/WorkspaceServiceImpl.scala
+++ als-server/jvm/src/main/scala/org/mulesoft/als/server/lsp4j/WorkspaceServiceImpl.scala
@@ -1,10 +1,25 @@
 package org.mulesoft.als.server.lsp4j
 
-import org.eclipse.lsp4j.{DidChangeConfigurationParams, DidChangeWatchedFilesParams}
-import org.eclipse.lsp4j.services.WorkspaceService
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletableFuture.completedFuture
 
-class WorkspaceServiceImpl extends WorkspaceService {
+import org.eclipse.lsp4j.services.WorkspaceService
+import org.eclipse.lsp4j.{DidChangeConfigurationParams, DidChangeWatchedFilesParams, ExecuteCommandParams}
+import org.mulesoft.als.server.custom.DidFocusCommand
+import org.mulesoft.lsp.server
+
+class WorkspaceServiceImpl(private val inner: server.LanguageServer) extends WorkspaceService with DidFocusCommand {
+  private val textDocumentSyncConsumer = inner.textDocumentSyncConsumer
+
   override def didChangeConfiguration(params: DidChangeConfigurationParams): Unit = {}
 
   override def didChangeWatchedFiles(params: DidChangeWatchedFilesParams): Unit = {}
+
+  override def executeCommand(params: ExecuteCommandParams): CompletableFuture[AnyRef] = {
+
+    params.getCommand match {
+      case DID_FOCUS_CHANGE_COMMAND => onDidFocus(params.getArguments, textDocumentSyncConsumer.didFocus)
+      case _                        => completedFuture("Command not recognized")
+    }
+  }
 }
