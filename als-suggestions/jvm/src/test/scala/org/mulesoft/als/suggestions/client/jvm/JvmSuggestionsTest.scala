6e6831dce56b6e661ccbb083762f27d6314d2a79
llibarona <leandro.libarona@mulesoft.com>
2019-07-01 09:53:49 -0300

    Added test for ResourceLoader in Java

---
+++ als-suggestions/jvm/src/test/scala/org/mulesoft/als/suggestions/client/jvm/JvmSuggestionsTest.scala
@@ -0,0 +1,58 @@
+package org.mulesoft.als.suggestions.client.jvm
+
+import amf.client.remote.Content
+import amf.core.remote.{FileNotFound, Raml10}
+import amf.core.unsafe.PlatformSecrets
+import amf.internal.resource.ResourceLoader
+import org.mulesoft.als.common.DirectoryResolver
+import org.mulesoft.als.suggestions.client.Suggestions
+import org.mulesoft.high.level.InitOptions
+import org.scalatest.{AsyncFunSuite, Matchers}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class JvmSuggestionsTest extends AsyncFunSuite with Matchers with PlatformSecrets {
+  override implicit val executionContext: ExecutionContext =
+    scala.concurrent.ExecutionContext.Implicits.global
+  // TODO: AMF is having trouble with "file:/" prefix (it transforms it in "file:///")
+//  val url = "file:/Users/mdeachaval/runtime-studio.product/apiwithdependencies/capurro/apiwithdependencies.raml"
+  val url = "file:///absolute/path/api.raml"
+
+  val textInput: String =
+    """#%RAML 1.0
+      |title: ApiWithDependencies10
+      |""".stripMargin
+
+  val fileLoader: ResourceLoader = new ResourceLoader {
+    override def accepts(resource: String): Boolean = resource == url
+
+    override def fetch(resource: String): Future[Content] =
+      Future[Content]({
+        try { new Content(textInput, resource) } catch {
+          case e: Exception => throw FileNotFound(e)
+        }
+      })
+  }
+
+  val directoryResolver: DirectoryResolver = new DirectoryResolver {
+    override def exists(path: String): Future[Boolean] = Future { path == url }
+
+    override def readDir(path: String): Future[Seq[String]] = Future { Seq() }
+
+    override def isDirectory(path: String): Future[Boolean] = Future { false }
+  }
+
+  import amf.internal.environment.Environment
+
+  val environment
+    : Environment = Environment().add(fileLoader) // .add(new ResourceLoaderAdapter(new FileResourceLoader()))
+
+  test("Custom Resource Loader test") {
+    for {
+      _           <- Suggestions.init(InitOptions.AllProfiles)
+      suggestions <- Suggestions.suggest(Raml10.name, url, 40, directoryResolver, environment, platform)
+    } yield {
+      assert(suggestions.size == 15)
+    }
+  }
+}
