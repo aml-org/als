cb8caa41d67acf946a6d3eab381374598bdc8823
llibarona <leandro.libarona@mulesoft.com>
2019-07-05 12:24:43 -0300

    completion plugins: enums (arrays) and cleanup

--- als-common/shared/src/main/scala/org/mulesoft/als/common/YamlUtils.scala
+++ als-common/shared/src/main/scala/org/mulesoft/als/common/YamlUtils.scala
@@ -2,29 +2,42 @@ package org.mulesoft.als.common
 
 import org.mulesoft.als.common.dtoTypes.{Position, PositionRange}
 import org.mulesoft.lexer.InputRange
-import org.yaml.model.{YMap, YMapEntry, YNode, YNonContent, YPart, YSequence}
+import org.yaml.model._
 
 import scala.annotation.tailrec
 
 object YamlUtils {
-  def isArray(ast: Option[YPart], amfPosition: Position): Boolean =
-    ast.exists(getNodeByPosition(_, amfPosition).isInstanceOf[YSequence])
+  def isArray(selectedNode: Option[YPart], amfPosition: Position): Boolean =
+    selectedNode.exists(_.isInstanceOf[YSequence])
 
-  def isKey(maybePart: Option[YPart], amfPosition: Position): Boolean =
-    maybePart.exists(part => {
-      val selected = getParent(part, amfPosition, None)
-      selected match {
-        case Some(entry: YMapEntry) =>
-          contains(entry.key.range, amfPosition)
-        case _ => false
+  def isInArray(parents: Seq[YPart], amfPosition: Position): Boolean = {
+    @tailrec
+    def inner(parents: Seq[YPart]): Boolean =
+      parents.headOption match {
+        case Some(_: YSequence) => true
+        case Some(_: YMapEntry) => false
+        case Some(_)            => inner(parents.tail)
+        case _                  => false
       }
-    })
+    parents.headOption match {
+      case Some(parent: YMapEntry) =>
+        contains(parent.key.range, amfPosition) && inner(parents.tail)
+      case _ => inner(parents)
+    }
+  }
+
+  def isKey(parent: Option[YPart], amfPosition: Position): Boolean =
+    parent match {
+      case Some(entry: YMapEntry) =>
+        contains(entry.key.range, amfPosition)
+      case _ => false
+    }
 
   def contains(range: InputRange, amfPosition: Position): Boolean =
     PositionRange(Position(range.lineFrom, range.columnFrom), Position(range.lineTo, range.columnTo))
       .contains(amfPosition)
 
-  def childWithPosition(s: YPart, amfPosition: Position): Option[YPart] =
+  private def childWithPosition(s: YPart, amfPosition: Position): Option[YPart] =
     s.children
       .filterNot(_.isInstanceOf[YNonContent])
       .filter {
@@ -46,32 +59,16 @@ object YamlUtils {
     }
 
   def getNodeBrothers(s: YPart, amfPosition: Position): Seq[YPart] =
-    getParent(s, amfPosition, Some(s)).flatMap(getParent(s, _, None)).map(_.children).getOrElse(Nil)
+    getParents(s, amfPosition, Seq()).tail.headOption
+      .map(_.children)
+      .getOrElse(Nil)
 
   @tailrec
-  final def getParent(s: YPart, amfPosition: Position, parent: Option[YPart]): Option[YPart] =
+  final def getParents(s: YPart, amfPosition: Position, parents: Seq[YPart]): Seq[YPart] =
     childWithPosition(s, amfPosition) match {
       case Some(c) =>
-        getParent(c, amfPosition, Some(s))
+        getParents(c, amfPosition, s +: parents)
       case None =>
-        parent
-    }
-
-  def getChildWithNode(children: IndexedSeq[YPart], node: YPart): Option[YPart] =
-    children.find(_ == node) match {
-      case Some(p) => Some(p)
-      case _ =>
-        children.find(c => {
-          getChildWithNode(c.children, node).isDefined
-        })
-    }
-
-  @tailrec
-  final def getParent(s: YPart, child: YPart, parent: Option[YPart] = None): Option[YPart] =
-    getChildWithNode(s.children, child) match {
-      case Some(c) =>
-        getParent(c, child, Some(s))
-      case None =>
-        parent
+        parents
     }
 }
