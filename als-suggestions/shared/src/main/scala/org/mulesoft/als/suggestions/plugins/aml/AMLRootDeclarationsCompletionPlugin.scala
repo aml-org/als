4aa6cd7d35b2e2b48fe1f77e26b8ec4156c2366d
llibarona <leandro.libarona@mulesoft.com>
2019-07-11 16:05:00 -0300

    ALS-805: root structure for declarations

---
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/AMLRootDeclarationsCompletionPlugin.scala
@@ -0,0 +1,81 @@
+package org.mulesoft.als.suggestions.plugins.aml
+
+import amf.core.annotations.SourceAST
+import amf.core.model.document.Document
+import amf.plugins.document.vocabularies.model.domain.{PropertyMapping, PublicNodeMapping}
+import org.mulesoft.als.common.YamlUtils.getParents
+import org.mulesoft.als.common.{AmfUtils, YamlUtils}
+import org.mulesoft.als.suggestions.interfaces.{CompletionParams, CompletionPlugin, RawSuggestion}
+import org.mulesoft.lsp.edit.TextEdit
+import org.yaml.model.YMapEntry
+
+import scala.concurrent.Future
+
+class AMLRootDeclarationsCompletionPlugin(params: CompletionParams, brothers: Set[String])
+    extends AMLSuggestionsHelper {
+
+  def extractText(mapping: PublicNodeMapping): (String, String) =
+    if (mapping.mappedNode().isNullOrEmpty) (s"${mapping.name().value()}", ": ")
+    else (s"${mapping.name().value()}", ":\n  ")
+
+  private def getSuggestions: Seq[(String, String)] =
+    params.actualDialect
+      .documents()
+      .root()
+      .declaredNodes()
+      .map(extractText)
+
+  def resolve(): Future[Seq[RawSuggestion]] =
+    Future.successful(
+      getSuggestions
+        .filter(tuple => !brothers.contains(tuple._1)) // TODO: extract filter for all plugins?
+        .map(s =>
+          new RawSuggestion {
+            override def newText: String = s._1
+
+            override def displayText: String = s._1
+
+            override def description: String = s._1
+
+            override def textEdits: Seq[TextEdit] = Seq()
+
+            override def whiteSpacesEnding: String = s._2
+        }))
+}
+
+object AMLRootDeclarationsCompletionPlugin extends CompletionPlugin {
+  override def id = "AMLRootDeclarationsCompletionPlugin"
+
+  override def resolve(params: CompletionParams): Future[Seq[RawSuggestion]] = {
+    val ast = params.currentBaseUnit match {
+      case d: Document =>
+        d.encodes.annotations.find(classOf[SourceAST]).map(_.ast)
+      case bu => bu.annotations.find(classOf[SourceAST]).map(_.ast)
+    }
+    val amfPosition = params.position.moveLine(1)
+
+    ast
+      .map(a => {
+        val parents = getParents(a, amfPosition, Seq())
+        if (parents.length <= 2 && // entry and root
+            YamlUtils
+              .isKey(parents.headOption, amfPosition) && !params.fieldEntry
+              .exists(_.value.value
+                .position()
+                .exists(li => AmfUtils.containsPosition(li, amfPosition, None))))
+          new AMLRootDeclarationsCompletionPlugin(
+            params,
+            ast
+              .map(yaml =>
+                YamlUtils.getNodeBrothers(yaml, amfPosition).flatMap {
+                  case yme: YMapEntry => yme.key.asScalar.map(_.text)
+                  case _              => None
+              })
+              .getOrElse(Nil)
+              .toSet
+          ).resolve()
+        else Future.successful(Seq())
+      })
+      .getOrElse(Future.successful(Seq()))
+  }
+}
