61e7ebf37801964e702b8f5572a930d211c53142
llibarona <leandro.libarona@mulesoft.com>
2019-07-16 17:55:36 -0300

    Additional KnownValues (ResponseCodes and MediaTypes for OAS)

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/patched/PatchedSuggestions.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/patched/PatchedSuggestions.scala
@@ -1,34 +1,37 @@
 package org.mulesoft.als.suggestions.plugins.aml.patched
 
-case class PatchedSuggestion(text: String, description: Option[String] = None)
+import org.mulesoft.typesystem.definition.system.{OasCommonMediaTypes, OasResponseCodes}
 
-case class PatchesForField(propertyTerm: String, suggestions: Set[PatchedSuggestion])
+case class PatchedSuggestion(text: String, description: Option[String] = None)
 
 case class FieldForClass(classTerm: String, propertyTerm: String)
 
 object PatchedSuggestionsForDialect {
 
-  private val classes: Map[FieldForClass, Map[String, Set[PatchedSuggestion]]] =
+  private val classes: Map[FieldForClass, Map[String, Seq[PatchedSuggestion]]] =
     Map(
-      FieldForClass("https://github.amlorg.com/visit#MeetingNode", "https://github.amlorg.com/visit#duration") ->
-        Map("KnownValues" -> Set(PatchedSuggestion("1"), PatchedSuggestion("2"), PatchedSuggestion("3"))))
+      FieldForClass("http://a.ml/vocabularies/http#Response", "http://schema.org/name") ->
+        Map("KnownValues" -> OasResponseCodes.all.map(PatchedSuggestion(_))),
+      FieldForClass("http://schema.org/WebAPI", "http://a.ml/vocabularies/http#contentType") ->
+        Map("KnownValues" -> OasCommonMediaTypes.all.map(PatchedSuggestion(_)))
+    )
 
-  def getKnownValues(classTerm: String, propertyTerm: String): Set[PatchedSuggestion] =
+  def getKnownValues(classTerm: String, propertyTerm: String): Seq[PatchedSuggestion] =
     getPatchedValues(classTerm, propertyTerm, Some("KnownValues"))
 
   private def getPatchedValues(classTerm: String,
                                propertyTerm: String,
-                               patchType: Option[String]): Set[PatchedSuggestion] =
+                               patchType: Option[String]): Seq[PatchedSuggestion] =
     patchType match {
       case Some(t) =>
         classes
           .getOrElse(FieldForClass(classTerm, propertyTerm), Map.empty)
-          .getOrElse(t, Set())
+          .getOrElse(t, Nil)
       case _ =>
         classes
           .getOrElse(FieldForClass(classTerm, propertyTerm), Map.empty)
           .flatMap(_._2)
-          .toSet
+          .toSeq
     }
 
 }
