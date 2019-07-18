e174345e344a05be1abd583875c3f4f9f0e49e15
Hernan Najles <hernan.naj@gmail.com>
2019-07-18 10:35:23 -0300

    Rebase devel

--- als-common/shared/src/main/scala/org/mulesoft/lsp/feature/codeactions/CodeActionCapabilities.scala
+++ als-common/shared/src/main/scala/org/mulesoft/lsp/feature/codeactions/CodeActionCapabilities.scala
@@ -1,4 +1,3 @@
 package org.mulesoft.lsp.feature.codeactions
 
 case class CodeActionCapabilities(dynamicRegistration: Option[Boolean], codeActionLiteralSupport: CodeActionLiteralSupportCapabilities)
-
