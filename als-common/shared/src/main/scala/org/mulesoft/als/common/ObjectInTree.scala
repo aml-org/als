52afbc3c8970bfee700a1398fa09e39896b6ae55
Hernan Najles <hernan.naj@gmail.com>
2019-07-27 18:04:32 -0300

    ALS-818 parameters suggestion plugin and related ALS-825 raml types facets completion plugin

---
+++ als-common/shared/src/main/scala/org/mulesoft/als/common/ObjectInTree.scala
@@ -0,0 +1,18 @@
+package org.mulesoft.als.common
+
+import amf.core.model.document.BaseUnit
+import amf.core.model.domain.AmfObject
+import org.mulesoft.als.common.dtoTypes.Position
+import AmfSonElementFinder._
+import amf.core.metamodel.document.BaseUnitModel
+import amf.core.parser.FieldEntry
+
+case class ObjectInTree(obj: AmfObject, stack: Seq[AmfObject])
+
+object ObjectInTreeBuilder {
+
+  def fromUnit(bu: BaseUnit, position: Position): ObjectInTree = {
+    val (obj, stack) = bu.findSonWithStack(position, Seq((f: FieldEntry) => f.field != BaseUnitModel.References))
+    ObjectInTree(obj, stack)
+  }
+}
