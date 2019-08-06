d2b1b751fdee2434b224a380e405a5f00d6e757b
Hernan Najles <hernan.naj@gmail.com>
2019-08-06 16:03:54 -0300

    ALS-822 add raml custom annotation reference plugin

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/webapi/RamlCompletionPluginRegistry.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/webapi/RamlCompletionPluginRegistry.scala
@@ -1,12 +1,7 @@
 package org.mulesoft.als.suggestions.aml.webapi
 
 import amf.dialects.RAML10Dialect
-import org.mulesoft.als.suggestions.plugins.aml.webapi.raml.{
-  RamlCustomFacetsCompletionPlugin,
-  RamlParamsCompletionPlugin,
-  RamlTypeDeclarationReferenceCompletionPlugin,
-  RamlTypeFacetsCompletionPlugin
-}
+import org.mulesoft.als.suggestions.plugins.aml.webapi.raml._
 import org.mulesoft.als.suggestions.{AMLBaseCompletionPlugins, CompletionsPluginHandler}
 
 object RamlCompletionPluginRegistry {
@@ -16,7 +11,8 @@ object RamlCompletionPluginRegistry {
       RamlParamsCompletionPlugin :+
       RamlTypeFacetsCompletionPlugin :+
       RamlTypeDeclarationReferenceCompletionPlugin :+
-      RamlCustomFacetsCompletionPlugin
+      RamlCustomFacetsCompletionPlugin :+
+      AnnotationReferenceCompletionPlugin
 
   def init(): Unit =
     CompletionsPluginHandler.registerPlugins(all, RAML10Dialect().id)
