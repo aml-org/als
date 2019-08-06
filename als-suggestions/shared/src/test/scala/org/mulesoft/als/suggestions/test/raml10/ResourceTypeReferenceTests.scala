692baeca464b8c6bfd328c51a1db8be4f3372910
Hernan Najles <hernan.naj@gmail.com>
2019-08-06 20:25:52 -0300

    ALS-820 add resource type reference and variable completion plugin

--- als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/raml10/ResourceTypeReferenceTests.scala
+++ als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/raml10/ResourceTypeReferenceTests.scala
@@ -10,43 +10,33 @@ class ResourceTypeReferenceTests extends RAML10Test {
     this.runSuggestionTest("resourceTypeReferences/test002.raml", Set("resourceType1: ", "resourceType2: "))
   }
 
+  test("test007") {
+    this.runTest("resourceTypeReferences/test007.raml", Set("resourceType1", "resourceType2"))
+  }
+
   test("test003") { // TODO: No multilines are allowed in completions. Replace for a snippet?
     this.runSuggestionTest(
       "resourceTypeReferences/test003.raml",
-      Set(
-        """
-            |    resourceType1:
-            |      param1:
-            |      param2: """.stripMargin,
-        """
-            |    resourceType2:
-            |      param1:
-            |      param2: """.stripMargin
-      )
+      Set("param1: ", "param2: ")
     )
   }
 
-  test("test004") { // TODO: No multilines are allowed in completions. Replace for a snippet?
+  test("test004") {
     this.runSuggestionTest(
       "resourceTypeReferences/test004.raml",
-      Set(
-        """resourceType1:
-            |      param1:
-            |      param2: """.stripMargin,
-        """resourceType2:
-            |      param1:
-            |      param2: """.stripMargin
-      )
+      Set("param1: ", "param2: ")
     )
   }
 
   test("test005") {
-    this.runSuggestionTest(
-      "resourceTypeReferences/test005.raml",
-      Set("{ resourceType1: {  param1 : ,  param2 : } }", "{ resourceType2: {  param1 : ,  param2 : } }"))
+    this.runSuggestionTest("resourceTypeReferences/test005.raml", Set.empty)
+  }
+
+  test("test008") {
+    this.runSuggestionTest("resourceTypeReferences/test008.raml", Set.empty) // what should do in this cases?
   }
 
   test("test006") {
-    this.runSuggestionTest("resourceTypeReferences/test006.raml", Set("searchableColl"))
+    this.runSuggestionTest("resourceTypeReferences/test006.raml", Set("searchableColl: "))
   }
 }
