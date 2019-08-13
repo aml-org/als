f9e79903430b8fa0cfc04c9f3314c2cdbd9e9b09
Hernan Najles <hernan.naj@gmail.com>
2019-08-13 12:36:37 -0300

    Fix raml types tests

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/package.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/package.scala
@@ -9,7 +9,7 @@ package object aml {
 
     def propertiesRaw(identation: String): Seq[RawSuggestion] = {
       nodeMapping.propertiesMapping().map { p =>
-        if (p.objectRange().nonEmpty || p.enum().nonEmpty)
+        if (p.objectRange().nonEmpty)
           RawSuggestion(p.name().value(), identation, isAKey = true)
         else RawSuggestion.forKey(p.name().value())
       }
