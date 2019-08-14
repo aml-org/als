4c23db0a4570907d3f1c8123c906c617394b871b
Hernan Najles <hernan.naj@gmail.com>
2019-08-14 09:58:01 -0300

    Fix security schemes tests

---
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/raml/SecuritySchemeStructureCompletionPlugin.scala
@@ -0,0 +1,34 @@
+package org.mulesoft.als.suggestions.plugins.aml.webapi.raml
+
+import amf.plugins.domain.webapi.models.security.SecurityScheme
+import org.mulesoft.als.suggestions.RawSuggestion
+import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
+import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object SecuritySchemeStructureCompletionPlugin extends AMLCompletionPlugin {
+  override def id: String = "SecuritySchemeStructureCompletionPlugin"
+
+  override def resolve(request: AmlCompletionRequest): Future[Seq[RawSuggestion]] = {
+    Future {
+      request.amfObject match {
+        case s: SecurityScheme if request.yPartBranch.isKeyDescendanceOf("describedBy") =>
+          SecuritySchemesDialect.DescribedBy.propertiesRaw(request.indentation)
+        case s: SecurityScheme if request.fieldEntry.isEmpty && request.yPartBranch.isKey =>
+          val suggestions =
+            new AMLStructureCompletionsPlugin(SecuritySchemesDialect.SecurityScheme.propertiesMapping(),
+                                              request.indentation).resolve()
+          if (s.`type`
+                .option()
+                .exists(t => Seq("OAuth 1.0", "OAuth 2.0").contains(t)))
+            suggestions :+ RawSuggestion("settings", request.indentation, isAKey = true)
+          else suggestions
+
+        case _ => Nil
+      }
+    }
+  }
+}
