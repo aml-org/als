8d9db8cc144f65803d1c3b835bbdd555516d30bc
Hernan Najles <hernan.naj@gmail.com>
2019-08-19 16:47:47 -0300

    Fix raml media types suggestions

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/patched/PatchedSuggestions.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/patched/PatchedSuggestions.scala
@@ -1,7 +1,9 @@
 package org.mulesoft.als.suggestions.plugins.aml.patched
 
+import amf.dialects.OAS20Dialect
 import amf.plugins.domain.webapi.metamodel.{PayloadModel, ResponseModel, WebApiModel}
-import org.mulesoft.typesystem.definition.system.{OasCommonMediaTypes, OasResponseCodes}
+import org.mulesoft.als.suggestions.plugins.aml.webapi.raml.Raml10TypesDialect
+import org.mulesoft.typesystem.definition.system.{OasCommonMediaTypes, OasResponseCodes, RamlResponseCodes}
 
 case class PatchedSuggestion(text: String, description: Option[String] = None)
 
@@ -23,19 +25,47 @@ object PatchedSuggestionsForDialect {
         Map("KnownValues" -> OasCommonMediaTypes.all.map(PatchedSuggestion(_)))
     )
 
-  def getKnownValues(classTerm: String, propertyTerm: String): Seq[PatchedSuggestion] =
-    getPatchedValues(classTerm, propertyTerm, Some("KnownValues"))
+  private val commonClasses: Map[FieldForClass, Map[String, Seq[PatchedSuggestion]]] =
+    Map(
+      FieldForClass(ResponseModel.`type`.head.iri(), ResponseModel.Name.value.iri()) ->
+        Map("KnownValues" -> OasResponseCodes.all.map(PatchedSuggestion(_))),
+      FieldForClass(WebApiModel.`type`.head.iri(), WebApiModel.Accepts.value.iri()) ->
+        Map("KnownValues" -> OasCommonMediaTypes.all.map(PatchedSuggestion(_))),
+      FieldForClass(WebApiModel.`type`.head.iri(), WebApiModel.ContentType.value.iri()) ->
+        Map("KnownValues" -> OasCommonMediaTypes.all.map(PatchedSuggestion(_))),
+      FieldForClass(PayloadModel.`type`.head.iri(), PayloadModel.MediaType.value.iri()) ->
+        Map("KnownValues" -> OasCommonMediaTypes.all.map(PatchedSuggestion(_)))
+    )
 
-  private def getPatchedValues(classTerm: String,
+  private val oas20Classes: Map[FieldForClass, Map[String, Seq[PatchedSuggestion]]] =
+    commonClasses ++ Map(
+      FieldForClass(ResponseModel.`type`.head.iri(), ResponseModel.StatusCode.value.iri()) ->
+        Map("KnownValues" -> OasResponseCodes.all.map(PatchedSuggestion(_))))
+
+  private val raml10Classes: Map[FieldForClass, Map[String, Seq[PatchedSuggestion]]] =
+    commonClasses ++ Map(
+      FieldForClass(ResponseModel.`type`.head.iri(), ResponseModel.StatusCode.value.iri()) ->
+        Map("KnownValues" -> RamlResponseCodes.all.map(PatchedSuggestion(_))))
+
+  private val classesByDialect: Map[String, Map[FieldForClass, Map[String, Seq[PatchedSuggestion]]]] =
+    Map(Raml10TypesDialect.dialect.id -> raml10Classes, OAS20Dialect.dialect.id -> oas20Classes)
+
+  def getKnownValues(dialect: String, classTerm: String, propertyTerm: String): Seq[PatchedSuggestion] =
+    getPatchedValues(dialect, classTerm, propertyTerm, Some("KnownValues"))
+
+  private def getPatchedValues(dialect: String,
+                               classTerm: String,
                                propertyTerm: String,
                                patchType: Option[String]): Seq[PatchedSuggestion] =
     patchType match {
       case Some(t) =>
-        classes
+        classesByDialect
+          .getOrElse(dialect, Map.empty)
           .getOrElse(FieldForClass(classTerm, propertyTerm), Map.empty)
           .getOrElse(t, Nil)
       case _ =>
-        classes
+        classesByDialect
+          .getOrElse(dialect, Map.empty)
           .getOrElse(FieldForClass(classTerm, propertyTerm), Map.empty)
           .flatMap(_._2)
           .toSeq
