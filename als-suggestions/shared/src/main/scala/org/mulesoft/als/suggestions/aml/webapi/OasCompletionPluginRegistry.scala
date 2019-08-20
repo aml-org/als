6e8ee8cee877ac2302b25379cd28361009ce33fb
Hernan Najles <hernan.naj@gmail.com>
2019-08-20 17:29:19 -0300

    Add oas types values completion plugin

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/webapi/OasCompletionPluginRegistry.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/webapi/OasCompletionPluginRegistry.scala
@@ -3,13 +3,20 @@ package org.mulesoft.als.suggestions.aml.webapi
 import amf.dialects.OAS20Dialect
 import org.mulesoft.als.suggestions.plugins.aml.webapi.oas.{
   OASRequiredObjectCompletionPlugin,
-  OasInBodyParametersCompletionPlugin
+  OasCommonTypes,
+  OasInBodyParametersCompletionPlugin,
+  OasTypeDeclarationReferenceCompletionPlugin
 }
 import org.mulesoft.als.suggestions.{AMLBaseCompletionPlugins, CompletionsPluginHandler}
 
 object OasCompletionPluginRegistry {
 
-  private val all = AMLBaseCompletionPlugins.all :+ OASRequiredObjectCompletionPlugin :+ OasInBodyParametersCompletionPlugin
+  private val all = AMLBaseCompletionPlugins.all :+
+    OASRequiredObjectCompletionPlugin :+
+    OasInBodyParametersCompletionPlugin :+
+    OasTypeDeclarationReferenceCompletionPlugin :+
+    OasCommonTypes
 
-  def init(): Unit = CompletionsPluginHandler.registerPlugins(all, OAS20Dialect().id)
+  def init(): Unit =
+    CompletionsPluginHandler.registerPlugins(all, OAS20Dialect().id)
 }
