35e78a626f9c379830e8b29a299f7b2efc3e2702
Hernan Najles <hernan.naj@gmail.com>
2019-08-23 19:18:28 -0300

    Extract object node properties suggestions plugin for examples to web api in order to support oas examples properties

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/oas/OasStructurePlugin.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/oas/OasStructurePlugin.scala
@@ -1,15 +1,15 @@
 package org.mulesoft.als.suggestions.plugins.aml.webapi.oas
 
-import amf.core.annotations.DeclaredElement
 import amf.core.model.domain.Shape
 import amf.dialects.OAS20Dialect
-import amf.plugins.domain.webapi.models.{EndPoint, Parameter, Payload}
+import amf.plugins.domain.webapi.models.{EndPoint, Parameter, WebApi}
 import org.mulesoft.als.common.YPartBranch
 import org.mulesoft.als.suggestions.RawSuggestion
 import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
 import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
-import org.mulesoft.als.suggestions.plugins.aml.{AMLStructureCompletionPlugin, AMLStructureCompletionsPlugin}
+import org.mulesoft.als.suggestions.plugins.aml.{AMLStructureCompletionPlugin, _}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object OasStructurePlugin extends AMLCompletionPlugin {
@@ -19,9 +19,15 @@ object OasStructurePlugin extends AMLCompletionPlugin {
     request.amfObject match {
       case _: Parameter | _: Shape                           => emptySuggestion
       case _: EndPoint if isInParameter(request.yPartBranch) => emptySuggestion
-      case _                                                 => AMLStructureCompletionPlugin.resolve(request)
+      case _: WebApi if request.yPartBranch.isKeyDescendanceOf("info") =>
+        infoSuggestions(request.indentation)
+      case _ => AMLStructureCompletionPlugin.resolve(request)
     }
   }
 
-  def isInParameter(yPartBranch: YPartBranch): Boolean = yPartBranch.isKeyDescendanceOf("parameters")
+  def isInParameter(yPartBranch: YPartBranch): Boolean =
+    yPartBranch.isKeyDescendanceOf("parameters")
+
+  def infoSuggestions(indentation: String) =
+    Future(OAS20Dialect.DialectNodes.InfoObject.propertiesRaw(indentation, Some("docs")))
 }
