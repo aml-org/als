c5bdc4a46d6665ffdd6b84359cb7f44110c0f811
Hernan Najles <hernan.naj@gmail.com>
2019-08-23 21:25:59 -0300

    Finish oas suit test

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/raml/RamlNumberShapeFormatValues.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/raml/RamlNumberShapeFormatValues.scala
@@ -1,29 +1,10 @@
 package org.mulesoft.als.suggestions.plugins.aml.webapi.raml
 
-import amf.core.parser.FieldEntry
-import amf.plugins.domain.shapes.metamodel.ScalarShapeModel
-import org.mulesoft.als.suggestions.RawSuggestion
-import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
-import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import amf.plugins.document.vocabularies.model.domain.NodeMapping
+import org.mulesoft.als.suggestions.plugins.aml.webapi.ShapeNumberShapeFormatValues
 
-object RamlNumberShapeFormatValues extends AMLCompletionPlugin {
+object RamlNumberShapeFormatValues extends ShapeNumberShapeFormatValues {
   override def id: String = "RamlNumberShapeFormatValues"
 
-  override def resolve(request: AmlCompletionRequest): Future[Seq[RawSuggestion]] = {
-    Future {
-      request.fieldEntry match {
-        case Some(FieldEntry(ScalarShapeModel.Format, _)) if request.yPartBranch.isValue =>
-          val formats: Seq[String] = Raml10TypesDialect.NumberShapeNode
-            .propertiesMapping()
-            .find(_.name().option().contains("format"))
-            .map(p => p.enum().flatMap(v => v.option().map(_.toString)))
-            .getOrElse(Nil)
-          formats.map(RawSuggestion(_, isAKey = false))
-        case _ => Nil
-
-      }
-    }
-  }
+  override def numberNodeMapping(dataType: String): NodeMapping = Raml10TypesDialect.NumberShapeNode
 }
