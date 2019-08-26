aed620cc994c82b9d633c4d6e95f36a87436f4bd
Hernan Najles <hernan.naj@gmail.com>
2019-08-26 19:20:54 -0300

    Remove old raml 10 tests

--- als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/raml10/MultilineJsonTests.scala
+++
@@ -1,8 +0,0 @@
-package org.mulesoft.als.suggestions.test.raml10
-
-class MultilineJsonTests extends RAML10Test {
-
-  test("test01") {
-    this.runSuggestionTest("multiline-json/second-line-example.raml", Set())
-  }
-}
