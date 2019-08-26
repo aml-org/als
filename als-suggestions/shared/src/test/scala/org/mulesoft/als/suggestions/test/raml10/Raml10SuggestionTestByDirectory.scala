086a97481a82df240440f455a00c3cf3ca4a81ed
Hernan Najles <hernan.naj@gmail.com>
2019-08-26 19:20:54 -0300

    Remove old raml 10 tests

--- als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/raml10/Raml10SuggestionTestByDirectory.scala
+++ als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/raml10/Raml10SuggestionTestByDirectory.scala
@@ -4,7 +4,7 @@ import amf.core.remote.{Hint, RamlYamlHint}
 import org.mulesoft.als.suggestions.test.SuggestionByDirectoryTest
 
 class Raml10SuggestionTestByDirectory extends SuggestionByDirectoryTest {
-  override def basePath: String = "als-suggestions/shared/src/test/resources/test/raml10/new-suits"
+  override def basePath: String = "als-suggestions/shared/src/test/resources/test/raml10/by-directory"
 
   override def origin: Hint = RamlYamlHint
 
