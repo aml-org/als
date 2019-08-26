753d71d49ee280ed104b663072b08766e9a46326
Hernan Najles <hernan.naj@gmail.com>
2019-08-26 20:09:23 -0300

    Remove oas 2.0 old test

--- als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/oas20/TestOasResponseCodes.scala
+++
@@ -1,7 +0,0 @@
-package org.mulesoft.als.suggestions.test.oas20
-
-import org.mulesoft.typesystem.definition.system.OasResponseCodes
-
-object TestOasResponseCodes {
-  val all: Seq[String] = OasResponseCodes.all.map(v => v + ":\n          ")
-}
