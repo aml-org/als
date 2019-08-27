07bb64de01e95d7d9b1ec49986a51c194481c218
Hernan Najles <hernan.naj@gmail.com>
2019-08-27 10:50:50 -0300

    ALS-857 fix json patcher for key with empty value. Add content patcher test

---
+++ als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/ContentPatcherTest.scala
@@ -0,0 +1,32 @@
+package org.mulesoft.als.suggestions
+
+import common.diff.FileAssertionTest
+import org.mulesoft.als.suggestions.interfaces.Syntax
+import org.scalatest.AsyncFunSuite
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ContentPatcherTest extends AsyncFunSuite with FileAssertionTest {
+
+  override implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
+
+  val basePath = "file://als-suggestions/shared/src/test/resources/test/patcher"
+
+  test("Test in json key patch") {
+    val url      = basePath + "/in-key.json"
+    val expected = basePath + "/in-key.result.json"
+    for {
+      c <- platform.resolve(url)
+      patched <- Future {
+
+        val content    = c.stream.toString
+        val offset     = content.indexOf("*")
+        val rawContent = content.substring(0, offset) + content.substring(offset + 1)
+        org.mulesoft.als.suggestions.Core.prepareText(rawContent, offset, Syntax.JSON)
+      }
+      tmp <- writeTemporaryFile(expected)(patched)
+      r   <- assertDifferences(tmp, expected)
+    } yield r
+  }
+
+}
