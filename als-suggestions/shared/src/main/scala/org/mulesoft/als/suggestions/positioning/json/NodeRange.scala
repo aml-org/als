c108ceefc1872668bad94cc77f688e4729a891c7
llibarona <leandro.libarona@mulesoft.com>
2019-09-04 10:30:20 -0300

    Cleanup and coverage improvement

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/positioning/json/NodeRange.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/positioning/json/NodeRange.scala
@@ -9,9 +9,11 @@ trait NodeRange {
 
   def end: Point
 
-  def toPositionRange: PositionRange = PositionRange(start.toPosition, end.toPosition)
+  def toPositionRange: PositionRange =
+    PositionRange(start.toPosition, end.toPosition)
 
-  def containsPosition(pos: Int): Boolean = start.position <= pos && end.position > pos
+  def containsPosition(pos: Int): Boolean =
+    start.position <= pos && end.position > pos
 
   def containsPosition(position: Position): Boolean = {
     toPositionRange.contains(position)
