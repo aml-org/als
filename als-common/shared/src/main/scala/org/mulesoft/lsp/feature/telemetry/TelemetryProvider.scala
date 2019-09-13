69b149f59d57a7b9d5df6f149a13cb1f5e7d6d70
llibarona <leandro.libarona@mulesoft.com>
2019-09-13 11:31:03 -0300

    ALS-844: Added telemetry functionality for time logs

---
+++ als-common/shared/src/main/scala/org/mulesoft/lsp/feature/telemetry/TelemetryProvider.scala
@@ -0,0 +1,35 @@
+package org.mulesoft.lsp.feature.telemetry
+
+import org.mulesoft.lsp.feature.telemetry.MessageTypes.MessageTypes
+
+object MessageTypes extends Enumeration {
+  type MessageTypes = Value
+  val BEGIN_PARSE, END_PARSE, BEGIN_PARSE_PATCHED, END_PARSE_PATCHED, BEGIN_REPORT, END_REPORT, BEGIN_PATCHING,
+  END_PATCHING, GOT_DIAGNOSTICS, BEGIN_COMPLETION, END_COMPLETION, BEGIN_STRUCTURE, END_STRUCTURE, BEGIN_DIAGNOSTIC,
+  END_DIAGNOSTIC, CHANGE_DOCUMENT = Value
+}
+
+trait TelemetryProvider {
+
+  def addTimedMessage(code: String, messageType: MessageTypes, msg: String): Unit
+
+  private def extractNames(className: String): (String, Option[String]) = {
+    val pattern = """.*\.(.*)\$\$anonfun\$([^\$]*).*""".r
+    pattern.findAllIn(className).matchData match {
+      case md if md.hasNext => {
+        val tmp = md.next()
+        (tmp.group(1), Some(tmp.group(2)))
+      }
+      case _ => (className, None)
+    }
+  }
+
+  def addTimedMessage(msg: String, messageType: MessageTypes): Unit = {
+    val element: StackTraceElement = Thread.currentThread().getStackTrace.toList(2)
+    extractNames(element.getClassName) match {
+      case (c, Some(m)) => addTimedMessage(s"$c : $m", messageType, msg)
+      case _ =>
+        addTimedMessage(s"${element.getClassName.split('.').last} : ${element.getMethodName}", messageType, msg)
+    }
+  }
+}
--- als-server/jvm/src/main/scala/org/mulesoft/als/server/lsp4j/LanguageClientWrapper.scala
+++ als-server/jvm/src/main/scala/org/mulesoft/als/server/lsp4j/LanguageClientWrapper.scala
@@ -2,11 +2,13 @@ package org.mulesoft.als.server.lsp4j
 
 import org.eclipse.lsp4j.services.LanguageClient
 import Lsp4JConversions._
-
 import org.mulesoft.lsp.feature.diagnostic.PublishDiagnosticsParams
 import org.mulesoft.lsp.client.{LanguageClient => lspLanguageClient}
+import org.mulesoft.lsp.feature.telemetry.TelemetryMessage
 case class LanguageClientWrapper(private val inner: LanguageClient) extends lspLanguageClient {
   override def publishDiagnostic(params: PublishDiagnosticsParams): Unit =
     inner.publishDiagnostics(params)
 
+  override def notifyTelemetry(params: TelemetryMessage): Unit =
+    inner.telemetryEvent(params)
 }
