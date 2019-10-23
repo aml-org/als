d41cb4de2bf7ee7eaf43286630a47d61d78369c0
llibarona <leandro.libarona@mulesoft.com>
2019-10-23 15:37:30 -0300

    Code cleanup

--- als-actions/shared/src/test/scala/org/mulesoft/als/actions/definition/DefinitionFileTests.scala
+++ als-actions/shared/src/test/scala/org/mulesoft/als/actions/definition/DefinitionFileTests.scala
@@ -1,10 +1,7 @@
 package org.mulesoft.als.actions.definition
 
 import org.mulesoft.als.actions.definition.files.FindDefinitionFile
-import org.mulesoft.als.common.NodeBranchBuilder
-import org.mulesoft.als.common.dtoTypes.Position
 import org.scalatest.{FlatSpec, Matchers}
-import org.yaml.parser.YamlParser
 
 class DefinitionFileTests extends FlatSpec with Matchers with FindDefinitionFile {
   behavior of "FindSourceFile"
@@ -14,26 +11,5 @@ class DefinitionFileTests extends FlatSpec with Matchers with FindDefinitionFile
     extractProtocol("file:///fragment") should be("file")
     extractProtocol("/fragment") should be("")
     extractProtocol("http://localhost:8080/test") should be("http")
-  }
-
-  it should "identify root 'uses' node declarations" in {
-    val textOK =
-      """uses:
-        |  lib: test.yaml
-        |""".stripMargin
-
-    val textWrong =
-      """types:
-        |  uses:
-        |    lib: test.yaml
-        |""".stripMargin
-
-    val yPartBranchWrong =
-      NodeBranchBuilder.build(YamlParser(textWrong, "file").parse(false).head, Position(3, 11).toAmfPosition)
-    isInUsesRef(yPartBranchWrong) should be(false)
-
-    val yPartBranchOK =
-      NodeBranchBuilder.build(YamlParser(textOK, "file").parse(false).head, Position(2, 9).toAmfPosition)
-    isInUsesRef(yPartBranchOK) should be(true)
   }
 }
