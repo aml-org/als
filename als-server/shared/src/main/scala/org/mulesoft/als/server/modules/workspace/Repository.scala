0ae12c3a2c73eec5ea8c08ca60522151db116e86
agusayerza <agusayerza@gmail.com>
2019-12-04 11:36:36 -0300

    Repository now searches unit by file path if possible

--- als-server/shared/src/main/scala/org/mulesoft/als/server/modules/workspace/Repository.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/modules/workspace/Repository.scala
@@ -14,7 +14,7 @@ import scala.concurrent.Future
 
 case class ParsedUnit(bu: BaseUnit, inTree: Boolean) {
   def toCU(next: Option[Future[CompilableUnit]], mf: Option[String], stack: Seq[ReferenceStack]): CompilableUnit =
-    CompilableUnit(bu.id, bu, if (inTree) mf else None, next, stack)
+    CompilableUnit(bu.location().getOrElse(bu.id), bu, if (inTree) mf else None, next, stack)
 }
 
 case class DiagnosticsBundle(isExternal: Boolean, references: Set[ReferenceStack]) {
@@ -38,10 +38,10 @@ class Repository(cachables: Set[String], logger: Logger) {
 
   def treeKeys: collection.Set[String] = innerRefs.keySet
 
-  def update(u: BaseUnit): Unit = {
-    if (treeKeys.contains(u.id)) throw new Exception("Cannot update an unit from the tree")
-    val unit = ParsedUnit(u, inTree = false)
-    units.update(u.id, unit)
+  def update(bu: BaseUnit): Unit = {
+    if (treeKeys.contains(getBaseUnitIdentifier(bu))) throw new Exception("Cannot update an unit from the tree")
+    val unit = ParsedUnit(bu, inTree = false)
+    units.update(getBaseUnitIdentifier(bu), unit)
   }
 
   def cleanTree(): Unit = {
@@ -59,25 +59,29 @@ class Repository(cachables: Set[String], logger: Logger) {
   private def indexUnit(unit: BaseUnit, stack: ReferenceStack): Future[Unit] =
     indexParsedUnit(ParsedUnit(unit, inTree = true), stack)
 
+  def isRecursive(stack: ReferenceStack, unit: ParsedUnit): Boolean =
+    stack.stack.exists(_.originUri == getBaseUnitIdentifier(unit.bu))
+
   private def indexParsedUnit(pu: ParsedUnit, stack: ReferenceStack): Future[Unit] = {
-    def isRecursive(stack: ReferenceStack, unit: ParsedUnit): Boolean =
-      stack.stack.exists(_.originUri == unit.bu.id)
 
     val cachedF = checkCache(pu)
 
     val refs = if (!isRecursive(stack, pu)) { // stop: recursion
-      units.put(pu.bu.id, pu)
-      innerRefs.get(pu.bu.id) match {
-        case Some(db) => innerRefs.update(pu.bu.id, db.and(stack))
-        case _        => innerRefs.put(pu.bu.id, DiagnosticsBundle(pu.bu.isInstanceOf[ExternalFragment], Set(stack)))
+      units.put(getBaseUnitIdentifier(pu.bu), pu)
+      innerRefs.get(getBaseUnitIdentifier(pu.bu)) match {
+        case Some(db) => innerRefs.update(getBaseUnitIdentifier(pu.bu), db.and(stack))
+        case _ =>
+          innerRefs.put(getBaseUnitIdentifier(pu.bu),
+                        DiagnosticsBundle(pu.bu.isInstanceOf[ExternalFragment], Set(stack)))
       }
       pu.bu.annotations
         .collect[ReferenceTargets] { case rt: ReferenceTargets => rt }
-        .map(rt =>
-          indexUnit(
-            pu.bu.references.find(_.id == rt.targetLocation).get,
-            stack.through(ReferenceOrigins(pu.bu.location().getOrElse(pu.bu.id), PositionRange(rt.originRange)))
-        ))
+        .map(
+          rt =>
+            indexUnit(
+              pu.bu.references.find(_.id == rt.targetLocation).get,
+              stack.through(ReferenceOrigins(getBaseUnitIdentifier(pu.bu), PositionRange(rt.originRange)))
+          ))
     } else Nil
 
     Future.sequence(cachedF +: refs).map(_ => Unit)
@@ -87,7 +91,7 @@ class Repository(cachables: Set[String], logger: Logger) {
     references.get(uri).map(db => db.references.toSeq).getOrElse(Nil)
 
   private def checkCache(p: ParsedUnit): Future[Unit] =
-    if (cache.isEmpty && cachables.contains(p.bu.id)) cache(p) else Future.unit
+    if (cache.isEmpty && cachables.contains(getBaseUnitIdentifier(p.bu))) cache(p) else Future.unit
 
   private def cache(p: ParsedUnit): Future[Unit] = {
     val eventualUnit: Future[Unit] = Future {
@@ -97,18 +101,20 @@ class Repository(cachables: Set[String], logger: Logger) {
         ParserHelper
           .reportResolved(resolved)
           .map(r => {
-            if (r.conforms) cache.put(p.bu.id, ParsedUnit(resolved, inTree = true))
+            if (r.conforms) cache.put(getBaseUnitIdentifier(p.bu), ParsedUnit(resolved, inTree = true))
             Unit
           }))
     eventualUnit
       .recoverWith {
         case e: Throwable => // ignore
-          logger.error(s"Error while resolving cachable unit: ${p.bu.id}. Message ${e.getMessage}",
+          logger.error(s"Error while resolving cachable unit: ${getBaseUnitIdentifier(p.bu)}. Message ${e.getMessage}",
                        "Repository",
                        "Cache unit")
           Future.successful(Unit)
       }
   }
+
+  def getBaseUnitIdentifier(baseUnit: BaseUnit): String = baseUnit.location().getOrElse(baseUnit.id)
 
   def resolverCache: ReferenceResolver = { url: String =>
     cache.get(url) match {
