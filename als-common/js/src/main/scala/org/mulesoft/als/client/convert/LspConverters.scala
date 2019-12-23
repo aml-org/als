254141a1579cc392e3dd89aae96e1b2a2c3f215d
llibarona <leandro.libarona@mulesoft.com>
2019-12-23 15:16:23 -0300

    WIP (export -> native)

--- als-common/js/src/main/scala/org/mulesoft/als/client/convert/LspConverters.scala
+++ als-common/js/src/main/scala/org/mulesoft/als/client/convert/LspConverters.scala
@@ -1,18 +1,24 @@
 package org.mulesoft.als.client.convert
 
 import org.mulesoft.als.client.lsp.command.ClientCommand
+import org.mulesoft.als.client.lsp.edit.ClientNewFileOptions
 import org.mulesoft.als.client.lsp.feature.completion.{
+  ClientCompletionClientCapabilities,
   ClientCompletionContext,
   ClientCompletionItem,
   ClientCompletionItemClientCapabilities,
   ClientCompletionItemKindClientCapabilities
 }
+import org.mulesoft.als.client.lsp.feature.diagnostic.ClientDiagnosticClientCapabilities
 import org.mulesoft.lsp
+import org.mulesoft.lsp.edit.NewFileOptions
 import org.mulesoft.lsp.feature.completion.{
+  CompletionClientCapabilities,
   CompletionContext,
   CompletionItemClientCapabilities,
   CompletionItemKindClientCapabilities
 }
+import org.mulesoft.lsp.feature.diagnostic.DiagnosticClientCapabilities
 
 import scala.language.implicitConversions
 
@@ -33,4 +39,16 @@ object LspConverters {
 
   implicit def toClientCompletionContext(completionContext: CompletionContext): ClientCompletionContext =
     new ClientCompletionContext(completionContext)
+
+  implicit def toClientDiagnosticClientCapabilities(
+      diagnosticClientCapabilities: DiagnosticClientCapabilities): ClientDiagnosticClientCapabilities =
+    new ClientDiagnosticClientCapabilities(diagnosticClientCapabilities)
+
+  implicit def toClientCompletionClientCapabilities(
+      completionClientCapabilities: CompletionClientCapabilities): ClientCompletionClientCapabilities =
+    new ClientCompletionClientCapabilities(completionClientCapabilities)
+
+  implicit def toClientNewFileOptions(newFileOptions: NewFileOptions): ClientNewFileOptions =
+    new ClientNewFileOptions(newFileOptions)
+
 }
---
+++ als-common/js/src/main/scala/org/mulesoft/als/client/lsp/edit/TextEdit.scala
@@ -0,0 +1,17 @@
+package org.mulesoft.als.client.lsp.edit
+
+import org.mulesoft.lsp.common.Range
+
+import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
+
+/**
+  * A textual edit applicable to a text document.
+  *
+  * @param range   The range of the text document to be manipulated. To insert
+  *                text into a document create a range where start === end.
+  * @param newText The string to be inserted. For delete operations use an
+  *                empty string.
+  */
+@JSExportAll
+@JSExportTopLevel("TextEditorg.mulesoft.als.client.lsp.Command")
+case class TextEdit(range: Range, newText: String)
