c575612cd8faafe5be67ba4dd57c470221472c78
llibarona <leandro.libarona@mulesoft.com>
2020-01-01 08:30:04 -0300

    CodeAction, DocumentSymbol and Definition

--- als-common/js/src/main/scala/org/mulesoft/als/client/lsp/feature/documentsymbol/ClientDocumentSymbol.scala
+++ als-common/js/src/main/scala/org/mulesoft/als/client/lsp/feature/documentsymbol/ClientDocumentSymbol.scala
@@ -1,23 +1,39 @@
 package org.mulesoft.als.client.lsp.feature.documentsymbol
 
-import org.mulesoft.lsp.feature.documentsymbol.SymbolKind.SymbolKind
-import org.mulesoft.lsp.common.Range
-
 import scala.scalajs.js
+import scala.scalajs.js.JSConverters._
+import org.mulesoft.als.client.convert.LspConvertersSharedToClient._
+import org.mulesoft.als.client.lsp.common.ClientRange
+import org.mulesoft.lsp.feature.documentsymbol.{DocumentSymbol, SymbolKindClientCapabilities}
 
 @js.native
 trait ClientDocumentSymbol extends js.Object {
   def name: String = js.native
 
-  def kind: SymbolKind = js.native
+  def kind: Int = js.native
 
-  def range: Range = js.native
+  def range: ClientRange = js.native
 
-  def selectionRange: Range = js.native
+  def selectionRange: ClientRange = js.native
 
   def children: js.Array[ClientDocumentSymbol] = js.native
 
   def detail: js.UndefOr[String] = js.native
 
   def deprecated: js.UndefOr[Boolean] = js.native
+}
+
+object ClientDocumentSymbol {
+  def apply(internal: DocumentSymbol): ClientDocumentSymbol =
+    js.Dynamic
+      .literal(
+        name = internal.name,
+        kind = internal.kind.id,
+        range = internal.range.toClient,
+        selectionRange = internal.selectionRange.toClient,
+        children = internal.children.map(_.toClient).toJSArray,
+        detail = internal.detail.orUndefined,
+        deprecated = internal.deprecated.orUndefined
+      )
+      .asInstanceOf[ClientDocumentSymbol]
 }
