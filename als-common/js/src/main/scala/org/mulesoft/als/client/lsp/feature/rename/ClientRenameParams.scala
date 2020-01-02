2031189a1f2dd4c8f1f749efde86c5f404f5b131
llibarona <leandro.libarona@mulesoft.com>
2020-01-02 11:46:20 -0300

    Rename js objects

---
+++ als-common/js/src/main/scala/org/mulesoft/als/client/lsp/feature/rename/ClientRenameParams.scala
@@ -0,0 +1,29 @@
+package org.mulesoft.als.client.lsp.feature.rename
+
+import org.mulesoft.lsp.common.{Position, TextDocumentIdentifier}
+
+import scala.scalajs.js
+import scala.scalajs.js.UndefOr
+import org.mulesoft.als.client.convert.LspConvertersSharedToClient._
+import org.mulesoft.als.client.lsp.common.{ClientPosition, ClientTextDocumentIdentifier}
+import org.mulesoft.lsp.feature.rename.RenameParams
+
+import js.JSConverters._
+
+@js.native
+trait ClientRenameParams extends js.Object {
+  def textDocument: ClientTextDocumentIdentifier = js.native
+  def position: ClientPosition                   = js.native
+  def newName: String                            = js.native
+}
+
+object ClientRenameParams {
+  def apply(internal: RenameParams): ClientRenameParams =
+    js.Dynamic
+      .literal(
+        textDocument = internal.textDocument.toClient,
+        position = internal.position.toClient,
+        newName = internal.newName
+      )
+      .asInstanceOf[ClientRenameParams]
+}
