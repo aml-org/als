ec59b7c16d73a9ba8280b980c440f351f38de5c2
llibarona <leandro.libarona@mulesoft.com>
2020-01-06 11:06:45 -0300

    ALS-955: ClientTextDocumentClientCapabilities update

--- als-common/js/src/main/scala/org/mulesoft/als/client/lsp/configuration/ClientTextDocumentClientCapabilities.scala
+++ als-common/js/src/main/scala/org/mulesoft/als/client/lsp/configuration/ClientTextDocumentClientCapabilities.scala
@@ -7,6 +7,12 @@ import org.mulesoft.lsp.configuration.TextDocumentClientCapabilities
 
 import scala.scalajs.js
 import org.mulesoft.als.client.convert.LspConvertersSharedToClient._
+import org.mulesoft.als.client.lsp.feature.codeactions.ClientCodeActionCapabilities
+import org.mulesoft.als.client.lsp.feature.definition.ClientDefinitionClientCapabilities
+import org.mulesoft.als.client.lsp.feature.link.ClientDocumentLinkClientCapabilities
+import org.mulesoft.als.client.lsp.feature.reference.ClientReferenceClientCapabilities
+import org.mulesoft.als.client.lsp.feature.rename.ClientRenameClientCapabilities
+
 import scala.scalajs.js.JSConverters._
 
 @js.native
@@ -19,17 +25,17 @@ trait ClientTextDocumentClientCapabilities extends js.Object {
   def completion: js.UndefOr[ClientCompletionClientCapabilities] =
     js.native
 
-//  def references: js.UndefOr[ClientReferenceClientCapabilities] = js.native
-//
+  def references: js.UndefOr[ClientReferenceClientCapabilities] = js.native
+
   def documentSymbol: js.UndefOr[ClientDocumentSymbolClientCapabilities] = js.native
-//
-//  def definition: js.UndefOr[ClientDefinitionClientCapabilities] = js.native
-//
-//  def rename: js.UndefOr[ClientRenameClientCapabilities] = js.native
-//
-//  def codeActionCapabilities: js.UndefOr[ClientCodeActionCapabilities] = js.native
-//
-//  def documentLink: js.UndefOr[ClientDocumentLinkClientCapabilities] = js.native
+
+  def definition: js.UndefOr[ClientDefinitionClientCapabilities] = js.native
+
+  def rename: js.UndefOr[ClientRenameClientCapabilities] = js.native
+
+  def codeActionCapabilities: js.UndefOr[ClientCodeActionCapabilities] = js.native
+
+  def documentLink: js.UndefOr[ClientDocumentLinkClientCapabilities] = js.native
 }
 
 object ClientTextDocumentClientCapabilities {
@@ -38,7 +44,12 @@ object ClientTextDocumentClientCapabilities {
       .literal(
         publishDiagnostics = internal.publishDiagnostics.map(_.toClient).orUndefined,
         completion = internal.completion.map(_.toClient).orUndefined,
-        documentSymbol = internal.documentSymbol.map(_.toClient).orUndefined
+        references = internal.references.map(_.toClient).orUndefined,
+        documentSymbol = internal.documentSymbol.map(_.toClient).orUndefined,
+        definition = internal.definition.map(_.toClient).orUndefined,
+        rename = internal.rename.map(_.toClient).orUndefined,
+        codeActionCapabilities = internal.codeActionCapabilities.map(_.toClient).orUndefined,
+        documentLink = internal.documentLink.map(_.toClient).orUndefined
       )
       .asInstanceOf[ClientTextDocumentClientCapabilities]
 }
