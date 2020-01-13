ff8ab61b1df6ce9c757da2d29e3c2db534c66817
Javier Isoldi <javierisoldi@gmail.com>
2020-01-13 10:55:15 -0300

    ALS initialization improved for JS

--- als-server/shared/src/main/scala/org/mulesoft/als/server/client/ClientNotifier.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/client/ClientNotifier.scala
@@ -3,6 +3,9 @@ package org.mulesoft.als.server.client
 import org.mulesoft.lsp.feature.diagnostic.PublishDiagnosticsParams
 import org.mulesoft.lsp.feature.telemetry.TelemetryMessage
 
+import scala.scalajs.js.annotation.JSExportAll
+
+@JSExportAll
 trait ClientNotifier {
 
   def notifyDiagnostic(params: PublishDiagnosticsParams): Unit
