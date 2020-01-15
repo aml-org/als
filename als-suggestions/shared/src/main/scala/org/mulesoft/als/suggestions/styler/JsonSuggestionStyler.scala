b47e877fad5382bca512e0dfcbc6ceb66618bb0c
agusayerza <agusayerza@gmail.com>
2020-01-15 12:28:41 -0300

    Fixed JSON styler for Japanese chars (#107)

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/styler/JsonSuggestionStyler.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/styler/JsonSuggestionStyler.scala
@@ -2,11 +2,11 @@ package org.mulesoft.als.suggestions.styler
 
 import org.mulesoft.als.suggestions.styler.astbuilder.{AstRawBuilder, JsonAstRawBuilder}
 import org.mulesoft.als.suggestions.{RawSuggestion, SuggestionStructure}
-import org.yaml.render.JsonRender
+import org.yaml.render.{JsonRender, JsonRenderOptions}
 
 case class JsonSuggestionStyler(override val params: StylerParams) extends SuggestionRender {
   override protected def render(options: SuggestionStructure, builder: AstRawBuilder): String = {
-    val json = JsonRender.render(builder.ast, params.indentation)
+    val json = JsonRender.render(builder.ast, params.indentation, options = JsonRenderOptions().withoutNonAsciiEncode)
     if (json.endsWith("{}")) {
       builder.forSnippet()
       json.replace("{}", "{\n" + (" " * params.indentation) + "  \"$1\"\n" + (" " * params.indentation) + "}")
