44a7351407c308bd460fe6c7abedd5f000466b55
Hernan Najles <hernan.naj@gmail.com>
2020-01-28 10:19:57 -0300

    Fix environment convertion at factory. Use given environment at text document manager

---
+++ als-server/jvm/src/test/scala/org.mulesoft.als.server.lsp4j/LspCustomEnvironment.scala
@@ -0,0 +1,53 @@
+package org.mulesoft.als.server.lsp4j
+
+import java.util.concurrent.CompletableFuture
+
+import amf.client.environment.{DefaultEnvironment, Environment}
+import amf.client.remote.Content
+import amf.client.resource.ClientResourceLoader
+import amf.core.unsafe.PlatformSecrets
+import org.eclipse.lsp4j.{DidOpenTextDocumentParams, TextDocumentItem}
+import org.mulesoft.als.server.MockDiagnosticClientNotifier
+import org.mulesoft.als.server.logger.EmptyLogger
+import org.mulesoft.als.server.modules.diagnostic.ALL_TOGETHER
+import org.scalatest.{AsyncFunSuite, Matchers}
+
+class LspCustomEnvironment extends AsyncFunSuite with Matchers with PlatformSecrets {
+
+  test("test custom environment") {
+
+    var calledRL = false
+    val cl = new ClientResourceLoader {
+      override def fetch(resource: String): CompletableFuture[Content] = {
+        calledRL = true
+        CompletableFuture.completedFuture(new Content("#%RAML 1.0 DataType\ntype: string", "jar:/api.raml"))
+      }
+
+      override def accepts(resource: String): Boolean = true
+    }
+
+    val notifier = new MockDiagnosticClientNotifier()
+    val env      = DefaultEnvironment().addClientLoader(cl)
+    val server = new LanguageServerImpl(
+      new LanguageServerFactory(notifier)
+        .withNotificationKind(ALL_TOGETHER)
+        .withLogger(EmptyLogger)
+        .withEnvironment(env)
+        .build())
+    val api =
+      """#%RAML 1.0
+        |title: test
+        |types:
+        | A: !include jar:/api.raml
+        |""".stripMargin
+    server.getTextDocumentService.didOpen(
+      new DidOpenTextDocumentParams(new TextDocumentItem("file://api.raml", "raml1.0", 1, api)))
+    for {
+      r1 <- notifier.nextCall
+      r2 <- notifier.nextCall
+    } yield {
+      r2.uri should be("file://jar:/api.raml")
+      calledRL should be(true)
+    }
+  }
+}
