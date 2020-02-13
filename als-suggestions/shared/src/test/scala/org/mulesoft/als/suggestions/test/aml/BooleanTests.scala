336b03db4dd342bd815de953e8a8b49160ff8f51
llibarona <leandro.libarona@mulesoft.com>
2020-02-13 17:55:55 -0300

    ALS-1021: add tests for boolean in AML

---
+++ als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/aml/BooleanTests.scala
@@ -0,0 +1,16 @@
+package org.mulesoft.als.suggestions.test.aml
+
+import amf.ProfileName
+
+class BooleanTests extends AMLSuggestionsTest {
+  override def rootPath: String = "AML/booleans"
+
+  test("test simple boolean") {
+    this.withDialect(
+      "instance.yaml",
+      Set("true"),
+      "dialect.yaml",
+      ProfileName("Boolean 1.0")
+    )
+  }
+}
