74cbcb7cedd5e4d2fb3320741066f400607a9a4f
agusayerza <agusayerza@gmail.com>
2020-02-20 14:19:43 -0300

    ALS-964 Multiworkspace support (#144)

--- als-lsp/shared/src/main/scala/org/mulesoft/lsp/configuration/WorkspaceFolderServerCapabilities.scala
+++ als-lsp/shared/src/main/scala/org/mulesoft/lsp/configuration/WorkspaceFolderServerCapabilities.scala
@@ -1,3 +1,12 @@
 package org.mulesoft.lsp.configuration
 
-case class WorkspaceFolderServerCapabilities(supported: Option[Boolean], changeNotifications: Option[Either[String, Boolean]])
+case class WorkspaceFolderServerCapabilities(supported: Option[Boolean],
+                                             changeNotifications: Option[Either[String, Boolean]]) {
+
+  def withSupported(value: Boolean): WorkspaceFolderServerCapabilities =
+    WorkspaceFolderServerCapabilities(Some(value), changeNotifications)
+
+  def withChangeNotification(value: Either[String, Boolean]): WorkspaceFolderServerCapabilities =
+    WorkspaceFolderServerCapabilities(supported, Some(value))
+
+}
--- als-server/shared/src/main/scala/org/mulesoft/als/server/modules/ManagersFactory.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/modules/ManagersFactory.scala
@@ -99,8 +99,15 @@ case class WorkspaceManagerFactory(projectDependencies: List[BaseUnitListener],
   val container: TextDocumentContainer = TextDocumentContainer(environment, platform, amfConfiguration)
 
   val cleanDiagnosticManager = new CleanDiagnosticTreeManager(container, logger)
-  val workspaceManager       = new WorkspaceManager(container, telemetryManager, projectDependencies, logger)
-  lazy val documentManager   = new TextDocumentManager(container, List(workspaceManager), logger)
+
+  val workspaceManager: WorkspaceManager =
+    new WorkspaceManager(container, telemetryManager, projectDependencies, logger)
+
+  def cleanWorkspaceManager: WorkspaceManager = {
+    new WorkspaceManager(container, telemetryManager, projectDependencies, logger)
+  }
+
+  lazy val documentManager = new TextDocumentManager(container, List(workspaceManager), logger)
 
   lazy val completionManager =
     new SuggestionsManager(container, workspaceManager, telemetryManager, directoryResolver, logger)
