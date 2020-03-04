bf6020b83ef18833ed13095245fa2f2e5f9272cf
llibarona <45210281+llibarona@users.noreply.github.com>
2020-03-04 16:20:54 -0300

    ALS-1030 (#152)
    * ALS-1030: array fixes
    
    * fix outlines
    
    * styling fixes, add lone child skipping to other nodes

--- als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/symbol/corebuilders/ObjectNodeSymbolBuilder.scala
+++ als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/symbol/corebuilders/ObjectNodeSymbolBuilder.scala
@@ -1,14 +1,14 @@
 package org.mulesoft.language.outline.structure.structureImpl.symbol.corebuilders
 
 import amf.core.annotations.{LexicalInformation, SourceAST}
-import amf.core.metamodel.domain.{ArrayNodeModel, ObjectNodeModel}
+import amf.core.metamodel.domain.ObjectNodeModel
 import amf.core.model.domain._
-import org.mulesoft.language.outline.structure.structureImpl._
-import org.yaml.model.YMapEntry
-import amf.core.utils._
 import amf.core.parser.{Value, Range => AmfRange}
+import amf.core.utils._
 import org.mulesoft.als.common.dtoTypes.PositionRange
+import org.mulesoft.language.outline.structure.structureImpl._
 import org.mulesoft.lexer.InputRange
+import org.yaml.model.YMapEntry
 
 class ObjectNodeSymbolBuilder(obj: ObjectNode)(override implicit val factory: BuilderFactory)
     extends ElementSymbolBuilder[ObjectNode] {
@@ -21,9 +21,10 @@ class ObjectNodeSymbolBuilder(obj: ObjectNode)(override implicit val factory: Bu
         value.map(v => f -> v)
       })
       .flatMap {
-        case (n, Value(v: ScalarNode, _)) => Nil
+        case (_, Value(_: ScalarNode, _)) => Nil
         case (n, Value(v: DataNode, a)) =>
-          val range = PositionRange(a.find(classOf[LexicalInformation]).map(l => l.range).getOrElse(AmfRange(InputRange.Zero)))
+          val range =
+            PositionRange(a.find(classOf[LexicalInformation]).map(l => l.range).getOrElse(AmfRange(InputRange.Zero)))
           val selectionRange = a
             .find(classOf[SourceAST])
             .map(_.ast)
@@ -59,34 +60,4 @@ object ObjectNodeSymbolBuilder extends ElementSymbolBuilderCompanion {
   override def construct(element: ObjectNode)(
       implicit factory: BuilderFactory): Option[ElementSymbolBuilder[ObjectNode]] =
     Some(new ObjectNodeSymbolBuilder(element))
-}
-
-class ArrayNodeSymbolBuilder(arr: ArrayNode)(override implicit val factory: BuilderFactory)
-    extends ElementSymbolBuilder[ArrayNode] {
-
-  override def build(): Seq[DocumentSymbol] = {
-    arr.members.zipWithIndex.map {
-      case (node, index) =>
-        val range =
-          PositionRange(node.annotations.find(classOf[LexicalInformation]).map(_.range).getOrElse(AmfRange.NONE))
-        new DocumentSymbol(index.toString,
-                           SymbolKind.Array,
-                           false,
-                           range,
-                           range,
-                           factory.builderFor(node).map(_.build()).getOrElse(Nil).toList)
-    }
-  }
-}
-
-object ArrayNodeSymbolBuilder extends ElementSymbolBuilderCompanion {
-  override type T = ArrayNode
-
-  override def getType: Class[_ <: AmfElement] = classOf[ArrayNode]
-
-  override val supportedIri: String = ArrayNodeModel.`type`.head.iri()
-
-  override def construct(element: ArrayNode)(
-      implicit factory: BuilderFactory): Option[ElementSymbolBuilder[ArrayNode]] =
-    Some(new ArrayNodeSymbolBuilder(element))
 }
