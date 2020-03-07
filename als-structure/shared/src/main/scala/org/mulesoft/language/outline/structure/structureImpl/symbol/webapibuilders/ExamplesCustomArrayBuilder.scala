de8bf5f1b1544931427fd1c6b2c82ba9ba5de753
llibarona <leandro.libarona@mulesoft.com>
2020-03-06 21:12:52 -0300

    OAS examples now labeled in plural (example -> examples)

--- als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/symbol/webapibuilders/ExamplesCustomArrayBuilder.scala
+++ als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/symbol/webapibuilders/ExamplesCustomArrayBuilder.scala
@@ -1,7 +1,8 @@
 package org.mulesoft.language.outline.structure.structureImpl.symbol.webapibuilders
 
-import amf.core.model.domain.AmfArray
+import amf.core.model.domain.{AmfArray, AmfScalar}
 import amf.core.parser.FieldEntry
+import amf.plugins.domain.shapes.metamodel.ExampleModel
 import amf.plugins.domain.shapes.models.Example
 import amf.plugins.domain.webapi.metamodel.ParameterModel
 import org.mulesoft.language.outline.structure.structureImpl.BuilderFactory
@@ -16,7 +17,11 @@ case class ExamplesCustomArrayBuilder(override implicit val factory: BuilderFact
 
   private def isSingleExample(fe: FieldEntry) =
     fe.array.values.exists {
-      case head: Example => head.name.isNullOrEmpty
+      case head: Example => head.name.isNullOrEmpty && !hasMediaType(head)
       case _             => false
     }
+
+  private def hasMediaType(e: Example): Boolean =
+    e.fields.fields().exists(_.field == ExampleModel.MediaType)
+
 }
