fc174672889d836e0f8db736a7f43d2ec2378cc6
Hernan Najles <hernan.naj+github@gmail.com>
2020-03-11 08:37:12 -0300

    subtract added tokens size to ranges for patched json elements in order to avoid invalid range (more columns that the number originally present at the line)

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/patcher/PatchedContent.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/patcher/PatchedContent.scala
@@ -1,9 +1,9 @@
 package org.mulesoft.als.suggestions.patcher
 
-abstract class PatchToken(token: String)
+abstract class PatchToken(token: String, val size: Int)
 
-object ColonToken extends PatchToken(":")
-object QuoteToken extends PatchToken("\"")
-object CommaToken extends PatchToken(",")
+object ColonToken extends PatchToken(":", 2)
+object QuoteToken extends PatchToken("\"", 1)
+object CommaToken extends PatchToken(",", 1)
 
 case class PatchedContent(content: String, original: String, addedTokens: List[PatchToken])
