4396b816bad8ac1a268de36758939f93996fa227
Hernan Najles <hernan.naj+github@gmail.com>
2020-03-11 08:37:12 -0300

    subtract added tokens size to ranges for patched json elements in order to avoid invalid range (more columns that the number originally present at the line)

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/styler/SuggestionRender.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/styler/SuggestionRender.scala
@@ -30,9 +30,17 @@ trait SuggestionRender {
   private def keyRange: Option[PositionRange] = {
     params.yPartBranch.node match {
       case n: YNode if n.value.isInstanceOf[YScalar] && params.yPartBranch.isJson =>
-        if (params.yPartBranch.isKey)
-          params.yPartBranch.parentEntry.map(_.range).orElse(Some(n.range)).map(PositionRange(_))
-        else Some(PositionRange(n.range))
+        if (params.yPartBranch.isKey) {
+          params.yPartBranch.parentEntry
+            .map(_.range)
+            .map(
+              r =>
+                if (r.lineTo > r.lineFrom)
+                  r.copy(columnTo = r.columnTo - params.patchedContent.addedTokens.foldLeft(0)((a, t) => a + t.size))
+                else r)
+            .orElse(Some(n.range))
+            .map(PositionRange(_))
+        } else Some(PositionRange(n.range))
       case _ => None
     }
   }
