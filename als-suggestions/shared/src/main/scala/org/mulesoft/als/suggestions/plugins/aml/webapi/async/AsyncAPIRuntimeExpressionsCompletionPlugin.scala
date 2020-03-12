3f37a07321bcbff566c4eadfded0d33772bc3de6
llibarona <leandro.libarona@mulesoft.com>
2020-03-12 12:34:05 -0300

    cleanup PR

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/AsyncAPIRuntimeExpressionsCompletionPlugin.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/AsyncAPIRuntimeExpressionsCompletionPlugin.scala
@@ -1,6 +1,5 @@
 package org.mulesoft.als.suggestions.plugins.aml.webapi.async
 
-import amf.core.metamodel.Field
 import amf.plugins.domain.webapi.models.{CorrelationId, Parameter}
 import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
 import org.mulesoft.als.suggestions.plugins.aml.webapi.AbstractRuntimeExpressionsCompletionPlugin
@@ -10,8 +9,6 @@ import org.mulesoft.als.suggestions.plugins.aml.webapi.runtimeexpression.Runtime
 object AsyncAPIRuntimeExpressionsCompletionPlugin extends AbstractRuntimeExpressionsCompletionPlugin {
   override def parserObject(value: String): RuntimeExpressionParser =
     AsyncAPIRuntimeExpressionParser(value)
-
-  override protected val applicableFields: Seq[Field] = Nil
 
   override protected def appliesToField(request: AmlCompletionRequest): Boolean = request.amfObject match {
     case _ @(_: Parameter | _: CorrelationId) => true
