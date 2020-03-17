77631b626ccc2b43c49e0973d6bc1706f700c41b
Hernan Najles <hernan.naj+github@gmail.com>
2020-03-17 09:42:47 -0300

    Remove trailing comma

--- als-common/shared/src/main/scala/org/mulesoft/amfintegration/dialect/dialects/asyncapi20/schema/BaseShapeAsync2Node.scala
+++ als-common/shared/src/main/scala/org/mulesoft/amfintegration/dialect/dialects/asyncapi20/schema/BaseShapeAsync2Node.scala
@@ -6,24 +6,33 @@ import amf.dialects.oas.nodes.AMLExternalDocumentationObject
 import amf.plugins.document.vocabularies.model.domain.PropertyMapping
 import amf.plugins.domain.shapes.metamodel.{NodeShapeModel, ScalarShapeModel}
 import org.mulesoft.als.suggestions.aml.dialects.asyncapi20.dialectLocation
-import org.mulesoft.amfintegration.dialect.dialects.jsonschema.draft7.base.{BaseAnyShapeNode, BaseArrayShapeNode, BaseNodeShapeNode, BaseNumberShapeNode, BaseShapeNode, BaseStringShapeNode}
+import org.mulesoft.amfintegration.dialect.dialects.jsonschema.draft7.base.{
+  BaseAnyShapeNode,
+  BaseArrayShapeNode,
+  BaseNodeShapeNode,
+  BaseNumberShapeNode,
+  BaseShapeNode,
+  BaseStringShapeNode
+}
 import amf.core.vocabulary.Namespace.XsdTypes._
 
-trait BaseShapeAsync2Node extends BaseShapeNode{
+trait BaseShapeAsync2Node extends BaseShapeNode {
 
   override def location: String = dialectLocation
 
-  override def properties: Seq[PropertyMapping] = super.properties ++ Seq(
-  PropertyMapping()
-    .withId(location + "#/declarations/AnyShapeNode/deprecated")
-    .withNodePropertyMapping(ShapeModel.Deprecated.value.iri())
-    .withName("deprecated")
-    .withLiteralRange(xsdBoolean.iri()),
-  PropertyMapping()
-    .withId(location + "#/declarations/AnyShapeNode/externalDocs")
-    .withNodePropertyMapping(ScalarShapeModel.Format.value.iri())
-    .withName("externalDocs")
-    .withObjectRange(Seq(AMLExternalDocumentationObject.id)))
+  override def properties: Seq[PropertyMapping] =
+    super.properties ++ Seq(
+      PropertyMapping()
+        .withId(location + "#/declarations/AnyShapeNode/deprecated")
+        .withNodePropertyMapping(ShapeModel.Deprecated.value.iri())
+        .withName("deprecated")
+        .withLiteralRange(xsdBoolean.iri()),
+      PropertyMapping()
+        .withId(location + "#/declarations/AnyShapeNode/externalDocs")
+        .withNodePropertyMapping(ScalarShapeModel.Format.value.iri())
+        .withName("externalDocs")
+        .withObjectRange(Seq(AMLExternalDocumentationObject.id))
+    )
 }
 
 object BaseShapeAsync2Node extends BaseShapeAsync2Node
@@ -33,14 +42,16 @@ object AnyShapeAsync2Node extends BaseAnyShapeNode with BaseShapeAsync2Node
 object ArrayShapeAsync2Node extends BaseArrayShapeNode with BaseShapeAsync2Node
 
 object NodeShapeAsync2Node extends BaseNodeShapeNode with BaseShapeAsync2Node {
-  override def properties: Seq[PropertyMapping] = super.properties ++ Seq( PropertyMapping()
-    .withId(location + "#/declarations/AnyShapeNode/discriminator")
-    .withNodePropertyMapping(NodeShapeModel.Discriminator.value.iri())
-    .withName("discriminator")
-    .withLiteralRange(xsdString.iri()))
+  override def properties: Seq[PropertyMapping] =
+    super.properties ++ Seq(
+      PropertyMapping()
+        .withId(location + "#/declarations/AnyShapeNode/discriminator")
+        .withNodePropertyMapping(NodeShapeModel.Discriminator.value.iri())
+        .withName("discriminator")
+        .withLiteralRange(xsdString.iri()))
 }
 
-object NumberShapeAsync2Node extends BaseNumberShapeNode with BaseShapeAsync2Node{
+object NumberShapeAsync2Node extends BaseNumberShapeNode with BaseShapeAsync2Node {
   override def properties: Seq[PropertyMapping] = super.properties ++ Seq(
     PropertyMapping()
       .withId(location + "#/declarations/AnyShapeNode/format")
@@ -57,7 +68,7 @@ object NumberShapeAsync2Node extends BaseNumberShapeNode with BaseShapeAsync2Nod
   )
 }
 
-object StringShapeAsync2Node extends BaseStringShapeNode with BaseShapeAsync2Node{
+object StringShapeAsync2Node extends BaseStringShapeNode with BaseShapeAsync2Node {
   override def properties: Seq[PropertyMapping] = super.properties ++ Seq(
     PropertyMapping()
       .withId(location + "#/declarations/AnyShapeNode/format")
@@ -71,6 +82,6 @@ object StringShapeAsync2Node extends BaseStringShapeNode with BaseShapeAsync2Nod
           "date-time",
           "password"
         ))
-      .withLiteralRange(xsdString.iri()),
+      .withLiteralRange(xsdString.iri())
   )
 }
