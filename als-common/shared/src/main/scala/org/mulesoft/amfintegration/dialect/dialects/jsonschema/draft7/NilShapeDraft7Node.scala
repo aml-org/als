93aeb2a3d0880f3d3153ea68cf2ce3f4e2934123
Hernan Najles <hernan.naj+github@gmail.com>
2020-03-17 09:41:33 -0300

    Minor fix over draft 7 nil shape node mapping hierachy

--- als-common/shared/src/main/scala/org/mulesoft/amfintegration/dialect/dialects/jsonschema/draft7/NilShapeDraft7Node.scala
+++ als-common/shared/src/main/scala/org/mulesoft/amfintegration/dialect/dialects/jsonschema/draft7/NilShapeDraft7Node.scala
@@ -1,9 +1,8 @@
 package org.mulesoft.amfintegration.dialect.dialects.jsonschema.draft7
 
 import amf.plugins.domain.shapes.metamodel.NilShapeModel
-import org.mulesoft.amfintegration.dialect.dialects.jsonschema.draft7.base.BaseShapeNode
 
-object NilShapeDraft7Node extends BaseShapeNode {
+object NilShapeDraft7Node extends BaseShapeDraft7Node {
   override def name: String = "NilShape"
 
   override def nodeTypeMapping: String = NilShapeModel.`type`.head.iri()
