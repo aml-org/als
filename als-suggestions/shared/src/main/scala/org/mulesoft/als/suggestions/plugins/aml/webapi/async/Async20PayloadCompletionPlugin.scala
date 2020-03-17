3e538affa73729a4361fc564bc0153c84c3c58bd
agusayerza <agusayerza@gmail.com>
2020-03-17 13:36:54 -0300

    ALS-970: Suggest payload properties of message based on schema format - OAS3

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/Async20PayloadCompletionPlugin.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/Async20PayloadCompletionPlugin.scala
@@ -5,7 +5,10 @@ import amf.plugins.domain.webapi.models.Payload
 import org.mulesoft.als.suggestions.RawSuggestion
 import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
 import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.async.Async20PayloadCompletionPlugin.oas3Regex
 import org.mulesoft.als.suggestions.plugins.aml.webapi.jsonSchema.draft7.JsonSchemeDraft7TypeFacetsCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.oas.oas30.Oas30TypeFacetsCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.raml.raml10.Raml10TypeFacetsCompletionPlugin
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -18,19 +21,25 @@ object Async20PayloadCompletionPlugin extends AMLCompletionPlugin {
     raw"application\/vnd\.aai\.asyncapi(\+yaml|\+json)?;?(version=2\.[0-9]\.[0-9])?".r
   val jsonRegex: Regex =
     raw"application\/schema(\+json|\+yaml)?;?(version=draft-07)?".r
+  val oas3Regex: Regex =
+    raw"application\/vnd\.oai\.openapi(\+json|\+yaml)?;?(version=3\.[0-9]\.[0-9])?".r
 
   override def resolve(request: AmlCompletionRequest): Future[Seq[RawSuggestion]] = {
     Future {
-      request.branchStack.headOption match {
+      val branchStack = request.branchStack
+      branchStack.headOption match {
         case Some(p: Payload) if request.yPartBranch.isKey =>
           request.amfObject match {
             case s: Shape =>
+              val mediaTypeFormat = p.schemaMediaType.value()
               if (p.schemaMediaType.isNullOrEmpty || asyncApiRegex
-                    .findAllMatchIn(p.schemaMediaType.value())
+                    .findAllMatchIn(mediaTypeFormat)
                     .nonEmpty)
-                Async20TypeFacetsCompletionPlugin.resolveShape(s, request.branchStack)
-              else if (jsonRegex.findAllMatchIn(p.schemaMediaType.value()).nonEmpty)
-                JsonSchemeDraft7TypeFacetsCompletionPlugin.resolveShape(s, request.branchStack)
+                Async20TypeFacetsCompletionPlugin.resolveShape(s, branchStack)
+              else if (jsonRegex.findAllMatchIn(mediaTypeFormat).nonEmpty)
+                JsonSchemeDraft7TypeFacetsCompletionPlugin.resolveShape(s, branchStack)
+              else if (oas3Regex.findAllMatchIn(mediaTypeFormat).nonEmpty)
+                Oas30TypeFacetsCompletionPlugin.resolveShape(s, branchStack)
               else Seq()
           }
         case _ => Seq()
