bfa77651ff23a2e8d361c827398540a2bd8fb8d4
Hernan Najles <hernan.naj+github@gmail.com>
2020-03-30 15:58:39 -0300

    Add test to check clone units and serialization to json ld with references

--- als-server/shared/src/test/scala/org/mulesoft/als/server/modules/serialization/SerializationTest.scala
+++ als-server/shared/src/test/scala/org/mulesoft/als/server/modules/serialization/SerializationTest.scala
@@ -16,7 +16,8 @@ import org.mulesoft.als.server.modules.{WorkspaceManagerFactory, WorkspaceManage
 import org.mulesoft.als.server.protocol.LanguageServer
 import org.mulesoft.als.server.protocol.configuration.{AlsClientCapabilities, AlsInitializeParams}
 import org.mulesoft.lsp.configuration.TraceKind
-import org.mulesoft.lsp.feature.common.TextDocumentIdentifier
+import org.mulesoft.lsp.feature.common.{TextDocumentIdentifier, TextDocumentItem}
+import org.mulesoft.lsp.textsync.DidOpenTextDocumentParams
 import org.yaml.builder.{DocBuilder, JsonOutputBuilder}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -75,7 +76,7 @@ class SerializationTest extends LanguageServerBaseTest {
     }
   }
 
-  test("Request serialized model"){
+  test("Request serialized model") {
     withServer { server =>
       val content =
         """#%RAML 1.0
@@ -88,7 +89,11 @@ class SerializationTest extends LanguageServerBaseTest {
 
       for {
         _ <- alsClient.nextCall.map(_.model.toString)
-        s <- server.resolveHandler(serializationProps.requestType).value.apply(SerializationParams(TextDocumentIdentifier(api))).map(_.model.toString)
+        s <- server
+          .resolveHandler(serializationProps.requestType)
+          .value
+          .apply(SerializationParams(TextDocumentIdentifier(api)))
+          .map(_.model.toString)
         parsed <- {
           val env = Environment(new ResourceLoader {
 
@@ -112,6 +117,43 @@ class SerializationTest extends LanguageServerBaseTest {
     }
   }
 
+  test("test test") {
+    withServer { server =>
+      val url = filePath("raml-endpoint-sorting.raml")
+
+      for {
+        _ <- platform.resolve(url).map { c =>
+          server.textDocumentSyncConsumer.didOpen(DidOpenTextDocumentParams(
+            TextDocumentItem(url, "RAML", 0, c.stream.toString))) // why clean empty lines was necessary?
+        }
+        s <- server
+          .resolveHandler(serializationProps.requestType)
+          .value
+          .apply(SerializationParams(TextDocumentIdentifier(url)))
+          .map(_.model.toString)
+        parsed <- {
+          val env = Environment(new ResourceLoader {
+
+            /** Fetch specified resource and return associated content. Resource should have benn previously accepted. */
+            override def fetch(resource: String): Future[Content] = Future.successful(new Content(s, url))
+
+            /** Accepts specified resource. */
+            override def accepts(resource: String): Boolean = resource == url
+          })
+          RuntimeCompiler
+            .forContext(
+              new CompilerContextBuilder(url, platform).withEnvironment(env).build(),
+              Some(Mimes.`APPLICATION/LD+JSONLD`),
+              Some(Amf.name),
+              UnspecifiedReference
+            )
+        }
+      } yield {
+        parsed.asInstanceOf[Document].encodes.id should be("amf://id#/web-api")
+      }
+    }
+  }
+
   override def buildServer(): LanguageServer = {
     val builder = new LanguageServerBuilder(factory.documentManager, factory.workspaceManager)
     builder.addInitializableModule(serializationManager)
@@ -119,5 +161,5 @@ class SerializationTest extends LanguageServerBaseTest {
     builder.build()
   }
 
-  override def rootPath: String = ""
+  override def rootPath: String = "serialization"
 }
