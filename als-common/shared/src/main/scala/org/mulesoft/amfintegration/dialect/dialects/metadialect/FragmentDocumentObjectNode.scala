118204e147df67acc41098c6f3e1affc5dcf8157
Hernan Najles <hernan.naj+github@gmail.com>
2020-04-05 18:48:12 -0300

    Enable more test. Support booleans and NM references. Also support completion on terms uris

--- als-common/shared/src/main/scala/org/mulesoft/amfintegration/dialect/dialects/metadialect/FragmentDocumentObjectNode.scala
+++ als-common/shared/src/main/scala/org/mulesoft/amfintegration/dialect/dialects/metadialect/FragmentDocumentObjectNode.scala
@@ -2,4 +2,6 @@ package org.mulesoft.amfintegration.dialect.dialects.metadialect
 
 object FragmentDocumentObjectNode extends EncodesDocumentObjectNode {
   override def name: String = "FragmentDocumentObjectNode"
+
+  override def nodeTypeMapping: String = "FakeToAvoidRoot"
 }
--- als-common/shared/src/main/scala/org/mulesoft/amfintegration/dialect/dialects/metadialect/LibraryDocumentObjectNode.scala
+++ als-common/shared/src/main/scala/org/mulesoft/amfintegration/dialect/dialects/metadialect/LibraryDocumentObjectNode.scala
@@ -2,4 +2,6 @@ package org.mulesoft.amfintegration.dialect.dialects.metadialect
 
 object LibraryDocumentObjectNode extends DeclaresDocumentObjectNode {
   override def name: String = "LibraryDocumentObjectNode"
+
+  override def nodeTypeMapping: String = "FakeToAvoidRootLibrary"
 }
