da4e02b3dbcedca2ef0ca337d2e3fedab0feea96
agusayerza <agusayerza@gmail.com>
2020-04-06 13:15:45 -0300

    ALS-1054: Suggestions on required properties | JSON Schema

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/webapi/AsyncApiCompletionPluginRegistry.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/webapi/AsyncApiCompletionPluginRegistry.scala
@@ -1,28 +1,22 @@
 package org.mulesoft.als.suggestions.aml.webapi
 
 import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
-import org.mulesoft.als.suggestions.plugins.aml.{ResolveDefault, StructureCompletionPlugin}
 import org.mulesoft.als.suggestions.plugins.aml.webapi.SecuredByCompletionPlugin
-import org.mulesoft.als.suggestions.plugins.aml.webapi.async._
 import org.mulesoft.als.suggestions.plugins.aml.webapi.async.bindings.{
   AsyncApiBindingsCompletionPlugin,
   BindingsDiscreditableProperties
 }
-import org.mulesoft.als.suggestions.plugins.aml.webapi.async.structure.{
-  ResolveInfo,
-  ResolveResponses,
-  ResolveServers,
-  ResolveShapeInPayload,
-  ResolveTraits
-}
+import org.mulesoft.als.suggestions.plugins.aml.webapi.async.structure._
 import org.mulesoft.als.suggestions.plugins.aml.webapi.async.{
   Async20EnumCompletionPlugin,
   Async20PayloadCompletionPlugin,
   Async20RuntimeExpressionsCompletionPlugin,
   Async20ShapeTypeFormatCompletionPlugin,
-  Async20TypeFacetsCompletionPlugin
+  Async20TypeFacetsCompletionPlugin,
+  _
 }
 import org.mulesoft.als.suggestions.plugins.aml.webapi.oas.OaslikeSecurityScopesCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.{ResolveDefault, StructureCompletionPlugin}
 import org.mulesoft.als.suggestions.{AMLBaseCompletionPlugins, CompletionsPluginHandler}
 import org.mulesoft.amfintegration.dialect.dialects.asyncapi20.AsyncApi20Dialect
 
@@ -47,11 +41,12 @@ object AsyncApiCompletionPluginRegistry {
       Async20MessageOneOfCompletionPlugin :+
       AsyncApiBindingsCompletionPlugin :+
       BindingsDiscreditableProperties :+
-      AsyncApi20TefTag :+
+      AsyncApi20RefTag :+
       Async20TypeFacetsCompletionPlugin :+
       Async20ShapeTypeFormatCompletionPlugin :+
       Async20EnumCompletionPlugin :+
-      AsyncMessageContentType
+      AsyncMessageContentType :+
+      Async20RequiredObjectCompletionPlugin
 
   def init(): Unit =
     CompletionsPluginHandler.registerPlugins(all, AsyncApi20Dialect().id)
