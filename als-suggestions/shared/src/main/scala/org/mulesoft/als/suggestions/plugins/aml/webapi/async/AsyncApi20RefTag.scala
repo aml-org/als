ae91504baeb5f757e363c8006ee6cba66651db76
agusayerza <agusayerza@gmail.com>
2020-04-06 13:15:45 -0300

    ALS-1054: Suggestions on required properties | JSON Schema

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/AsyncApi20RefTag.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/AsyncApi20RefTag.scala
@@ -3,10 +3,12 @@ package org.mulesoft.als.suggestions.plugins.aml.webapi.async
 import amf.plugins.domain.shapes.models.ScalarShape
 import amf.plugins.domain.webapi.models.Server
 import amf.plugins.domain.webapi.models.bindings.{ChannelBinding, MessageBinding, OperationBinding, ServerBinding}
+import org.mulesoft.als.common.YPartBranch
 import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
 import org.mulesoft.als.suggestions.plugins.aml.AMLRefTagCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.IsInsideRequired
 
-object AsyncApi20TefTag extends AMLRefTagCompletionPlugin {
+object AsyncApi20RefTag extends AMLRefTagCompletionPlugin with IsInsideRequired {
 
   // hack for bindings, has the K: will be a dynamic name. Also, when the refactor of syaml is done, is method should still works (as we will at an empty map?)
   override protected def isObjectDeclarable(params: AmlCompletionRequest): Boolean =
@@ -20,4 +22,6 @@ object AsyncApi20TefTag extends AMLRefTagCompletionPlugin {
       case _              => true
     }
   }
+
+  override def isExceptionCase(branch: YPartBranch): Boolean = isInsideRequired(branch)
 }
