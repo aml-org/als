7ce05049f2584e9fa7604cb704f8bac575fbd6f0
agusayerza <agusayerza@gmail.com>
2020-04-20 12:13:29 -0300

    ALS-1053: apply mediatype on nested properties

---
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/AsyncMediaTypePluginFinder.scala
@@ -0,0 +1,30 @@
+package org.mulesoft.als.suggestions.plugins.aml.webapi.async
+
+import amf.plugins.domain.webapi.models.Payload
+import org.mulesoft.als.suggestions.plugins.aml.webapi.WebApiTypeFacetsCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.jsonSchema.draft7.JsonSchemeDraft7TypeFacetsCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.oas.oas30.Oas30TypeFacetsCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.raml.raml10.Raml10TypeFacetsCompletionPlugin
+
+import scala.util.matching.Regex
+
+trait AsyncMediaTypePluginFinder {
+  val asyncApiRegex: Regex =
+    "(application\\/vnd\\.aai\\.asyncapi(\\+yaml|\\+json)?;?(version=2\\.[0-9]\\.[0-9])?)".r
+  val jsonRegex: Regex =
+    "(application\\/schema(\\+json|\\+yaml)?;?(version=draft-07)?)".r
+  val oas3Regex: Regex =
+    "(application\\/vnd\\.oai\\.openapi(\\+json|\\+yaml)?;?(version=3\\.[0-9]\\.[0-9])?)".r
+  val raml10Regex: Regex =
+    "(application\\/raml(\\+yaml)?;?(version=1(\\.[0-9])?)?)".r
+
+  def findPluginForMediaType(payload: Payload): WebApiTypeFacetsCompletionPlugin = {
+    payload.schemaMediaType.value() match {
+      case asyncApiRegex(_ *) => Async20TypeFacetsCompletionPlugin
+      case jsonRegex(_ *)     => JsonSchemeDraft7TypeFacetsCompletionPlugin
+      case oas3Regex(_ *)     => Oas30TypeFacetsCompletionPlugin
+      case raml10Regex(_ *)   => Raml10TypeFacetsCompletionPlugin
+      case _                  => Async20TypeFacetsCompletionPlugin
+    }
+  }
+}
