a28f52c0718377bcf50c15c33f6326a7adec829b
Hernan Najles <hernan.naj+github@gmail.com>
2020-04-21 14:31:25 -0300

    Map diagnostics with unknow location to the root file. Add drelated information about that situation

--- als-server/shared/src/main/scala/org/mulesoft/als/server/modules/diagnostic/DiagnosticConverters.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/modules/diagnostic/DiagnosticConverters.scala
@@ -8,27 +8,33 @@ import org.mulesoft.als.convert.LspRangeConverter
 import org.mulesoft.als.server.modules.workspace.DiagnosticsBundle
 import org.mulesoft.lsp.feature.common.Location
 import org.mulesoft.lsp.feature.diagnostic.DiagnosticRelatedInformation
+import org.mulesoft.lsp.feature.common.Range
 
 object DiagnosticConverters {
+
+  private val UNKNOWN_LOCATION = "Unknown location for error. Could not retrieve effective location"
   def buildIssueResults(results: Map[String, Seq[AMFValidationResult]],
                         references: Map[String, DiagnosticsBundle],
                         profile: ProfileName): Seq[ValidationReport] = {
 
-    val issuesWithStack = buildIssues(results.values.flatten.toSeq, references)
+    val issuesWithStack = buildIssues(results, references)
     results
       .map(t => ValidationReport(t._1, issuesWithStack.filter(_.filePath == t._1).toSet, profile))
       .toSeq
       .sortBy(_.pointOfViewUri)
   }
 
-  private def buildIssues(results: Seq[AMFValidationResult],
-                          references: Map[String, DiagnosticsBundle]): Seq[ValidationIssue] = {
+  // need to search with path because location of result could be empty?
+  private def buildLocatedIssue(uri: String,
+                                results: Seq[AMFValidationResult],
+                                references: Map[String, DiagnosticsBundle]) = {
     results.flatMap { r =>
-      references.get(r.location.getOrElse("")) match {
+      references.get(uri) match {
         case Some(t)
             if !t.isExternal && t.references.nonEmpty => // Has stack, ain't ExternalFragment todo: check if it's a syntax error?
           t.references.map { stackContainer =>
             buildIssue(
+              uri,
               r,
               stackContainer.stack
                 .map(
@@ -69,20 +75,36 @@ object DiagnosticConverters {
             )
           }
         case _ =>
-          Seq(buildIssue(r, Nil))
+          Seq(buildIssue(uri, r, Nil))
       }
     }
   }
-
-  private def buildIssue(r: AMFValidationResult, stack: Seq[DiagnosticRelatedInformation]): ValidationIssue = {
+  private def buildIssues(results: Map[String, Seq[AMFValidationResult]],
+                          references: Map[String, DiagnosticsBundle]): Seq[ValidationIssue] = {
+    results.flatMap { case (uri, r) => buildLocatedIssue(uri, r, references) }.toSeq
+  }
+  private def buildIssue(iri: String,
+                         r: AMFValidationResult,
+                         stack: Seq[DiagnosticRelatedInformation]): ValidationIssue = {
+    val position = lexicalToPosition(r.position)
     ValidationIssue(r.validationId,
                     ValidationSeverity(r.level),
-                    r.location.getOrElse(""),
+                    r.location.getOrElse(iri),
                     r.message,
-                    lexicalToPosition(r.position),
-                    stack)
+                    position,
+                    stack ++ buildUnknowLocation(iri, r, position))
   }
 
+  private def buildUnknowLocation(iri: String,
+                                  r: AMFValidationResult,
+                                  positionRange: PositionRange): Option[DiagnosticRelatedInformation] = {
+    r.location match {
+      case Some(_) => None
+      case None    => Some(buildDiagnosticRelated(iri, LspRangeConverter.toLspRange(positionRange)))
+    }
+  }
+  private def buildDiagnosticRelated(iri: String, range: Range) =
+    DiagnosticRelatedInformation(Location(iri, range), UNKNOWN_LOCATION)
   private def buildIssue(path: String,
                          range: PositionRange,
                          message: String,
