397455ce1592f5ff9b851e95babbe3952f6af398
llibarona <leandro.libarona@mulesoft.com>
2020-05-07 15:36:46 -0300

    ALS-1075: Async 2 declarations outline

--- als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/factory/webapi/Async20BuilderFactory.scala
+++ als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/factory/webapi/Async20BuilderFactory.scala
@@ -1,3 +1,9 @@
 package org.mulesoft.language.outline.structure.structureImpl.factory.webapi
+import org.mulesoft.language.outline.structure.structureImpl.companion.FieldCompanionList
+import org.mulesoft.language.outline.structure.structureImpl.symbol.webapibuilders.asyncbuilders.Async20DeclaresFieldSymbolBuilderCompanion
 
-object Async20BuilderFactory extends AmfBuilderFactory {}
+object Async20BuilderFactory extends AmfBuilderFactory {
+
+  override protected def companion: FieldCompanionList =
+    super.companion + Async20DeclaresFieldSymbolBuilderCompanion
+}
