cff7a4452eeebf3730135bfec37996a01fe9f04e
llibarona <leandro.libarona@mulesoft.com>
2020-05-08 10:58:28 -0300

    Async payload structure fix

--- als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/symbol/webapibuilders/asyncbuilders/AsyncPayloadFieldSymbolBuilder.scala
+++ als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/symbol/webapibuilders/asyncbuilders/AsyncPayloadFieldSymbolBuilder.scala
@@ -2,10 +2,11 @@ package org.mulesoft.language.outline.structure.structureImpl.symbol.webapibuild
 
 import amf.core.model.domain.AmfArray
 import amf.core.parser.{FieldEntry, Range}
-import amf.plugins.domain.webapi.metamodel.{EndPointModel, PayloadModel}
+import amf.plugins.domain.webapi.metamodel.{EndPointModel, ParametersFieldModel, PayloadModel}
 import amf.plugins.domain.webapi.models.Payload
+import org.mulesoft.als.common.dtoTypes.PositionRange
 import org.mulesoft.amfmanager.AmfImplicits.AmfAnnotationsImp
-import org.mulesoft.language.outline.structure.structureImpl.StructureContext
+import org.mulesoft.language.outline.structure.structureImpl.{DocumentSymbol, KindForResultMatcher, StructureContext}
 import org.mulesoft.language.outline.structure.structureImpl.symbol.builders.fieldbuilders.ArrayFieldTypeSymbolBuilderCompanion
 import org.mulesoft.language.outline.structure.structureImpl.symbol.builders.{
   FieldTypeSymbolBuilder,
@@ -16,8 +17,22 @@ class AsyncPayloadFieldSymbolBuilder(override val element: FieldEntry, override 
     override implicit val ctx: StructureContext)
     extends PayloadFieldSymbolBuilder(element, value) {
 
-  override protected val payloadsLabel = "payload"
+  override protected def buildForKey(key: String, sons: List[DocumentSymbol]): Option[DocumentSymbol] =
+    range
+      .map(PositionRange.apply)
+      .map(
+        r =>
+          DocumentSymbol(
+            key,
+            KindForResultMatcher.kindForField(ParametersFieldModel.QueryParameters),
+            deprecated = false,
+            r,
+            r,
+            sons
+        ))
 
+  override protected val payloadsLabel      = "payload"
+  override def build(): Seq[DocumentSymbol] = payloadSymbols.toSeq
   override protected def range: Option[Range] =
     value.values
       .collectFirst { case p: Payload => p }
