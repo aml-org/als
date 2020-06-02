0467a881c81cc3966f8c4f1ba754a7ae2890b172
Hernan Najles <hernan.naj+github@gmail.com>
2020-06-02 17:59:34 -0300

    Adop new reference targets logic from amf

--- als-server/shared/src/main/scala/org/mulesoft/als/server/modules/workspace/references/visitors/aliases/AliasesVisitor.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/modules/workspace/references/visitors/aliases/AliasesVisitor.scala
@@ -1,12 +1,12 @@
 package org.mulesoft.als.server.modules.workspace.references.visitors.aliases
 
-import amf.core.annotations.{Aliases, ReferenceTargets}
+import amf.core.annotations.Aliases
 import amf.core.model.document.BaseUnit
 import amf.core.model.domain.AmfElement
 import org.mulesoft.als.actions.common.AliasInfo
 import org.mulesoft.als.server.modules.workspace.references.visitors.AmfElementVisitorFactory
+import org.mulesoft.amfmanager.AmfImplicits._
 import org.mulesoft.lsp.feature.common.{Location, Position, Range}
-
 class AliasesVisitor extends AliasesVisitorType {
   private def parserToDtoRange(core: amf.core.parser.Range): Range =
     Range(Position(core.start.line - 1, core.start.column), Position(core.end.line - 1, core.end.column))
@@ -17,14 +17,10 @@ class AliasesVisitor extends AliasesVisitorType {
         bu.annotations
           .find(classOf[Aliases])
           .map { aliases =>
-            val targets = bu.annotations.collect {
-              case rt: ReferenceTargets => rt
-            }
+            val targets = bu.annotations.targets()
             aliases.aliases.flatMap { alias =>
-              targets.find(t => t.targetLocation == alias._2._1).map { tl =>
-                AliasInfo(alias._1,
-                          Location(bu.location().getOrElse(bu.id), parserToDtoRange(tl.originRange)),
-                          tl.targetLocation)
+              targets.get(alias._2._1).map { range =>
+                AliasInfo(alias._1, Location(bu.location().getOrElse(bu.id), parserToDtoRange(range)), alias._2._1)
               }
             }
           }
