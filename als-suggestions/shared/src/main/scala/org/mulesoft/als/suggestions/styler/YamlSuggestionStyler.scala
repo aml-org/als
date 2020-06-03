68a62a4e0468fea930a4da529ca38ce6feaf1f39
llibarona <45210281+llibarona@users.noreply.github.com>
2020-06-03 10:12:22 -0300

    Als 1067 - suggest response codes with quotes (#258)
    * YAMLContentPatcher contemplates quotes on key
    
    * ALS-1067: Patch YAML quoted keys and adjust positions accordingly

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/styler/YamlSuggestionStyler.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/styler/YamlSuggestionStyler.scala
@@ -1,6 +1,8 @@
 package org.mulesoft.als.suggestions.styler
 
+import org.mulesoft.als.common.dtoTypes.PositionRange
 import org.mulesoft.als.suggestions._
+import org.mulesoft.als.suggestions.patcher.QuoteToken
 import org.mulesoft.als.suggestions.styler.astbuilder.{AstRawBuilder, YamlAstRawBuilder}
 import org.yaml.model._
 import org.yaml.render.YamlRender
@@ -9,14 +11,13 @@ case class YamlSuggestionStyler(override val params: StylerParams) extends Sugge
 
   private def fixEmptyMap(rendered: String): String =
     if (rendered.endsWith("{}"))
-      rendered.stripSuffix(" {}") + "\n  " + stringIden
+      rendered.stripSuffix(" {}") + "\n  " + stringIndentation
     else rendered
 
-  private def fixPrefix(prefix: String, text: String) = {
-    if (prefix.isEmpty && text.startsWith(stringIden))
-      text.stripPrefix(stringIden)
+  private def fixPrefix(prefix: String, text: String) =
+    if (prefix.isEmpty && text.startsWith(stringIndentation))
+      text.stripPrefix(stringIndentation)
     else prefix + text
-  }
 
   override protected def render(options: SuggestionStructure, builder: AstRawBuilder): String = {
     val prefix =
@@ -30,6 +31,24 @@ case class YamlSuggestionStyler(override val params: StylerParams) extends Sugge
     }
     fixPrefix(prefix, fixEmptyMap(YamlRender.render(ast, indentation)))
   }
+
+  override def style(raw: RawSuggestion): Styled = super.style(fixTokens(raw))
+
+  private def fixTokens(raw: RawSuggestion): RawSuggestion =
+    if (hasAddedQuotes)
+      raw
+        .withPositionRange(Some(innerSuggestionRange(raw)))
+        .withStringKey
+    else raw
+
+  private def hasAddedQuotes =
+    params.patchedContent.addedTokens.contains(QuoteToken)
+
+  private def innerSuggestionRange(raw: RawSuggestion): PositionRange =
+    if (hasAddedQuotes && raw.options.keyRange != StringScalarRange) { // it has added quotes, and was not originally a string
+      val range = suggestionRange(raw)
+      PositionRange(range.start.moveColumn(-1), range.end.moveColumn(1))
+    } else suggestionRange(raw)
 
   override def astBuilder: RawSuggestion => AstRawBuilder =
     (raw: RawSuggestion) => new YamlAstRawBuilder(raw, false, params.yPartBranch)
