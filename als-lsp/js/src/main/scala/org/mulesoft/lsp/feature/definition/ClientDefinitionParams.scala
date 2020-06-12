09ce19f554659bd7d9b30322044ce2c99d7f44f6
Hernan Najles <hernan.naj+github@gmail.com>
2020-06-12 12:16:27 -0300

    try removing interface

--- als-lsp/js/src/main/scala/org/mulesoft/lsp/feature/definition/ClientDefinitionParams.scala
+++ als-lsp/js/src/main/scala/org/mulesoft/lsp/feature/definition/ClientDefinitionParams.scala
@@ -1,12 +1,16 @@
 package org.mulesoft.lsp.feature.definition
 
 import org.mulesoft.lsp.convert.LspConvertersSharedToClient._
-import org.mulesoft.lsp.feature.common.ClientTextDocumentPositionParams
+import org.mulesoft.lsp.feature.common.{ClientPosition, ClientTextDocumentIdentifier, ClientTextDocumentPositionParams}
 
 import scala.scalajs.js
 
 @js.native
-trait ClientDefinitionParams extends ClientTextDocumentPositionParams
+trait ClientDefinitionParams extends js.Object {
+  def textDocument: ClientTextDocumentIdentifier = js.native
+
+  def position: ClientPosition = js.native
+}
 
 object ClientDefinitionParams {
   def apply(internal: DefinitionParams): ClientDefinitionParams =
