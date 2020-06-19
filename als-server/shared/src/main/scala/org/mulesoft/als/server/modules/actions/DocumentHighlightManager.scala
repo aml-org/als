877ade7689ccff9cd56782b7f6c1398e1bf25b77
llibarona <leandro.libarona@mulesoft.com>
2020-06-19 12:52:39 -0300

    ALS-999: Implemented document highlights

--- als-server/shared/src/main/scala/org/mulesoft/als/server/modules/actions/DocumentHighlightManager.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/modules/actions/DocumentHighlightManager.scala
@@ -1,19 +1,22 @@
 package org.mulesoft.als.server.modules.actions
 
 import amf.core.remote.Platform
+import org.mulesoft.als.actions.references.FindReferences
+import org.mulesoft.als.common.dtoTypes.Position
 import org.mulesoft.als.server.RequestModule
 import org.mulesoft.als.server.logger.Logger
-import org.mulesoft.als.server.workspace.UnitWorkspaceManager
+import org.mulesoft.als.server.workspace.WorkspaceManager
 import org.mulesoft.lsp.ConfigType
 import org.mulesoft.lsp.feature.TelemeteredRequestHandler
-import org.mulesoft.lsp.feature.common.Position
+import org.mulesoft.lsp.feature.common.Location
 import org.mulesoft.lsp.feature.highlight._
 import org.mulesoft.lsp.feature.telemetry.MessageTypes.MessageTypes
 import org.mulesoft.lsp.feature.telemetry.{MessageTypes, TelemetryProvider}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class DocumentHighlightManager(val workspaceManager: UnitWorkspaceManager,
+class DocumentHighlightManager(val workspace: WorkspaceManager,
                                private val telemetryProvider: TelemetryProvider,
                                platform: Platform,
                                private val logger: Logger)
@@ -28,17 +31,19 @@ class DocumentHighlightManager(val workspaceManager: UnitWorkspaceManager,
         DocumentHighlightRequestType
 
       override def task(params: DocumentHighlightParams): Future[Seq[DocumentHighlight]] =
-        documentHighlights(params.textDocument.uri, uuid(params))
+        documentHighlights(params.textDocument.uri, Position(params.position), uuid(params))
 
-      override protected def telemetry: TelemetryProvider                  = telemetryProvider
-      override protected def code(params: DocumentHighlightParams): String = "DocumentHighlight"
+      override protected def telemetry: TelemetryProvider = telemetryProvider
+      override protected def code(params: DocumentHighlightParams): String =
+        "DocumentHighlight"
       override protected def beginType(params: DocumentHighlightParams): MessageTypes =
         MessageTypes.BEGIN_DOCUMENT_HIGHLIGHT
       override protected def endType(params: DocumentHighlightParams): MessageTypes =
         MessageTypes.END_DOCUMENT_HIGHLIGHT
       override protected def msg(params: DocumentHighlightParams): String =
-        s"request for document links on ${params.textDocument.uri}"
-      override protected def uri(params: DocumentHighlightParams): String = params.textDocument.uri
+        s"request for document highlights on ${params.textDocument.uri}"
+      override protected def uri(params: DocumentHighlightParams): String =
+        params.textDocument.uri
     }
   )
 
@@ -46,10 +51,19 @@ class DocumentHighlightManager(val workspaceManager: UnitWorkspaceManager,
 //    Left(config.flatMap(_.dynamicRegistration).getOrElse(false)) // DocumentHighlightOptions not in LSP4J
     Left(true)
 
-  val onDocumentHighlights: (String, String) => Future[Seq[DocumentHighlight]] = documentHighlights
+  def documentHighlights(uri: String, position: Position, uuid: String): Future[Seq[DocumentHighlight]] =
+    workspace
+      .getLastUnit(uri, uuid)
+      .flatMap(_.getLast)
+      .flatMap(_ => {
+        FindReferences
+          .getReferences(uri, position, workspace.getRelationships(uri, uuid))
+          .map(_.map(_.source))
+      }.map(_.filter(_.uri == uri)
+        .map(toDocumentHighlight)))
 
-  def documentHighlights(uri: String, uuid: String): Future[Seq[DocumentHighlight]] =
-    Future.successful(Seq())
+  private def toDocumentHighlight(link: Location): DocumentHighlight =
+    DocumentHighlight(link.range, DocumentHighlightKind.Text)
 
   override def initialize(): Future[Unit] = Future.successful()
 }
