973be982f4fb3e24bbbaf5566c0dd5473190793c
llibarona <leandro.libarona@mulesoft.com>
2020-06-19 13:10:44 -0300

    ALS-999: Added test for Document Highlight

--- als-lsp/shared/src/main/scala/org/mulesoft/lsp/feature/highlight/DocumentHighlightRegistrationOptions.scala
+++ als-lsp/shared/src/main/scala/org/mulesoft/lsp/feature/highlight/DocumentHighlightRegistrationOptions.scala
@@ -1,5 +1,3 @@
 package org.mulesoft.lsp.feature.highlight
 
-import org.eclipse.lsp4j.TextDocumentRegistrationOptions
-
-case class DocumentHighlightRegistrationOptions() extends TextDocumentRegistrationOptions
+case class DocumentHighlightRegistrationOptions()
