d7e1814eeb02d8df1ee0f3b490b23ad8125b8e50
Hernan Najles <hernan.naj+github@gmail.com>
2020-06-30 13:06:27 -0300

    ALS-1117: unify selections range to cover all the object range and not just the name

--- als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/symbol/webapibuilders/EndPointSymbolBuilder.scala
+++ als-structure/shared/src/main/scala/org/mulesoft/language/outline/structure/structureImpl/symbol/webapibuilders/EndPointSymbolBuilder.scala
@@ -1,8 +1,6 @@
 package org.mulesoft.language.outline.structure.structureImpl.symbol.webapibuilders
 
-import amf.core.annotations.LexicalInformation
 import amf.core.metamodel.Field
-import amf.core.parser.Range
 import amf.plugins.domain.webapi.models.EndPoint
 import org.mulesoft.language.outline.structure.structureImpl.StructureContext
 class EndPointSymbolBuilder(override val element: EndPoint)(override implicit val ctx: StructureContext)
@@ -11,7 +9,4 @@ class EndPointSymbolBuilder(override val element: EndPoint)(override implicit va
   override def ignoreFields: List[Field] = super.ignoreFields
   override protected val optionName: Option[String] =
     Some(element.path.value().stripPrefix(element.parent.flatMap(_.path.option()).getOrElse("")))
-
-  override protected val selectionRange: Option[Range] =
-    element.path.annotations().find(classOf[LexicalInformation]).map(l => l.range)
 }
