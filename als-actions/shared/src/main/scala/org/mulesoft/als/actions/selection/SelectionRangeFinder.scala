1126aa412718657073d84447f5da99111fa4cfb4
agusayerza <agusayerza@gmail.com>
2020-06-30 09:08:00 -0300

    Selection range

--- als-actions/shared/src/main/scala/org/mulesoft/als/actions/selection/SelectionRangeFinder.scala
+++ als-actions/shared/src/main/scala/org/mulesoft/als/actions/selection/SelectionRangeFinder.scala
@@ -1,15 +1,11 @@
 package org.mulesoft.als.actions.selection
 
-import amf.core.model.document.BaseUnit
-import amf.core.model.domain.AmfObject
-import org.mulesoft.als.common.{ObjectInTree, ObjectInTreeBuilder}
 import org.mulesoft.als.common.dtoTypes.{Position, PositionRange}
 import org.mulesoft.als.convert.LspRangeConverter._
-import org.mulesoft.lexer.InputRange
 import org.mulesoft.lsp.feature.common
 import org.mulesoft.lsp.feature.selectionRange.SelectionRange
-import org.yaml.model.YNode.MutRef
-import org.yaml.model.{YDocument, YMap, YMapEntry, YNode, YNodePlain, YPart, YSequence}
+import org.yaml.model.{YMapEntry, YPart, YScalar, YSequence}
+import org.mulesoft.lsp.feature.common.{Position => LspPosition, Range => LspRange}
 
 object SelectionRangeFinder {
   def findSelectionRange(yPart: YPart, positions: Seq[Position]): Option[Seq[SelectionRange]] = {
@@ -20,21 +16,27 @@ object SelectionRangeFinder {
     )
   }
 
-  def findSelectionRangeFor(yPart: YPart, position: Position): SelectionRange = {
-    val range              = PositionRange(yPart.range)
+  private def findSelectionRangeFor(yPart: YPart, position: Position): SelectionRange = {
+    val range = PositionRange(yPart.range)
+    println(yPart.getClass.getCanonicalName + ": " + yPart.range)
     val rootSelectionRange = SelectionRange(range, None)
     findSelectionRangeFor(yPart.children, position, rootSelectionRange).getOrElse(rootSelectionRange)
   }
 
-  def findSelectionRangeFor(yPart: Iterable[YPart],
-                            position: Position,
-                            parent: SelectionRange): Option[SelectionRange] = {
+  private def findSelectionRangeFor(yPart: Iterable[YPart],
+                                    position: Position,
+                                    parent: SelectionRange): Option[SelectionRange] = {
     yPart
       .find(p => PositionRange(p.range).contains(position))
       .flatMap(y => {
         y match {
-          case _ @(_: YNodePlain) =>
+          case _ @(_: YMapEntry | _: YSequence) =>
+            println(y.getClass.getCanonicalName + ": " + y.range)
             findSelectionRangeFor(y.children, position, SelectionRange(PositionRange(y.range), Some(parent)))
+
+          case scalar: YScalar =>
+            println(scalar.getClass.getCanonicalName + ": " + scalar.range)
+            findSelectionRangeFor(scalar.children, position, SelectionRange(PositionRange(scalar.range), Some(parent)))
           case _ =>
             // We skip this node
             findSelectionRangeFor(y.children, position, parent)
@@ -44,4 +46,5 @@ object SelectionRangeFinder {
   }
 
   implicit def lspRange(p: PositionRange): common.Range = toLspRange(p)
+
 }
