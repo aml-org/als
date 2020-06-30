2fcc92cd70af23c611a628823fca289ef1451fca
agusayerza <agusayerza@gmail.com>
2020-06-30 14:02:04 -0300

    Solved using only one pass over AST

--- als-actions/shared/src/main/scala/org/mulesoft/als/actions/selection/SelectionRangeFinder.scala
+++ als-actions/shared/src/main/scala/org/mulesoft/als/actions/selection/SelectionRangeFinder.scala
@@ -2,43 +2,44 @@ package org.mulesoft.als.actions.selection
 
 import org.mulesoft.als.common.dtoTypes.{Position, PositionRange}
 import org.mulesoft.als.convert.LspRangeConverter._
+import org.mulesoft.lexer.InputRange
 import org.mulesoft.lsp.feature.common
 import org.mulesoft.lsp.feature.selectionRange.SelectionRange
 import org.yaml.model.{YMapEntry, YPart, YScalar, YSequence}
-import org.mulesoft.lsp.feature.common.{Position => LspPosition, Range => LspRange}
 
 object SelectionRangeFinder {
+
   def findSelectionRange(yPart: YPart, positions: Seq[Position]): Option[Seq[SelectionRange]] = {
-    Some(
-      positions.map(p => {
-        findSelectionRangeFor(yPart, p)
-      })
-    )
+    Some(findSelectionRange(yPart, positions, None))
   }
 
-  private def findSelectionRangeFor(yPart: YPart, position: Position): SelectionRange = {
-    val range              = PositionRange(yPart.range)
-    val rootSelectionRange = SelectionRange(range, None)
-    findSelectionRangeFor(yPart.children, position, rootSelectionRange).getOrElse(rootSelectionRange)
+  private def findSelectionRange(yPart: YPart,
+                                 positions: Seq[Position],
+                                 parent: Option[SelectionRange]): Seq[SelectionRange] = {
+    val range = PositionRange(yPart.range)
+    findSelectionRangeFor(yPart.children, positions, SelectionRange(range, parent))
   }
 
-  private def findSelectionRangeFor(yPart: Iterable[YPart],
-                                    position: Position,
-                                    parent: SelectionRange): Option[SelectionRange] = {
-    yPart
-      .find(p => PositionRange(p.range).contains(position))
-      .flatMap(yPart => {
+  private def findSelectionRangeFor(yParts: Seq[YPart],
+                                    positions: Seq[Position],
+                                    parent: SelectionRange): Seq[SelectionRange] = {
+    // The idea is to solve all the selections for the positions in only 1 pass through the AST
+    yParts.flatMap(yPart => {
+      val containedPositions: Seq[Position] = positions.filter(yPart.range.contains)
+      if (containedPositions.nonEmpty) {
         yPart match {
-          case _ @(_: YMapEntry | _: YSequence | _: YScalar) =>
-            findSelectionRangeFor(yPart.children, position, SelectionRange(PositionRange(yPart.range), Some(parent)))
+          case _ @(_: YMapEntry | _: YSequence) =>
+            findSelectionRange(yPart, containedPositions, Some(parent))
+          case _: YScalar =>
+            Seq(SelectionRange(PositionRange(yPart.range), Some(parent))) // This will get flatted
           case _ =>
             // We skip this node
-            findSelectionRangeFor(yPart.children, position, parent)
+            findSelectionRangeFor(yPart.children, containedPositions, parent)
         }
-      })
-      .orElse(Some(parent))
+      } else None
+    })
   }
 
-  implicit def lspRange(p: PositionRange): common.Range = toLspRange(p)
-
+  implicit def lspRange(p: PositionRange): common.Range          = toLspRange(p)
+  implicit def inputRangeConverter(p: InputRange): PositionRange = PositionRange(p)
 }
