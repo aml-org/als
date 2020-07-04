46f25619f89fd0317d5a8bfff998a6f9623add5d
llibarona <leandro.libarona@mulesoft.com>
2020-07-04 16:16:21 -0300

    ALS-1164, ALS-1150, ALS-1153, ALS-1154: Changed logic in iterator for visitors, in order to include abstract traits and resource types. Also changed visitors to accomodate this new information

---
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/modules/workspace/references/visitors/noderelationship/plugins/AbstractDefinitionLinksVisitor.scala
@@ -0,0 +1,46 @@
+package org.mulesoft.als.server.modules.workspace.references.visitors.noderelationship.plugins
+
+import amf.core.model.domain.templates.ParametrizedDeclaration
+import amf.core.model.domain.{AmfArray, AmfElement}
+import amf.core.parser.FieldEntry
+import amf.core.vocabulary.Namespace
+import amf.plugins.domain.webapi.models.{EndPoint, Operation}
+import org.mulesoft.als.actions.common.RelationshipLink
+import org.mulesoft.als.server.modules.workspace.references.visitors.AmfElementVisitorFactory
+import org.mulesoft.als.server.modules.workspace.references.visitors.noderelationship.NodeRelationshipVisitorType
+import org.mulesoft.amfintegration.AmfImplicits._
+
+class AbstractDefinitionLinksVisitor extends NodeRelationshipVisitorType {
+  override protected def innerVisit(element: AmfElement): Seq[RelationshipLink] =
+    element match {
+      case o: Operation =>
+        extractFromEntries(o.fields.fields())
+      case e: EndPoint =>
+        extractFromEntries(e.fields.fields())
+      case _ => Nil
+    }
+
+  private def extractFromEntries(entries: Iterable[FieldEntry]): Seq[RelationshipLink] =
+    entries
+      .find(fe => fe.field.value == Namespace.Document + "extends")
+      .map(parametrizedDeclarationTargetsWithPosition)
+      .getOrElse(Nil)
+
+  private def parametrizedDeclarationTargetsWithPosition(fe: FieldEntry): Seq[RelationshipLink] =
+    fe.value.value match {
+      case array: AmfArray =>
+        array.values.flatMap {
+          case p: ParametrizedDeclaration =>
+            p.annotations
+              .ast()
+              .flatMap(source => p.target.annotations.ast().map(target => (source, target)))
+              .map(t => RelationshipLink(t._1, t._2))
+          case _ => None
+        }
+      case _ => Nil
+    }
+}
+
+object AbstractDefinitionLinksVisitor extends AmfElementVisitorFactory {
+  override def apply(): AbstractDefinitionLinksVisitor = new AbstractDefinitionLinksVisitor()
+}
--- als-server/shared/src/test/scala/org/mulesoft/als/server/modules/rename/raml/ServerRenameTests.scala
+++ als-server/shared/src/test/scala/org/mulesoft/als/server/modules/rename/raml/ServerRenameTests.scala
@@ -7,4 +7,8 @@ class ServerRenameTests extends RAMLRenameTest {
   test("test 003") { runTest("test003/api.yaml", "RENAMED") }
   test("test 004") { runTest("test004/api.yaml", "RENAMED") }
   test("test 005") { runTest("test005/api.yaml", "RENAMED") }
+  test("test 006") { runTest("test006/api.raml", "RENAMED") }
+  test("test 007") { runTest("test007/api.raml", "RENAMED") }
+  test("test 008") { runTest("test008/api.raml", "RENAMED") }
+  ignore("test 009") { runTest("test009/api.raml", "RENAMED") }
 }
