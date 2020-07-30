3c28c4e52dc9a23105cc210403e5a17b22c60cb9
agusayerza <agusayerza@gmail.com>
2020-07-30 14:06:34 -0300

    Default value excluded

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/patched/PatchedSuggestions.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/patched/PatchedSuggestions.scala
@@ -8,6 +8,9 @@ import org.mulesoft.amfintegration.dialect.dialects.oas.{OAS20Dialect, OAS30Dial
 import org.mulesoft.amfintegration.dialect.dialects.raml.raml08.Raml08TypesDialect
 import org.mulesoft.amfintegration.dialect.dialects.raml.raml10.Raml10TypesDialect
 
+/**
+  * @param nonPlain nonPlain suggestions will have double quotes added to them
+  */
 case class PatchedSuggestion(text: String,
                              description: Option[String] = None,
                              isObj: Boolean = false,
@@ -58,9 +61,11 @@ object PatchedSuggestionsForDialect {
       FieldForClass(PayloadModel.`type`.head.iri(), PayloadModel.MediaType.value.iri()) ->
         Map("KnownValues" -> Oas30CommonMediaTypes.all.map(PatchedSuggestion(_))),
       FieldForClass(ResponseModel.`type`.head.iri(), ResponseModel.StatusCode.value.iri()) ->
-        Map("KnownValues" -> Oas30ResponseCodes.all.map(PatchedSuggestion(_, isObj = true, nonPlain = true))),
+        Map("KnownValues" -> Oas30ResponseCodes.all.map(v =>
+          PatchedSuggestion(v, isObj = true, nonPlain = !Oas30ResponseCodes.notQuoted.contains(v)))),
       FieldForClass(ResponseModel.`type`.head.iri(), ResponseModel.Name.value.iri()) ->
-        Map("KnownValues" -> Oas30ResponseCodes.all.map(PatchedSuggestion(_, isObj = true, nonPlain = true)))
+        Map("KnownValues" -> Oas30ResponseCodes.all.map(v =>
+          PatchedSuggestion(v, isObj = true, nonPlain = !Oas30ResponseCodes.notQuoted.contains(v))))
     ) ++ webApiClasses
 
   private val asyncApi20Classes: Map[FieldForClass, Map[String, Seq[PatchedSuggestion]]] = {
