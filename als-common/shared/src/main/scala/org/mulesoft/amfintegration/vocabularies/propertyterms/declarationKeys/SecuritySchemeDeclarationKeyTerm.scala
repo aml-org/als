e722a3345cab943103c3c9682b797201ccd40be9
Hernan Najles <hernan.naj+github@gmail.com>
2020-08-27 15:32:39 -0300

    ALS-1236 override semantic terms for declaration keys

---
+++ als-common/shared/src/main/scala/org/mulesoft/amfintegration/vocabularies/propertyterms/declarationKeys/SecuritySchemeDeclarationKeyTerm.scala
@@ -0,0 +1,9 @@
+package org.mulesoft.amfintegration.vocabularies.propertyterms.declarationKeys
+
+import org.mulesoft.amfintegration.vocabularies.PropertyTermObjectNode
+
+object SecuritySchemeDeclarationKeyTerm extends PropertyTermObjectNode {
+  // This one i'm not so sure
+  override val name: String        = "SecuritySchemeDeclarationKey"
+  override val description: String = "Declarations of reusable authentication mechanisms to ensure secured operations"
+}
---
+++ als-common/shared/src/main/scala/org/mulesoft/amfintegration/vocabularies/propertyterms/declarationKeys/TraitDeclarationKeyTerm.scala
@@ -0,0 +1,9 @@
+package org.mulesoft.amfintegration.vocabularies.propertyterms.declarationKeys
+
+import org.mulesoft.amfintegration.vocabularies.PropertyTermObjectNode
+
+object TraitDeclarationKeyTerm extends PropertyTermObjectNode {
+  // This one i'm not so sure
+  override val name: String        = "TraitDeclarationKey"
+  override val description: String = "Declarations of sets of characteristics to reuse across multiple methods"
+}
