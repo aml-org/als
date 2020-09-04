00d75cf68ff6eb118515f463426156ad0554943b
llibarona <leandro.libarona@mulesoft.com>
2020-09-04 14:16:49 -0300

    Fix ObjectInTree tests

--- als-common/shared/src/test/scala/org/mulesoft/als/common/objectintree/ObjectInTreeBaseTest.scala
+++ als-common/shared/src/test/scala/org/mulesoft/als/common/objectintree/ObjectInTreeBaseTest.scala
@@ -16,46 +16,39 @@ case class ObjectInTreeBaseTest(instanceFile: String, dialectFile: String) exten
 
   private val instance = AmfInstance.default
 
-  private val initDialects: Future[Unit] =
-    platform
-      .resolve(uriTemplate(dialectFile))
-      .map(_.stream.toString)
-      .flatMap(
-        dialectContent =>
-          instance
-            .init()
-            .flatMap(_ => instance.alsAmlPlugin.registry.registerDialect(dialectContent)))
-      .flatMap(_ => Future.unit)
+  private val initDialects: Future[Unit] = for {
+    dialectContent <- platform.resolve(uriTemplate(dialectFile)).map(_.stream.toString)
+    _              <- instance.init()
+    _              <- instance.alsAmlPlugin.registry.registerDialect(dialectContent)
+  } yield {}
 
-  private val eventualResult = instance.parse(uriTemplate(instanceFile))
+  private val eventualResult = initDialects.flatMap(_ => instance.parse(uriTemplate(instanceFile)))
+
   private val objectInTree: Future[parser.Position => ObjectInTree] =
-    for {
-      _      <- initDialects
-      result <- eventualResult
-    } yield {
-      ObjectInTreeBuilder.fromUnit(result.baseUnit, _)
-    }
+    eventualResult.map(result => ObjectInTreeBuilder.fromUnit(result.baseUnit, _))
 
   def runTest(pos: Position, expectedTypeIri: String, expectedPropertyTerm: Option[String]): Future[Assertion] = {
     objectInTree.map { fn =>
       val tree = fn(pos.toAmfPosition)
-      expectedPropertyTerm.foreach { pt =>
-        val fieldEntry = tree.fieldEntry.map(_.field.toString())
-        fieldEntry should contain(pt)
-      }
-      expectedPropertyTerm match {
-        case Some(pt) =>
-          val fieldEntry = tree.fieldEntry.map(_.field.toString())
-          fieldEntry should contain(pt)
-        case None =>
-          tree.fieldEntry should be(None)
-      }
-      tree.obj.meta match {
-        case ddem: DialectDomainElementModel =>
-          ddem.typeIri should contain(expectedTypeIri)
-        case _ =>
-          fail("Not a DialectDomainElementModel")
-      }
+      assertPropertyTerm(expectedPropertyTerm, tree)
+      assertTypeIri(expectedTypeIri, tree)
     }
   }
+
+  private def assertTypeIri(expectedTypeIri: String, tree: ObjectInTree) =
+    tree.obj.meta match {
+      case ddem: DialectDomainElementModel =>
+        ddem.typeIri should contain(expectedTypeIri)
+      case _ =>
+        fail("Not a DialectDomainElementModel")
+    }
+
+  private def assertPropertyTerm(expectedPropertyTerm: Option[String], tree: ObjectInTree) =
+    expectedPropertyTerm match {
+      case Some(pt) =>
+        val fieldEntry = tree.fieldEntry.map(_.field.toString())
+        fieldEntry should contain(pt)
+      case None =>
+        tree.fieldEntry should be(None)
+    }
 }
