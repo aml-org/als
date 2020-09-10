41c7127b7316a6ebd61a07e85dfda4ef4d4610a5
agusayerza <agusayerza@gmail.com>
2020-09-10 13:30:31 -0300

    ALS-1140: Single bracket Json suggestions

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/HeaderCompletionParams.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/HeaderCompletionParams.scala
@@ -1,6 +1,11 @@
 package org.mulesoft.als.suggestions
 
 import org.mulesoft.als.common.dtoTypes.Position
+import org.mulesoft.als.configuration.AlsConfigurationReader
 import org.mulesoft.amfintegration.AmfInstance
 
-case class HeaderCompletionParams(uri: String, content: String, position: Position, amfInstance: AmfInstance)
+case class HeaderCompletionParams(uri: String,
+                                  content: String,
+                                  position: Position,
+                                  amfInstance: AmfInstance,
+                                  configuration: AlsConfigurationReader)
--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/HeaderCompletionProviderBuilder.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/HeaderCompletionProviderBuilder.scala
@@ -1,14 +1,20 @@
 package org.mulesoft.als.suggestions
 
 import org.mulesoft.als.common.dtoTypes.Position
+import org.mulesoft.als.configuration.AlsConfigurationReader
 import org.mulesoft.amfintegration.AmfInstance
 
 object HeaderCompletionProviderBuilder extends BasicPrefixExtractor {
-  def build(uri: String, header: String, position: Position, amfInstance: AmfInstance): BasicCompletionProvider =
+  def build(uri: String,
+            header: String,
+            position: Position,
+            amfInstance: AmfInstance,
+            configuration: AlsConfigurationReader): BasicCompletionProvider =
     new BasicCompletionProvider(
       getPrefix(header, position).dropWhile(_ == ' '),
       position,
       () =>
-        CompletionPluginsRegistryHeaders.pluginSuggestions(HeaderCompletionParams(uri, header, position, amfInstance))
+        CompletionPluginsRegistryHeaders.pluginSuggestions(
+          HeaderCompletionParams(uri, header, position, amfInstance, configuration))
     )
 }
