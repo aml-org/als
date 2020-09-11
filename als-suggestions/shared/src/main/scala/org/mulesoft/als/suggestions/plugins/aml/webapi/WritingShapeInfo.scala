018efdc4bbe56c133144ad8bef3408fc9c01418f
llibarona <leandro.libarona@mulesoft.com>
2020-09-11 14:14:00 -0300

    Fix in Parameter suggestions

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/WritingShapeInfo.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/WritingShapeInfo.scala
@@ -3,19 +3,13 @@ package org.mulesoft.als.suggestions.plugins.aml.webapi
 import amf.core.model.domain.{AmfObject, Shape}
 import amf.plugins.domain.webapi.models.Parameter
 import org.mulesoft.als.common.YPartBranch
-import org.yaml.model.YMapEntry
+import org.mulesoft.als.suggestions.plugins.NonPatchHacks
 
-trait WritingShapeInfo {
+trait WritingShapeInfo extends NonPatchHacks {
   protected def isWritingFacet(yPartBranch: YPartBranch, shape: Shape, stack: Seq[AmfObject]): Boolean =
-    (jsonHackNotValue(yPartBranch) || nonPatchNotValue(yPartBranch)) && !yPartBranch.isKeyDescendantOf("required") && !writingShapeName(
-      shape,
-      yPartBranch) && !writingParamName(stack, yPartBranch) && !yPartBranch.parentEntryIs("properties")
-
-  private def nonPatchNotValue(yPartBranch: YPartBranch) =
-    !yPartBranch.isJson && !yPartBranch.isValue
-
-  private def jsonHackNotValue(yPartBranch: YPartBranch) =
-    yPartBranch.isJson && yPartBranch.isKey
+    notValue(yPartBranch) && !yPartBranch.isKeyDescendantOf("required") && !writingShapeName(shape, yPartBranch) && !writingParamName(
+      stack,
+      yPartBranch) && !yPartBranch.parentEntryIs("properties")
 
   protected def writingShapeName(shape: Shape, yPartBranch: YPartBranch): Boolean =
     shape.name.value() == yPartBranch.stringValue
