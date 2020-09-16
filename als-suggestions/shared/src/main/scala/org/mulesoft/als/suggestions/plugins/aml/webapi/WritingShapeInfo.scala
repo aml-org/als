ef2eea324a65680d0e9e3b904503351f7ab5f8c6
llibarona <leandro.libarona@mulesoft.com>
2020-09-16 13:54:26 -0300

    fix inclusion suggestions

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/WritingShapeInfo.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/WritingShapeInfo.scala
@@ -1,15 +1,21 @@
 package org.mulesoft.als.suggestions.plugins.aml.webapi
 
 import amf.core.model.domain.{AmfObject, Shape}
+import amf.plugins.document.vocabularies.model.document.Dialect
 import amf.plugins.domain.webapi.models.Parameter
 import org.mulesoft.als.common.YPartBranch
 import org.mulesoft.als.suggestions.plugins.NonPatchHacks
+import org.mulesoft.amfintegration.dialect.DialectKnowledge
 
 trait WritingShapeInfo extends NonPatchHacks {
-  protected def isWritingFacet(yPartBranch: YPartBranch, shape: Shape, stack: Seq[AmfObject]): Boolean =
+  protected def isWritingFacet(yPartBranch: YPartBranch,
+                               shape: Shape,
+                               stack: Seq[AmfObject],
+                               actualDialect: Dialect): Boolean =
     notValue(yPartBranch) && !yPartBranch.isKeyDescendantOf("required") && !writingShapeName(shape, yPartBranch) && !writingParamName(
       stack,
-      yPartBranch) && !yPartBranch.parentEntryIs("properties")
+      yPartBranch) && !yPartBranch.parentEntryIs("properties") &&
+      !DialectKnowledge.isInclusion(yPartBranch, actualDialect)
 
   protected def writingShapeName(shape: Shape, yPartBranch: YPartBranch): Boolean =
     shape.name.value() == yPartBranch.stringValue
