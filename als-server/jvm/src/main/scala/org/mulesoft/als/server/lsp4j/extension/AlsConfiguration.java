2689e20baad7e55e4585a08545789c181bf1c0c3
agusayerza <agusayerza@gmail.com>
2020-10-19 22:26:04 -0300

    Fix AlsConfiguration

--- als-server/jvm/src/main/scala/org/mulesoft/als/server/lsp4j/extension/AlsConfiguration.java
+++ als-server/jvm/src/main/scala/org/mulesoft/als/server/lsp4j/extension/AlsConfiguration.java
@@ -1,14 +1,22 @@
 package org.mulesoft.als.server.lsp4j.extension;
 
-import org.eclipse.lsp4j.FormattingOptions;
-
 import java.util.Map;
 
 public class AlsConfiguration {
     private Map<String, AlsFormattingOptions> formattingOptions;
 
+    public AlsConfiguration(Map<String, AlsFormattingOptions> formattingOptions) {
+        this.formattingOptions = formattingOptions;
+    }
+
+    public AlsConfiguration() {
+    }
+
     public Map<String, AlsFormattingOptions> getFormattingOptions() {
         return formattingOptions;
     }
 
+    public void setFormattingOptions(Map<String, AlsFormattingOptions> formattingOptions) {
+        this.formattingOptions = formattingOptions;
+    }
 }
