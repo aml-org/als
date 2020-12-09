9a2b1e661ea5e83bf67c6e6ab13383507c2596ef
llibarona <leandro.libarona@mulesoft.com>
2020-12-09 11:36:57 -0300

    refactor  method for same vendor in object and baseunit

--- als-actions/shared/src/main/scala/org/mulesoft/als/actions/codeactions/plugins/declarations/samefile/ExtractSameFileDeclaration.scala
+++ als-actions/shared/src/main/scala/org/mulesoft/als/actions/codeactions/plugins/declarations/samefile/ExtractSameFileDeclaration.scala
@@ -54,21 +54,19 @@ trait ExtractSameFileDeclaration extends CodeActionResponsePlugin with BaseEleme
                      yamlOptions)
       .map(de => TextEdit(rangeFromEntryBottom(de._2), s"\n${de._1}\n"))
 
-  def applies(params: CodeActionRequestParams): Boolean =
+  protected lazy val homogeneousVendor: Boolean =
     maybeTree.flatMap(_.objVendor).forall(params.bu.sourceVendor.contains)
 
-  override protected def task(params: CodeActionRequestParams): Future[Seq[AbstractCodeAction]] = {
-    if (applies(params))
-      linkEntry.map {
-        _.flatMap(e => declaredElementTextEdit.map(Seq(e, _)))
-          .map(edits => {
-            kindTitle.baseCodeAction(
-              AbstractWorkspaceEdit(
-                Seq(Left(TextDocumentEdit(VersionedTextDocumentIdentifier(params.uri, None), edits)))))
-          })
-          .toSeq
-      } else Future.successful(Seq.empty)
-  }
+  override protected def task(params: CodeActionRequestParams): Future[Seq[AbstractCodeAction]] =
+    linkEntry.map {
+      _.flatMap(e => declaredElementTextEdit.map(Seq(e, _)))
+        .map(edits => {
+          kindTitle.baseCodeAction(
+            AbstractWorkspaceEdit(
+              Seq(Left(TextDocumentEdit(VersionedTextDocumentIdentifier(params.uri, None), edits)))))
+        })
+        .toSeq
+    }
 
   override protected lazy val renderLink: Future[Option[YNode]] = Future {
     amfObject
