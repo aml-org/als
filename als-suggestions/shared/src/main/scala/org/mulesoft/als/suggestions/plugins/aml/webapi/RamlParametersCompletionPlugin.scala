e919f7ff41cf1d327e5bdc8e33c972d1655e9d41
Lele <45210281+llibarona@users.noreply.github.com>
2021-01-25 15:36:20 -0300

    ALS-1261: add inlined type suggestion for headers in RAML 1.0 (#450)

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/RamlParametersCompletionPlugin.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/RamlParametersCompletionPlugin.scala
@@ -4,32 +4,33 @@ import amf.core.annotations.SynthesizedField
 import amf.core.metamodel.domain.ShapeModel
 import amf.core.model.domain.Shape
 import amf.plugins.document.vocabularies.model.domain.PropertyMapping
-import amf.plugins.domain.shapes.models.UnresolvedShape
-import amf.plugins.domain.webapi.models.{Parameter, Payload}
-import org.mulesoft.als.common.SemanticNamedElement._
+import amf.plugins.domain.webapi.models.Parameter
 import org.mulesoft.als.suggestions.RawSuggestion
 import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
 import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
-import org.mulesoft.als.suggestions.plugins.aml.{AMLRamlStyleDeclarationsReferences, BooleanSuggestions}
-import org.yaml.model.YMapEntry
 import org.mulesoft.amfintegration.AmfImplicits._
+import org.mulesoft.amfintegration.dialect.dialects.raml.raml10.Raml10TypesDialect
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-trait ShapeDeclarationReferenceCompletionPlugin extends AMLCompletionPlugin with DeclaredTypesSuggestions {
+object RamlParametersCompletionPlugin extends AMLCompletionPlugin with DeclaredTypesSuggestions {
 
   override def resolve(params: AmlCompletionRequest): Future[Seq[RawSuggestion]] = {
     Future {
       params.amfObject match {
-        case s: Shape if params.yPartBranch.isValue =>
-          val iri = extractIri(params, s)
+        case s: Parameter if params.yPartBranch.isValue =>
+          val iri =
+            extractIri(params, s)
           val declaredSuggestions =
-            getDeclaredSuggestions(params, s.name.option(), iri)
-
+            getDeclaredSuggestions(params, None, iri)
           suggestAllTypes(params, declaredSuggestions)
         case _ => Nil
       }
     }
   }
+
+  override def typeProperty: PropertyMapping = Raml10TypesDialect.shapeTypesProperty
+
+  override def id: String = "RamlParametersCompletionPlugin"
 }
