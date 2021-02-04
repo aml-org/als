cba7d0cfe79c0a7a89e1ef0d1083b920ebeaae3b
Lele <45210281+llibarona@users.noreply.github.com>
2021-02-04 17:38:43 -0300

    publish dist/ for als-server.js (#457)

--- als-server/js/node-package/scripts/publish.sh
+++ als-server/js/node-package/scripts/publish.sh
@@ -23,7 +23,7 @@ echo "Connecting to MuleSoft internal registry"
 printf "@mulesoft:registry=https://nexus3.build.msap.io/repository/npm-internal/\n//nexus3.build.msap.io/repository/npm-internal/:_authToken=$NPM_TOKEN" > .npmrc
 touch .npmrc
 
-LATEST_TAG=`npm v @mulesoft/als-node-client dist-tags.releases`
+LATEST_TAG=`npm v @mulesoft/als-server dist-tags.releases`
 
 echo "Repo latest tag: $LATEST_TAG"
 
