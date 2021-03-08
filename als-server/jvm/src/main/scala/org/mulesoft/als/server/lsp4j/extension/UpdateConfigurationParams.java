4b05561c760254dde9421ab74e177565362fd036
Lele <45210281+llibarona@users.noreply.github.com>
2021-03-08 12:50:56 -0300

    Remove unused property (#478)

--- als-server/jvm/src/main/scala/org/mulesoft/als/server/lsp4j/extension/UpdateConfigurationParams.java
+++ als-server/jvm/src/main/scala/org/mulesoft/als/server/lsp4j/extension/UpdateConfigurationParams.java
@@ -1,5 +1,7 @@
 package org.mulesoft.als.server.lsp4j.extension;
 
+import org.mulesoft.als.configuration.TemplateTypes;
+
 import java.util.Map;
 
 public class UpdateConfigurationParams {
@@ -10,8 +12,13 @@ public class UpdateConfigurationParams {
 
     private Map<String, Object> genericOptions;
 
-    public UpdateConfigurationParams(Map<String, AlsFormattingOptions> updateFormatOptionsParams, Boolean disableTemplates) {
+    public UpdateConfigurationParams(Map<String, AlsFormattingOptions> updateFormatOptionsParams) {
         this.formattingOptions = updateFormatOptionsParams;
+    }
+
+    public UpdateConfigurationParams(Map<String, AlsFormattingOptions> updateFormatOptionsParams, String templateType) {
+        this.formattingOptions = updateFormatOptionsParams;
+        this.templateType = templateType;
     }
 
     public Map<String, AlsFormattingOptions> getUpdateFormatOptionsParams() {
