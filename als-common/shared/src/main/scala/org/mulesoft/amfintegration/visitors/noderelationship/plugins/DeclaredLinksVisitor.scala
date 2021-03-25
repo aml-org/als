07794f061bd3975cde700939a8f9f9f730b1cd8d
agusayerza <agusayerza@gmail.com>
2021-03-25 10:32:57 -0300

    Prevent null pointer on null security scheme

--- als-common/shared/src/main/scala/org/mulesoft/amfintegration/visitors/noderelationship/plugins/DeclaredLinksVisitor.scala
+++ als-common/shared/src/main/scala/org/mulesoft/amfintegration/visitors/noderelationship/plugins/DeclaredLinksVisitor.scala
@@ -35,14 +35,13 @@ class DeclaredLinksVisitor extends NodeRelationshipVisitorType {
   private def extractSecuritySchemes(sr: SecurityRequirement) =
     sr.schemes
       .flatMap(source => {
-        val target = source.scheme
-        target.annotations.ast().map(targetAST => (source, target, targetAST))
+        val optionTarget = Option(source.scheme)
+        optionTarget.flatMap(t => t.annotations.ast().map(targetAST => (source, t, targetAST)))
       })
-      .flatMap(
-        triple =>
-          triple._1.annotations
-            .ast()
-            .map(sourceAST => RelationshipLink(sourceAST, triple._3, getName(triple._2), getName(triple._1))))
+      .flatMap(triple =>
+        triple._1.annotations
+          .ast()
+          .map(sourceAST => RelationshipLink(sourceAST, triple._3, getName(triple._2), getName(triple._1))))
 
   private def extractInherits(obj: AmfObject) =
     obj.fields
