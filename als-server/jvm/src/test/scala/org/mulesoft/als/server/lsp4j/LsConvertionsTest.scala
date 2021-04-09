64832953f4883922ed83160906da0efad40d26b7
emilianoascona <emiliano.ascona@gmail.com>
2021-04-09 11:04:56 -0300

    Adding optional value for property "Code"

--- als-server/jvm/src/test/scala/org/mulesoft/als/server/lsp4j/LsConvertionsTest.scala
+++ als-server/jvm/src/test/scala/org/mulesoft/als/server/lsp4j/LsConvertionsTest.scala
@@ -7,7 +7,9 @@ import org.mulesoft.als.server.protocol.configuration
 import org.mulesoft.lsp.LspConversions._
 import org.mulesoft.lsp.configuration.{TextDocumentClientCapabilities => InternalDocumentClient}
 import org.scalatest.FunSuite
+import org.mulesoft.lsp.feature.diagnostic.{Diagnostic => InternalDiagnostic}
 
+import java.util
 import scala.collection.JavaConverters._
 class LsConvertionsTest extends FunSuite {
 
@@ -44,6 +46,39 @@ class LsConvertionsTest extends FunSuite {
     documentClient.setRename(new RenameCapabilities())
     val internal: InternalDocumentClient = documentClient
     succeed
+  }
+
+  test("Full Diagnostic conversion") {
+    val clientDiagnostic: Diagnostic = new Diagnostic()
+    clientDiagnostic.setCode(2)
+    clientDiagnostic.setMessage("Message")
+    clientDiagnostic.setTags(new util.ArrayList[DiagnosticTag])
+    val start = new Position(0, 1)
+    val end   = new Position(1, 1)
+    clientDiagnostic.setRange(new Range(start, end))
+    clientDiagnostic.setRelatedInformation(new util.ArrayList[DiagnosticRelatedInformation])
+    clientDiagnostic.setSeverity(DiagnosticSeverity.Information)
+    clientDiagnostic.setSource("Source")
+    val internalDiagnostic: InternalDiagnostic = clientDiagnostic
+    succeed
+  }
+
+  test("Diagnostic Only mandatory fields conversion") {
+    val start                                  = new Position(0, 1);
+    val end                                    = new Position(1, 1);
+    val clientDiagnostic                       = new Diagnostic(new Range(start, end), "Message")
+    val internalDiagnostic: InternalDiagnostic = clientDiagnostic
+    succeed
+  }
+
+  test("Empty clientDiagnostic conversion") {
+    val clientDiagnostic = new Diagnostic()
+    try {
+      val internalDiagnostic: InternalDiagnostic = clientDiagnostic
+    } catch {
+      case _: NullPointerException => succeed
+      case e                       => fail("Unexpected error on empty clientDiagnostic conversion test", e)
+    }
   }
 
 }
