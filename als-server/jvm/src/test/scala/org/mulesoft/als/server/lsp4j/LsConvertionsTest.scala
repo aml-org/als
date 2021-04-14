25030f1b47981d4b13828a482915d85806edb38f
emilianoascona <emiliano.ascona@gmail.com>
2021-04-14 10:14:09 -0300

    Adding tests for Diagnostic conversions

--- als-server/jvm/src/test/scala/org/mulesoft/als/server/lsp4j/LsConvertionsTest.scala
+++ als-server/jvm/src/test/scala/org/mulesoft/als/server/lsp4j/LsConvertionsTest.scala
@@ -7,7 +7,9 @@ import org.mulesoft.als.server.protocol.configuration
 import org.mulesoft.lsp.LspConversions._
 import org.mulesoft.lsp.configuration.{TextDocumentClientCapabilities => InternalDocumentClient}
 import org.scalatest.FunSuite
+import org.mulesoft.lsp.feature.diagnostic.{Diagnostic => InternalDiagnostic}
 
+import java.util
 import scala.collection.JavaConverters._
 class LsConvertionsTest extends FunSuite {
 
@@ -44,6 +46,40 @@ class LsConvertionsTest extends FunSuite {
     documentClient.setRename(new RenameCapabilities())
     val internal: InternalDocumentClient = documentClient
     succeed
+  }
+
+  test("Full Diagnostic conversion") {
+    val diagnostic = new Diagnostic()
+    diagnostic.setCode(2)
+    diagnostic.setMessage("Message")
+    diagnostic.setTags( new util.ArrayList[DiagnosticTag])
+    val start = new Position(0,1)
+    val end = new Position(1,1)
+    diagnostic.setRange(new Range(start, end))
+    diagnostic.setRelatedInformation(new util.ArrayList[DiagnosticRelatedInformation])
+    diagnostic.setSeverity(DiagnosticSeverity.Information)
+    diagnostic.setSource("Source")
+    val internalDiagnostic: InternalDiagnostic = diagnostic
+    succeed
+  }
+
+  test("Diagnostic Only mandatory fields conversion") {
+    val start = new Position(0,1);
+    val end = new Position(1,1);
+    val diagnostic = new Diagnostic(new Range(start, end), "Message")
+    val internalDiagnostic: InternalDiagnostic = diagnostic
+    succeed
+  }
+
+  test("Empty diagnostic conversion") {
+    val diagnostic = new Diagnostic()
+    try{
+      val internalDiagnostic: InternalDiagnostic = diagnostic
+    }
+    catch {
+      case _: NullPointerException => succeed
+      case e => fail("Unexpected error on empty diagnostic conversion test", e)
+    }
   }
 
 }
