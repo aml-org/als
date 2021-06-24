63813c2651d3e0eda81e4aa5e225cfb8361b58bd
agusayerza <agusayerza@gmail.com>
2021-06-24 15:22:36 -0300

    Fix JS/JVM interface pairs

--- als-server/jvm/src/main/scala/org/mulesoft/als/server/custom/CustomEvents.java
+++
@@ -1,9 +0,0 @@
-package org.mulesoft.als.server.custom;
-
-import org.eclipse.lsp4j.jsonrpc.services.JsonNotification;
-import org.mulesoft.als.server.protocol.textsync.DidFocusParams;
-
-public interface CustomEvents {
-    @JsonNotification
-    void didFocus(DidFocusParams params);
-}
