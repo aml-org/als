b8ca1fbadba074c7e93553292f5fb5e02d97c623
agusayerza <agusayerza@gmail.com>
2021-07-01 13:20:32 -0300

    ALS-1563: UnionNodeMapping and NodeMapping suggestions improvements

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/AMLUnionCompletionPlugin.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/AMLUnionCompletionPlugin.scala
@@ -1,14 +1,16 @@
 package org.mulesoft.als.suggestions.plugins.aml
 
-import amf.core.model.domain.AmfObject
+import amf.core.model.domain.{AmfObject, DomainElement}
 import amf.plugins.document.vocabularies.annotations.DiscriminatorField
 import amf.plugins.document.vocabularies.model.document.Dialect
 import amf.plugins.document.vocabularies.model.domain.{
   DialectDomainElement,
+  NodeMappable,
   NodeMapping,
   PropertyMapping,
   UnionNodeMapping
 }
+import org.mulesoft.als.common.YPartBranch
 import org.mulesoft.als.suggestions.RawSuggestion
 import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
 import org.mulesoft.als.suggestions.interfaces.ResolveIfApplies
@@ -22,11 +24,11 @@ object AMLUnionCompletionPlugin extends ResolveIfApplies {
 }
 
 class AMLUnionCompletionPlugin(params: AmlCompletionRequest) extends UnionSuggestions {
-  override protected val amfObject: AmfObject  = params.amfObject
-  override protected val dialect: Dialect      = params.actualDialect
-  lazy val unionType: Option[UnionNodeMapping] = getUnionType
+  override protected val amfObject: AmfObject     = params.amfObject
+  override protected val dialect: Dialect         = params.actualDialect
+  override protected val yPartBranch: YPartBranch = params.yPartBranch
 
-  lazy val definedProperties: Set[String] = params.yPartBranch.brothersKeys
+  lazy val unionType: Option[UnionNodeMapping] = getUnionType
 
   // this plugin applies when we are on a union with no type discriminator or the type discriminator is not set
   def resolve(): Option[Future[Seq[RawSuggestion]]] =
@@ -44,39 +46,10 @@ class AMLUnionCompletionPlugin(params: AmlCompletionRequest) extends UnionSugges
   }
 
   private def getSuggestions(unionMapping: UnionNodeMapping): Future[Seq[RawSuggestion]] = Future {
-    val inherited: Seq[RawSuggestion] = getPropertiesMapping(unionMapping).map(_.toRaw(""))
+    val inherited: Seq[RawSuggestion] = getUnionProperties(unionMapping).map(_.toRaw("unknown"))
     inherited ++
       Option(unionMapping.typeDiscriminatorName().value())
         .map(name => RawSuggestion(name, isAKey = true, "", mandatory = true))
   }
-
-  private def getPropertiesMapping(unionMapping: UnionNodeMapping): Seq[PropertyMapping] = {
-    unionMapping
-      .objectRange()
-      .map(_.value())
-      .map(getDeclaredDomainElement)
-      .flatMap({
-        case Some(value: UnionNodeMapping) => getPropertiesMapping(value)
-        case Some(value: NodeMapping)
-            if hasAllDefinedProperties(value, unionMapping.typeDiscriminatorName().option()) =>
-          value.propertiesMapping()
-        case _ => Seq.empty
-      })
-      // filter duplicates (same property defined in multiple ranges of the union)
-      .foldLeft(Seq[PropertyMapping]())({
-        case (acc, prop: PropertyMapping) if !acc.exists(p => isDuplicateOf(p, prop)) => acc :+ prop
-        case (acc, _)                                                                 => acc
-      })
-  }
-
-  private def hasAllDefinedProperties(value: NodeMapping, typeDiscriminatorName: Option[String]): Boolean =
-    definedProperties
-      .filterNot(typeDiscriminatorName.contains)
-      .forall(property => value.propertiesMapping().exists(_.name().value() == property))
-
-  private def isDuplicateOf(p: PropertyMapping, that: PropertyMapping): Boolean =
-    p.name().value() == that.name().value() &&
-      p.objectRange().headOption == that.objectRange().headOption &&
-      p.allowMultiple().value() == that.allowMultiple().value()
 
 }
