34ca2844a8187d416a00f918605adbf585952412
llibarona <llibarona@salesforce.com>
2021-07-11 13:09:24 -0300

    ALS-1595: fix path suggestions in Windows

--- als-common/shared/src/main/scala/org/mulesoft/als/common/PlatformDirectoryResolver.scala
+++ als-common/shared/src/main/scala/org/mulesoft/als/common/PlatformDirectoryResolver.scala
@@ -1,16 +1,18 @@
 package org.mulesoft.als.common
 
-import amf.core.remote.{File, Platform}
+import amf.core.remote.Platform
 
 import scala.concurrent.ExecutionContext.Implicits._
 import scala.concurrent.Future
 
 class PlatformDirectoryResolver(private val platform: Platform) extends DirectoryResolver {
-  override def exists(uri: String): Future[Boolean] = platform.fs.asyncFile(platform.decodeURI(toPath(uri))).exists
+  override def exists(uri: String): Future[Boolean] = platform.fs.asyncFile(FileUtils.getPath(uri, platform)).exists
 
   override def readDir(uri: String): Future[Seq[String]] =
-    platform.fs.asyncFile(platform.decodeURI(toPath(uri))).list.map(array => array.toSeq)
+    platform.fs.asyncFile(FileUtils.getPath(uri, platform)).list.map(array => array.toSeq)
 
   override def isDirectory(uri: String): Future[Boolean] =
-    platform.fs.asyncFile(platform.decodeURI(toPath(uri))).isDirectory
+    platform.fs.asyncFile(FileUtils.getPath(uri, platform)).isDirectory
+
+  override def toPath(uri: String): String = FileUtils.getPath(uri, platform)
 }
