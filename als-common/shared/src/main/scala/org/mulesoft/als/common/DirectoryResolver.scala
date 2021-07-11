70ab5e115497e2d12d0507263bd4d22c1dcd8812
llibarona <llibarona@salesforce.com>
2021-07-11 13:09:24 -0300

    ALS-1595: fix path suggestions in Windows

--- als-common/shared/src/main/scala/org/mulesoft/als/common/DirectoryResolver.scala
+++ als-common/shared/src/main/scala/org/mulesoft/als/common/DirectoryResolver.scala
@@ -6,25 +6,13 @@ import scala.concurrent.Future
 
 trait DirectoryResolver {
   val FilePrefix = "file://"
-
-  def dirName(uri: String): String = {
-    val lastSeparatorIndex = uri.lastIndexOf("/")
-
-    if (lastSeparatorIndex == -1 || lastSeparatorIndex == 0)
-      ""
-    else
-      uri.substring(0, lastSeparatorIndex)
-  }
-
   def exists(path: String): Future[Boolean]
 
   def readDir(path: String): Future[Seq[String]]
 
   def isDirectory(path: String): Future[Boolean]
 
-  def toPath(uri: String) =
+  def toPath(uri: String): String =
     File.unapply(uri).getOrElse(uri)
 
-  def toUri(uri: String) =
-    FilePrefix + File.unapply(uri).getOrElse(uri)
 }
