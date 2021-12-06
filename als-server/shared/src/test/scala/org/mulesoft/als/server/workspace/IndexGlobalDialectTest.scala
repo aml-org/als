57de0ddf040b46e563eb91d625d52b6aef10a7ed
agusayerza <agusayerza@gmail.com>
2021-12-06 17:09:38 -0300

    Index global dialect tests

---
+++ als-server/shared/src/test/scala/org/mulesoft/als/server/workspace/IndexGlobalDialectTest.scala
@@ -0,0 +1,119 @@
+package org.mulesoft.als.server.workspace
+
+import amf.aml.client.scala.model.document.DialectInstance
+import amf.core.client.scala.model.document.ExternalFragment
+import org.mulesoft.als.server.modules.WorkspaceManagerFactoryBuilder
+import org.mulesoft.als.server.protocol.LanguageServer
+import org.mulesoft.als.server.protocol.configuration.{AlsClientCapabilities, AlsInitializeParams}
+import org.mulesoft.als.server.{LanguageServerBaseTest, LanguageServerBuilder, MockDiagnosticClientNotifier}
+import org.mulesoft.amfintegration.amfconfiguration.EditorConfiguration
+import org.mulesoft.lsp.configuration.{TraceKind, WorkspaceFolder}
+
+class IndexGlobalDialectTest extends LanguageServerBaseTest {
+  override def rootPath: String = "config-provider"
+
+  val dialect: String       = """#%Dialect 1.0
+                  |dialect: Test
+                  |version: 2
+                  |external:
+                  |  myExternal: http://voc.com/#
+                  |documents:
+                  |  root:
+                  |    encodes: A
+                  |nodeMappings:
+                  |  A:
+                  |    classTerm: myExternal.ExternalClass
+                  |    mapping:
+                  |      prop1:
+                  |        range: string
+                  |        propertyTerm: myExternal.stringName
+                  |      prop2:
+                  |        required: true
+                  |        range: integer
+                  |        propertyTerm: myExternal.mixedScalar""".stripMargin
+  val instance1Path: String = filePath("ws1/instance.yaml")
+  val instance1: String =
+    """#%Test 2
+      |prop1: name
+      |prop2: 23""".stripMargin
+
+  val instance2Path: String = filePath("ws2/instance.yaml")
+  val instance2: String =
+    """#%Test 2
+      |prop1: name
+      |prop2: error""".stripMargin
+
+  private val dialectPath = "file://in-memory-dialect/dialect.yaml"
+
+  test("Global dialect will apply to all workspaces") {
+    val notifier     = new MockDiagnosticClientNotifier(3000)
+    val (server, wm) = buildServer(notifier)
+    withServer(
+      server,
+      AlsInitializeParams(Some(AlsClientCapabilities()),
+                          Some(TraceKind.Off),
+                          workspaceFolders =
+                            Some(Seq(WorkspaceFolder(filePath("ws1")), WorkspaceFolder(filePath("ws2")))))
+    )(s => {
+      for {
+        _  <- openFile(s)(instance1Path, instance1)
+        _  <- openFile(s)(instance2Path, instance2)
+        _  <- notifier.nextCall
+        _  <- notifier.nextCall
+        u1 <- wm.getUnit(instance1Path, "Instance1")
+        u2 <- wm.getUnit(instance2Path, "Instance2")
+        _  <- indexGlobalDialect(s)(dialectPath, dialect)
+        _  <- focusNotification(s)(instance1Path, 1)
+        _  <- focusNotification(s)(instance2Path, 1)
+        _  <- notifier.nextCall
+        _  <- notifier.nextCall
+        i1 <- wm.getUnit(instance1Path, "Instance1")
+        i2 <- wm.getUnit(instance2Path, "Instance2")
+      } yield {
+        assert(u1.unit.isInstanceOf[ExternalFragment])
+        assert(u2.unit.isInstanceOf[ExternalFragment])
+        assert(i1.unit.isInstanceOf[DialectInstance])
+        assert(i2.unit.isInstanceOf[DialectInstance])
+      }
+    })
+  }
+
+  test("Global dialect is immutable") {
+    val notifier     = new MockDiagnosticClientNotifier(3000)
+    val (server, wm) = buildServer(notifier)
+    withServer(server,
+               AlsInitializeParams(Some(AlsClientCapabilities()),
+                                   Some(TraceKind.Off),
+                                   workspaceFolders = Some(Seq(WorkspaceFolder(filePath("ws1"))))))(s => {
+      for {
+        _  <- openFile(s)(dialectPath, dialect)
+        _  <- indexGlobalDialect(s)(dialectPath, dialect)
+        _  <- openFile(s)(instance1Path, instance1)
+        _  <- notifier.nextCall
+        _  <- notifier.nextCall
+        i1 <- wm.getUnit(instance1Path, "Instance1")
+        _  <- changeFile(s)(dialectPath, dialect.replace("required: true", ""), 2)
+        _  <- notifier.nextCall
+        _  <- focusNotification(s)(instance1Path, 1)
+        i2 <- wm.getUnit(instance1Path, "Instance2")
+      } yield {
+        assert(i1.unit.isInstanceOf[DialectInstance])
+        assert(i2.unit.isInstanceOf[DialectInstance])
+        assert(i1.unit == i2.unit)
+      }
+    })
+  }
+
+  def buildServer(diagnosticNotifier: MockDiagnosticClientNotifier): (LanguageServer, WorkspaceManager) = {
+    val builder = new WorkspaceManagerFactoryBuilder(diagnosticNotifier, logger, EditorConfiguration())
+    val dm      = builder.buildDiagnosticManagers()
+    val factory = builder.buildWorkspaceManagerFactory()
+    val b = new LanguageServerBuilder(factory.documentManager,
+                                      factory.workspaceManager,
+                                      factory.configurationManager,
+                                      factory.resolutionTaskManager)
+    dm.foreach(m => b.addInitializableModule(m))
+    (b.build(), factory.workspaceManager)
+  }
+
+}
