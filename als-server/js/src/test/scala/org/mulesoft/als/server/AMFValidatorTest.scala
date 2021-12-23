ab452abd2cd21ab1d98edae792ee6b4d8f03f597
agusayerza <agusayerza@gmail.com>
2021-12-23 12:32:04 -0300

    Fix ignored JS Server test

--- als-server/js/src/test/scala/org/mulesoft/als/server/AMFValidatorTest.scala
+++ als-server/js/src/test/scala/org/mulesoft/als/server/AMFValidatorTest.scala
@@ -1,25 +1,35 @@
 package org.mulesoft.als.server
 
 import amf.core.client.common.validation.ValidationMode
-import amf.core.client.platform.model.domain
-import amf.core.client.platform.validation.payload
+import amf.core.client.platform.model.domain.Shape
 import amf.core.client.platform.validation.payload.{
   JsAMFPayloadValidationPlugin,
   JsPayloadValidator,
+  ShapeValidationConfiguration,
   ValidatePayloadRequest
 }
 import amf.core.internal.unsafe.PlatformSecrets
 
+import scala.scalajs.js
+
 trait AMFValidatorTest extends LanguageServerBaseTest with PlatformSecrets {
-//  def testValidator(fn: () => Unit): JsAMFPayloadValidationPlugin = new JsAMFPayloadValidationPlugin {
-//    override val id: String = "test-plugin"
-//
-//    override def applies(element: ValidatePayloadRequest): Boolean = {
-//      fn()
-//      logger.debug("Validator called", "AMFValidatorTest", "testValidator:applies")
-//      false
-//    }
-//
-//    override def validator(shape: domain.Shape, mediaType: String, config: payload.ShapeValidationConfiguration, validationMode: ValidationMode): JsPayloadValidator = ???
-//  }
+  def testValidator(fn: () => Unit): JsAMFPayloadValidationPlugin =
+    js.Dynamic
+      .literal(
+        id = "test-plugin",
+        applies = new js.Function1[ValidatePayloadRequest, Boolean] {
+          def apply(element: ValidatePayloadRequest): Boolean = {
+            fn()
+            logger.debug("Validator called", "AMFValidatorTest", "testValidator:applies")
+            false
+          }
+        },
+        validator = new js.Function4[Shape, String, ShapeValidationConfiguration, ValidationMode, JsPayloadValidator] {
+          def apply(shape: Shape,
+                    mediaType: String,
+                    config: ShapeValidationConfiguration,
+                    validationMode: ValidationMode): JsPayloadValidator = ???
+        }
+      )
+      .asInstanceOf[JsAMFPayloadValidationPlugin]
 }
