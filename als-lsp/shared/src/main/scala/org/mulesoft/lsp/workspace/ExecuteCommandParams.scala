0232c93cb8875a59d9d19617bbb4ce59914b408c
agusayerza <agusayerza@gmail.com>
2022-01-06 09:46:21 -0300

    Update documentation to reflect changes on didChangeConfiguration command and fix tests

--- als-lsp/shared/src/main/scala/org/mulesoft/lsp/workspace/ExecuteCommandParams.scala
+++ als-lsp/shared/src/main/scala/org/mulesoft/lsp/workspace/ExecuteCommandParams.scala
@@ -1,3 +1,11 @@
 package org.mulesoft.lsp.workspace
 
-case class ExecuteCommandParams(command: String, arguments: List[String])
+case class ExecuteCommandParams(command: String, args: List[String]) {
+  val arguments: List[String] = args.map(s => {
+    if (s.endsWith("\"") && s.endsWith("\"")) {
+      s.substring(1, s.length - 1)
+    } else {
+      s
+    }
+  })
+}
