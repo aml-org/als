fc450e35d5d135550389b6fc30987e12fc542cf4
agusayerza <agusayerza@gmail.com>
2022-01-06 11:03:06 -0300

    Fix AML rename tests

--- als-server/shared/src/test/scala/org/mulesoft/als/server/modules/rename/aml/AmlServerRenameTests.scala
+++ als-server/shared/src/test/scala/org/mulesoft/als/server/modules/rename/aml/AmlServerRenameTests.scala
@@ -2,6 +2,6 @@ package org.mulesoft.als.server.modules.rename.aml
 
 class AmlServerRenameTests extends AMLRenameTest {
 
-  ignore("test 001") { runTest("extensions/test001/extension.yaml", "RENAMED") }
-  ignore("test 002") { runTest("extensions/test002/extension.yaml", "RENAMED") }
+  test("test 001") { runTest("extensions/test001/extension.yaml", "RENAMED") }
+  test("test 002") { runTest("extensions/test002/extension.yaml", "RENAMED") }
 }
