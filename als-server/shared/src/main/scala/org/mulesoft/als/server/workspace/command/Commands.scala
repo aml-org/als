3669839410db54d2842dbb1f8542b2021c870bbf
agusayerza <agusayerza@gmail.com>
2022-01-19 15:53:16 -0300

    Remove didFocus command and update docs

--- als-server/shared/src/main/scala/org/mulesoft/als/server/workspace/command/Commands.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/workspace/command/Commands.scala
@@ -1,7 +1,6 @@
 package org.mulesoft.als.server.workspace.command
 
 object Commands {
-  val DID_FOCUS_CHANGE_COMMAND: String = "didFocusChange"
   val INDEX_DIALECT: String            = "indexDialect"
   val DID_CHANGE_CONFIGURATION: String = "didChangeConfiguration"
 }
