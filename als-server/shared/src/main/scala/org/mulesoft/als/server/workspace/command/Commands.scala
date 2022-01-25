0fa96ec537bebea125ac694f402b78e5daa2128e
Hernan Najles <hernan.naj+github@gmail.com>
2022-01-25 09:30:16 -0300

    Revert 3669839410db54d2842dbb1f8542b2021c870bbf (didFocus command deleted)

--- als-server/shared/src/main/scala/org/mulesoft/als/server/workspace/command/Commands.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/workspace/command/Commands.scala
@@ -1,6 +1,7 @@
 package org.mulesoft.als.server.workspace.command
 
 object Commands {
+  val DID_FOCUS_CHANGE_COMMAND: String = "didFocusChange"
   val INDEX_DIALECT: String            = "indexDialect"
   val DID_CHANGE_CONFIGURATION: String = "didChangeConfiguration"
 }
