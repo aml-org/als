28b9fac25f4d7e296d7e130cddfd23aa3da8c81e
Fede <48660757+mansillafederico@users.noreply.github.com>
2022-05-31 10:23:39 -0300

    W-11138673: Added version validation for uses facet (#760)
    W-11138673: remove plugin from CompletionPluginsRegistryAML.scala and added to VocabularyDialectPluginRegistry and RamlCompletionPluginRegistry

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/VocabularyDialectPluginRegistry.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/VocabularyDialectPluginRegistry.scala
@@ -4,10 +4,8 @@ import amf.aml.client.scala.model.document.Dialect
 import org.mulesoft.als.suggestions.AMLBaseCompletionPlugins
 import org.mulesoft.als.suggestions.aml.webapi.WebApiCompletionPluginRegistry
 import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
-import org.mulesoft.als.suggestions.plugins.aml.vocabulary.{
-  ClassTermUriCompletionPlugin,
-  PropertyTermUriCompletionPlugin
-}
+import org.mulesoft.als.suggestions.plugins.aml.vocabulary.{ClassTermUriCompletionPlugin, PropertyTermUriCompletionPlugin}
+import org.mulesoft.als.suggestions.plugins.aml.webapi.raml.AMLLibraryPathCompletion
 import org.mulesoft.amfintegration.dialect.dialects.metadialect.VocabularyDialect
 
 object VocabularyDialectPluginRegistry extends WebApiCompletionPluginRegistry {
@@ -15,7 +13,8 @@ object VocabularyDialectPluginRegistry extends WebApiCompletionPluginRegistry {
   private val all: Seq[AMLCompletionPlugin] =
     AMLBaseCompletionPlugins.all :+
       PropertyTermUriCompletionPlugin :+
-      ClassTermUriCompletionPlugin
+      ClassTermUriCompletionPlugin :+
+      AMLLibraryPathCompletion
 
   override def plugins: Seq[AMLCompletionPlugin] = all
 
