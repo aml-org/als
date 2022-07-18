1445c7ac7ffd65902ea4d99a69290c604075efb7
llibarona <llibarona@salesforce.com>
2022-07-18 13:20:51 -0300

    remove reconciler

--- als-server/shared/src/main/scala/org/mulesoft/als/server/modules/ast/ResolvedUnitListener.scala
+++ als-server/shared/src/main/scala/org/mulesoft/als/server/modules/ast/ResolvedUnitListener.scala
@@ -1,26 +1,22 @@
 package org.mulesoft.als.server.modules.ast
 
 import org.mulesoft.als.logger.Logger
-import org.mulesoft.als.server.modules.common.reconciler.{CancelledException, Reconciler, Runnable}
-import org.mulesoft.amfintegration.AmfResolvedUnit
+import org.mulesoft.als.server.modules.common.reconciler.Runnable
 import org.mulesoft.amfintegration.AmfImplicits._
+import org.mulesoft.amfintegration.AmfResolvedUnit
 
-import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 trait ResolvedUnitListener extends AstListener[AmfResolvedUnit] with AccessUnits[AmfResolvedUnit] {
   type RunType <: Runnable[Unit]
   protected val logger: Logger
-  protected val timeout              = 500
-  private val reconciler: Reconciler = new Reconciler(logger, timeout)
 
   protected def runnable(ast: AmfResolvedUnit, uuid: String): RunType
 
   protected def onSuccess(uuid: String, uri: String): Unit
   protected def onFailure(uuid: String, uri: String, t: Throwable): Unit
-  protected def onCancel(uuid: String, uri: String): Unit =
-    logger.debug(s"Canceled $uri - $uuid", "ResolveUnitListener", "onCancel")
 
   /** Meant just for logging
     * @param resolved
@@ -30,12 +26,9 @@ trait ResolvedUnitListener extends AstListener[AmfResolvedUnit] with AccessUnits
 
   override def onNewAst(ast: AmfResolvedUnit, uuid: String): Future[Unit] = {
     onNewAstPreprocess(ast, uuid)
-    reconciler
-      .schedule(runnable(ast, uuid))
-      .future andThen {
-      case Success(_)                     => onSuccess(uuid, ast.baseUnit.identifier)
-      case Failure(_: CancelledException) => onCancel(uuid, ast.baseUnit.identifier)
-      case Failure(exception)             => onFailure(uuid, ast.baseUnit.identifier, exception)
+    runnable(ast, uuid).run().future andThen {
+      case Success(_)         => onSuccess(uuid, ast.baseUnit.identifier)
+      case Failure(exception) => onFailure(uuid, ast.baseUnit.identifier, exception)
     }
   }
 }
