ef272a32e3b48892362fc9595502d0edfc04dfd8
Federico Mansilla <mansilla.fede@gmail.com>
2022-07-21 17:57:43 -0300

    W-11224225: Add tests
    Signed-off-by: Federico Mansilla <mansilla.fede@gmail.com>

--- als-server/shared/src/test/scala/org/mulesoft/als/server/modules/rename/jsonSchema/JsonSchemaRenameTest.scala
+++
@@ -1,7 +0,0 @@
-package org.mulesoft.als.server.modules.rename.jsonSchema
-
-import org.mulesoft.als.server.modules.rename.ServerRenameTest
-
-trait JsonSchemaRenameTest extends ServerRenameTest {
-  def rootPath: String = "rename/jsonSchema/test.001/draft-03.propertyName.json"
-}
--- als-server/shared/src/test/scala/org/mulesoft/als/server/modules/rename/jsonschema/JsonSchemaServerRenameTest.scala
+++ als-server/shared/src/test/scala/org/mulesoft/als/server/modules/rename/jsonschema/JsonSchemaServerRenameTest.scala
@@ -1,7 +1,7 @@
-package org.mulesoft.als.server.modules.rename.jsonSchema
+package org.mulesoft.als.server.modules.rename.jsonschema
 
 class JsonSchemaServerRenameTest extends JsonSchemaRenameTest {
   ignore("Test 001 - Rename security scheme referenced from sequence") {
-    runTest("test.001/draft-03.propertyName.json", "RENAMED")
+    runTest("test001/basic-schema.json", "RENAMED")
   }
 }
