99ae9ad44ca890f5bbc89287a6a066d89a467c33
llibarona <llibarona@salesforce.com>
2022-09-07 15:12:56 -0300

    W-11224290: add 'required' completion plugin for json schema

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/jsonschema/JsonSchemaCompletionPluginRegistry.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/aml/jsonschema/JsonSchemaCompletionPluginRegistry.scala
@@ -2,9 +2,11 @@ package org.mulesoft.als.suggestions.aml.jsonschema
 import org.mulesoft.als.suggestions.AMLBaseCompletionPlugins
 import org.mulesoft.als.suggestions.aml.webapi.WebApiCompletionPluginRegistry
 import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.jsonSchema.JsonSchemaRequiredObjectCompletionPlugin
 
 trait JsonSchemaCompletionPluginRegistry extends WebApiCompletionPluginRegistry {
   protected val all: Seq[AMLCompletionPlugin] =
-    AMLBaseCompletionPlugins.all
+    AMLBaseCompletionPlugins.all :+
+      JsonSchemaRequiredObjectCompletionPlugin
   def plugins: Seq[AMLCompletionPlugin] = all
 }
