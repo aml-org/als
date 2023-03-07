f534d4e36a4a0eda6831fc06496f24645fc61222
Federico Mansilla <mansilla.fede@gmail.com>
2023-03-07 14:54:43 -0300

    W-12601018: Compiling with node 14
    Signed-off-by: Federico Mansilla <mansilla.fede@gmail.com>

--- als-lsp/shared/src/main/scala/org/mulesoft/lsp/feature/common/Position.scala
+++ als-lsp/shared/src/main/scala/org/mulesoft/lsp/feature/common/Position.scala
@@ -15,7 +15,7 @@ import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
   * If the character value is greater than the line length it defaults back to the line length
   */
 @JSExportAll
-@JSExportTopLevel("Position")
+@JSExportTopLevel("LSPPosition")
 case class Position(line: Int, character: Int) {
   override def toString: String = s"common.Position[L:$line,C:$character]"
 }
