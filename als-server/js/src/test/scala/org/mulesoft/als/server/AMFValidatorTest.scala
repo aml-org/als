71f4cc7c136441d8fb53eb28269bcaa75c5a61d8
Federico Mansilla <mansilla.fede@gmail.com>
2023-03-13 12:11:05 -0300

    W-12585757/Adopt AMF: Fix test JS on server and lsp
    Signed-off-by: Federico Mansilla <mansilla.fede@gmail.com>

--- als-server/js/src/test/scala/org/mulesoft/als/server/AMFValidatorTest.scala
+++ als-server/js/src/test/scala/org/mulesoft/als/server/AMFValidatorTest.scala
@@ -5,45 +5,35 @@ import amf.core.client.platform.model.domain.Shape
 import amf.core.client.platform.validation.payload.{
   JsAMFPayloadValidationPlugin,
   JsPayloadValidator,
-  ShapeValidationConfiguration,
-  ValidatePayloadRequest
+  ShapeValidationConfiguration
 }
 import amf.core.internal.unsafe.PlatformSecrets
 
 import scala.scalajs.js
 
 trait AMFValidatorTest extends LanguageServerBaseTest with PlatformSecrets {
+
+  def getAppliesFunction(fn: () => Unit): Boolean = {
+    fn()
+    logger.debug("Validator called", "AMFValidatorTest", "testValidator:applies")
+    false
+  }
+
+  def getValidatorFunction(): JsPayloadValidator = {
+    def apply(
+        shape: Shape,
+        mediaType: String,
+        config: ShapeValidationConfiguration,
+        validationMode: ValidationMode
+    ): JsPayloadValidator = ???
+  }.asInstanceOf[JsPayloadValidator]
+
   def testValidator(fn: () => Unit): JsAMFPayloadValidationPlugin =
     js.Dynamic
       .literal(
         id = "test-plugin",
-        applies = new AppliesFunction {
-          override def applies(element: ValidatePayloadRequest): Boolean = {
-            def apply(element: ValidatePayloadRequest): Boolean = {
-              fn()
-              logger.debug("Validator called", "AMFValidatorTest", "testValidator:applies")
-              false
-            }
-          }.asInstanceOf[Boolean]
-        },
-        validator = new ValidatorFunction {
-          override def validator(): JsPayloadValidator = {
-            def apply(
-                shape: Shape,
-                mediaType: String,
-                config: ShapeValidationConfiguration,
-                validationMode: ValidationMode
-            ): JsPayloadValidator = ???
-          }.asInstanceOf[JsPayloadValidator]
-        }
+        applies = getAppliesFunction(fn),
+        validator = getValidatorFunction()
       )
       .asInstanceOf[JsAMFPayloadValidationPlugin]
-}
-
-trait AppliesFunction extends js.Object {
-  def applies(element: ValidatePayloadRequest): Boolean
-}
-
-trait ValidatorFunction extends js.Object {
-  def validator(): JsPayloadValidator
 }
