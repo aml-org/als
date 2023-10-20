11db66d13f64f80d7f52df483b3cb14777cc0031
agusayerza <agusayerza@gmail.com>
2023-10-20 00:23:14 -0300

    Do not provide suggestions inside YAML comments

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/CompletionProviderAST.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/CompletionProviderAST.scala
@@ -1,9 +1,11 @@
 package org.mulesoft.als.suggestions
 
-import org.mulesoft.als.common.YPartBranch
+import org.mulesoft.als.common.{ASTPartBranch, YPartBranch, YamlUtils}
 import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
 import org.mulesoft.als.suggestions.interfaces._
+import org.mulesoft.common.client.lexical.Position
 import org.mulesoft.lsp.feature.completion.CompletionItem
+import org.yaml.model.YComment
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -22,6 +24,7 @@ class CompletionProviderAST(request: AmlCompletionRequest) extends CompletionPro
 
   override def suggest(): Future[Seq[CompletionItem]] =
     if (request.astPartBranch.isMultiline) Future.successful(Nil)
+    else if (isInsideComment(request.position.toAmfPosition, request.astPartBranch)) Future.successful(Nil)
     else
       request.completionsPluginHandler
         .pluginSuggestions(request)
@@ -31,6 +34,20 @@ class CompletionProviderAST(request: AmlCompletionRequest) extends CompletionPro
             .filterNot(rs => arraySiblings(rs.newText))
             .map(request.styler.rawToStyledSuggestion)
         })
+
+  private def isInsideComment(position: Position, astPartBranch: ASTPartBranch): Boolean =
+    astPartBranch match {
+      case yPart: YPartBranch =>
+        // we are inside a comment if the current node is an empty YScalar and the request position matches a YComment on the parent node
+        yPart.isEmptyNode && yPart.parentEntry.exists(parent => {
+          parent.children.exists {
+            case comment: YComment if YamlUtils.contains(comment.range, position) => true
+            case _ => false
+          }
+
+        })
+      case _ => false
+    }
 }
 
 object CompletionProviderAST {
