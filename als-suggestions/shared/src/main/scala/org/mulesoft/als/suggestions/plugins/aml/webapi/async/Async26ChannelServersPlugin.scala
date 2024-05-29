c5c58cec3953782b050b5b4fde12b42202b959d2
Leandro Libarona <llibarona@salesforce.com>
2024-05-29 17:23:05 -0300

    only suggest encoded servers and not declared ones under channels in async 2

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/Async26ChannelServersPlugin.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/Async26ChannelServersPlugin.scala
@@ -2,13 +2,13 @@ package org.mulesoft.als.suggestions.plugins.aml.webapi.async
 
 import amf.apicontract.client.scala.model.domain.api.AsyncApi
 import amf.apicontract.client.scala.model.domain.{EndPoint, Server}
-import amf.core.client.scala.model.document.{BaseUnit, DeclaresModel, EncodesModel}
+import amf.core.client.scala.model.document.{BaseUnit, EncodesModel}
 import org.mulesoft.als.suggestions.RawSuggestion
 import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
 import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 object Async26ChannelServersPlugin extends AMLCompletionPlugin {
   override def id: String = "Async26ChannelServersPlugin"
@@ -16,7 +16,7 @@ object Async26ChannelServersPlugin extends AMLCompletionPlugin {
   override def resolve(request: AmlCompletionRequest): Future[Seq[RawSuggestion]] = Future {
     request.amfObject match {
       case _: Server if request.branchStack.headOption.exists(_.isInstanceOf[EndPoint]) =>
-        (getDeclaredServers(request.baseUnit) ++ getRootServers(request.baseUnit)).distinct
+        getRootServers(request.baseUnit)
           .map(server => RawSuggestion(server, isAKey = false, "Servers", mandatory = false))
       case _ => Seq.empty
     }
@@ -34,13 +34,4 @@ object Async26ChannelServersPlugin extends AMLCompletionPlugin {
   } yield {
     asyncApi.servers.map(_.name).flatMap(_.option())
   }).getOrElse(Seq.empty)
-
-  private def getDeclaredServers(baseUnit: BaseUnit): Seq[String] =
-    baseUnit match {
-      case d: DeclaresModel =>
-        d.declares.collect { case s: Server =>
-          s.name.value()
-        }
-      case _ => Seq.empty
-    }
 }
