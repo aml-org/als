9233578b50a8c487d2d7d2ef7e279d0d67f68753
emilianoascona <39959392+emilianoascona@users.noreply.github.com>
2024-07-31 16:29:07 -0300

    W 16346170/bump dependencies (#1044)
    * bump dependencies
    
    * Update ProfileNames

--- als-common/shared/src/main/scala/org/mulesoft/amfintegration/override/OverrideProfileNames.scala
+++ als-common/shared/src/main/scala/org/mulesoft/amfintegration/override/OverrideProfileNames.scala
@@ -22,6 +22,7 @@ object ProfileNames {
   val GRAPHQL: ProfileName            = GraphQLProfile
   val GRAPHQL_FEDERATION: ProfileName = GraphQLFederationProfile
   val JSONSCHEMA: ProfileName         = JsonSchemaProfile
+  val AVROSCHEMA: ProfileName         = AvroSchemaProfile
 
   lazy val specProfiles: Seq[ProfileName] =
     Seq(
@@ -36,7 +37,8 @@ object ProfileNames {
       GraphQLProfile,
       GraphQLFederationProfile,
       GrpcProfile,
-      JsonSchemaProfile
+      JsonSchemaProfile,
+      AvroSchemaProfile
     )
 }
 
@@ -95,6 +97,11 @@ object JsonSchemaProfile extends ProfileName(JsonSchema.id, AMFStyle) {
   override def isRaml(): Boolean = false
 }
 
+object AvroSchemaProfile extends ProfileName(AvroSchema.id, AMFStyle) {
+  override def isOas(): Boolean  = false
+  override def isRaml(): Boolean = false
+}
+
 object ProfileName {
   def unapply(name: String): Option[ProfileName] =
     name match {
@@ -108,6 +115,7 @@ object ProfileName {
       case GraphQLProfile.p           => Some(GraphQLProfile)
       case GraphQLFederationProfile.p => Some(GraphQLFederationProfile)
       case JsonSchemaProfile.p        => Some(JsonSchemaProfile)
+      case AvroSchemaProfile.p        => Some(AvroSchemaProfile)
       case _                          => None
     }
 
@@ -129,6 +137,7 @@ object ProfileName {
     case GraphQL.id           => GraphQLProfile
     case GraphQLFederation.id => GraphQLFederationProfile
     case JsonSchema.id        => JsonSchemaProfile
+    case AvroSchema.id        => AvroSchemaProfile
     case custom               => new ProfileName(custom)
   }
 }
