f6f6cecbf73cccd6a95d9829699238cf6f13546c
Leandro Libarona <llibarona@salesforce.com>
2024-08-12 17:23:08 -0300

    fix styling for avsc files

--- als-server/shared/src/test/scala/org/mulesoft/als/server/modules/completion/avro/StructureTests.scala
+++ als-server/shared/src/test/scala/org/mulesoft/als/server/modules/completion/avro/StructureTests.scala
@@ -7,7 +7,7 @@ class StructureTests extends AVROSuggestionTestServer {
   override implicit val executionContext = ExecutionContext.Implicits.global
 
   test("test avro empty") {
-    runTest("base/dir1/avro-schema-01.avsc", Set("type:", "default:", "logicalType:"))
+    runTest("base/dir1/avro-schema-01.avsc", Set("\"type\": \"$1\"", "\"default\": \"$1\"", "\"logicalType\": \"$1\""))
   }
 
   test("test avro types") {
@@ -15,24 +15,24 @@ class StructureTests extends AVROSuggestionTestServer {
       "base/dir1/avro-schema-02.avsc",
       Set(
         "\"null\"",
-        "boolean",
-        "int",
-        "long",
-        "float",
-        "double",
-        "bytes",
-        "string",
-        "record",
-        "enum",
-        "array",
-        "map",
-        "fixed"
+        "\"boolean\"",
+        "\"int\"",
+        "\"long\"",
+        "\"float\"",
+        "\"double\"",
+        "\"bytes\"",
+        "\"string\"",
+        "\"record\"",
+        "\"enum\"",
+        "\"array\"",
+        "\"map\"",
+        "\"fixed\""
       )
     )
   }
 
   test("test avro nested schema") {
-    runTest("base/dir1/avro-schema-03.avsc", Set("type:", "default:", "logicalType:"))
+    runTest("base/dir1/avro-schema-03.avsc", Set("\"type\": \"$1\"", "\"default\": \"$1\"", "\"logicalType\": \"$1\""))
   }
 
   test("test avro nested types") {
@@ -40,20 +40,20 @@ class StructureTests extends AVROSuggestionTestServer {
       "base/dir1/avro-schema-04.avsc",
       Set(
         "\"null\"",
-        "boolean",
-        "int",
-        "long",
-        "float",
-        "double",
-        "bytes",
-        "string",
-        "record",
-        "enum",
-        "array",
-        "map",
-        "fixed",
-        "org.apache.avro.file.Header",
-        "Sync"
+        "\"boolean\"",
+        "\"int\"",
+        "\"long\"",
+        "\"float\"",
+        "\"double\"",
+        "\"bytes\"",
+        "\"string\"",
+        "\"record\"",
+        "\"enum\"",
+        "\"array\"",
+        "\"map\"",
+        "\"fixed\"",
+        "\"org.apache.avro.file.Header\"",
+        "\"Sync\""
       )
     )
   }
