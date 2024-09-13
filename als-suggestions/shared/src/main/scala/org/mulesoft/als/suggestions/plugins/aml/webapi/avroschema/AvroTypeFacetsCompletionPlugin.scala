202469e7103a9721389c3c007d35c67f41002348
BlockLucas <block.lucas@gmail.com>
2024-09-13 16:27:30 -0300

    W-16734026 - wip - root suggestions ok but with extra default facets like items / properties

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/avroschema/AvroTypeFacetsCompletionPlugin.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/avroschema/AvroTypeFacetsCompletionPlugin.scala
@@ -3,9 +3,10 @@ package org.mulesoft.als.suggestions.plugins.aml.webapi.avroschema
 import amf.aml.client.scala.model.document.Dialect
 import amf.aml.client.scala.model.domain.NodeMapping
 import org.mulesoft.als.suggestions.plugins.aml.webapi.WebApiTypeFacetsCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.async.Async20TypeFacetsCompletionPlugin.dialect
 import org.mulesoft.amfintegration.dialect.dialects.asyncapi20.schema.StringShapeAsync2Node
 import org.mulesoft.amfintegration.dialect.dialects.avro.AvroFieldNode.PropertyShapeAvroNode
-import org.mulesoft.amfintegration.dialect.dialects.avro.{AvroDialect, AvroTypesDialect}
+import org.mulesoft.amfintegration.dialect.dialects.avro.{AvroAnyNode, AvroDialect, AvroPrimitiveNode, AvroTypesDialect}
 
 object AvroTypeFacetsCompletionPlugin extends WebApiTypeFacetsCompletionPlugin {
 
@@ -14,15 +15,15 @@ object AvroTypeFacetsCompletionPlugin extends WebApiTypeFacetsCompletionPlugin {
   val dialect: Dialect = AvroDialect.dialect
 
 //TODO CHECK HOW TO COMPLETE THIS
-  override def stringShapeNode: NodeMapping = AvroTypesDialect.StringShapeNode
+  override def stringShapeNode: NodeMapping = AvroPrimitiveNode.Obj
 
-  override def numberShapeNode: NodeMapping = StringShapeAsync2Node.Obj
+  override def numberShapeNode: NodeMapping = AvroPrimitiveNode.Obj
 
-  override def integerShapeNode: NodeMapping = StringShapeAsync2Node.Obj
+  override def integerShapeNode: NodeMapping = AvroPrimitiveNode.Obj
 
-  override def anyShapeNode: NodeMapping = ???
+  override def anyShapeNode: NodeMapping = AvroAnyNode.Obj
 
-  override def declarations: Seq[NodeMapping] = ???
+  override def declarations: Seq[NodeMapping] = dialect.declares.collect({ case n: NodeMapping => n })
 
-  override def propertyShapeNode: Option[NodeMapping] = PropertyShapeAvroNode
+  override def propertyShapeNode: Option[NodeMapping] = None
 }
--- als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/async2/AsyncAPI26YamlTestByDirectory.scala
+++ als-suggestions/shared/src/test/scala/org/mulesoft/als/suggestions/test/async2/AsyncAPI26YamlTestByDirectory.scala
@@ -4,7 +4,7 @@ import amf.core.internal.remote.{Async20YamlHint, Hint}
 import org.mulesoft.als.suggestions.test.SuggestionByDirectoryTest
 
 class AsyncAPI26YamlTestByDirectory extends SuggestionByDirectoryTest {
-  override def basePath: String = "als-suggestions/shared/src/test/resources/test/async26/by-directory"
+  override def basePath: String = "als-suggestions/shared/src/test/resources/test/async26/by-directory/messages/payload"
 
   override def origin: Hint = Async20YamlHint
 
