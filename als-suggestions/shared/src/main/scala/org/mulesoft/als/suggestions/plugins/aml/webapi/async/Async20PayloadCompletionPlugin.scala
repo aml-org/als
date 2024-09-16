7007bfc380c76bc714cacf1c0cf992b75e8886c8
Leandro Libarona <l.libarona@gmail.com>
2024-09-16 14:04:32 -0300

    add avro support for payloads

--- als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/Async20PayloadCompletionPlugin.scala
+++ als-suggestions/shared/src/main/scala/org/mulesoft/als/suggestions/plugins/aml/webapi/async/Async20PayloadCompletionPlugin.scala
@@ -5,6 +5,7 @@ import amf.core.client.scala.model.domain.Shape
 import org.mulesoft.als.suggestions.RawSuggestion
 import org.mulesoft.als.suggestions.aml.AmlCompletionRequest
 import org.mulesoft.als.suggestions.interfaces.AMLCompletionPlugin
+import org.mulesoft.als.suggestions.plugins.aml.webapi.WebApiTypeFacetsCompletionPlugin
 import org.mulesoft.amfintegration.dialect.dialects.asyncapi20.AsyncApi20Dialect
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -14,23 +15,25 @@ object Async20PayloadCompletionPlugin extends AMLCompletionPlugin with AsyncMedi
   override def id: String = "Async20PayloadCompletionPlugin"
 
   override def resolve(request: AmlCompletionRequest): Future[Seq[RawSuggestion]] = {
-    Future {
-      val branchStack = request.branchStack
-      branchStack.headOption match { // check replacing this with findFirst
-        case Some(p: Payload)
-            if request.astPartBranch.isKeyDescendantOf("payload") &&
-              !branchStack.exists(_.isInstanceOf[Server]) =>
-          request.amfObject match {
-            case s: Shape =>
-              if (p.schemaMediaType.isNullOrEmpty)
-                Async20TypeFacetsCompletionPlugin.resolveShape(s, branchStack, AsyncApi20Dialect())
-              else
-                findPluginForMediaType(p)
-                  .map(_.resolveShape(s, branchStack, AsyncApi20Dialect()))
-                  .getOrElse(Async20TypeFacetsCompletionPlugin.resolveShape(s, branchStack, AsyncApi20Dialect()))
-          }
-        case _ => Seq.empty
-      }
+    val branchStack = request.branchStack
+    branchStack.headOption match { // check replacing this with findFirst
+      case Some(p: Payload)
+          if request.astPartBranch.isKeyDescendantOf("payload") &&
+            !branchStack.exists(_.isInstanceOf[Server]) =>
+        request.amfObject match {
+          case s: Shape =>
+            if (p.schemaMediaType.isNullOrEmpty)
+              Future(Async20TypeFacetsCompletionPlugin.resolveShape(s, branchStack, AsyncApi20Dialect()))
+            else
+              findPluginForMediaType(p)
+                .map{
+                  case wf: WebApiTypeFacetsCompletionPlugin =>
+                    Future(wf.resolveShape(s, branchStack, AsyncApi20Dialect()))
+                  case generic => generic.resolve(request)
+                }
+                .getOrElse(Future(Async20TypeFacetsCompletionPlugin.resolveShape(s, branchStack, AsyncApi20Dialect())))
+        }
+      case _ => Future(Seq.empty)
     }
   }
 
